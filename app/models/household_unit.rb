2b419231ea14432e0e08c92551c1e54b17708a32
Paul Friedman <p-friedman@northwestern.edu>
2011-08-02 13:10:13 -0500

    added surveyor to application with a the pregnancy visit one instrument

--- app/models/household_unit.rb
+++ app/models/household_unit.rb
@@ -22,6 +22,7 @@
 #
 
 # The definition of a household is really based on the individual person's definition of a family.
+#
 # The common definition is residence at the same address; however, the composition of the household might be 
 # parent/child; siblings; roommates, or other combinations of relationships. 
 # SCs will initially assign a household an ID (HH_ID) during the recruitment process, once a family/household 
@@ -29,7 +30,14 @@
 # (Several scenarios are described in the Dwelling Unit Household Linkage Transmission Table.) 
 # Children may be associated with multiple households, including households where non-residential fathers, 
 # other family members or informants who provide information may live. 
+#
 # (Several Scenarios are described in the Household Person Linkage Table.)
+#
+# A household record is created when it is determined that a household is eligible for the NCS and also records the reason it is eligible. 
+# "Household information becomes known during the Enumeration or the Pregnancy Screening process.  
+#  The Household table should be completed at that time."
+# Once a household is identified it continues to be associated with the participant(s) and people related to them. 
+#
 class HouseholdUnit < ActiveRecord::Base
   include MdesRecord
   acts_as_mdes_record :public_id_field => :hh_id
