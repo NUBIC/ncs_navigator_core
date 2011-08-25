7b370d9367f245692af696cf32864f7fdee7840a
Paul Friedman <p-friedman@northwestern.edu>
2011-08-25 16:24:19 -0500

    use new ncs nav config for values

--- config/initializers/system_configuration.rb
+++ config/initializers/system_configuration.rb
@@ -1,5 +1,21 @@
-class SystemConfiguration
+require 'ncs_navigator/mdes'
+require 'ncs_navigator/configuration'
+
+module NcsNavigatorCore
   class << self
-    attr_accessor :psu_code
+    
+    def footer_left_logo_path
+      NcsNavigator.configuration.footer_logo_left.to_s.split("/").last if NcsNavigator.configuration.footer_logo_left
+    end
+    
+    def footer_right_logo_path
+      NcsNavigator.configuration.footer_logo_right.to_s.split("/").last if NcsNavigator.configuration.footer_logo_right
+    end
+    
+    def psu
+      NcsNavigator.configuration.psus.first.id unless NcsNavigator.configuration.psus.blank?
+    end
+    
   end
 end
+
