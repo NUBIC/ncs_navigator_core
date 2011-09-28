2db7f01533413cd653dffb49fdd8f7e6041c0656
Paul Friedman <p-friedman@northwestern.edu>
2011-09-28 12:37:42 -0500

    contacts for pre-pregnancy instrument

--- app/models/pre_pregnancy_operational_data_extractor.rb
+++ app/models/pre_pregnancy_operational_data_extractor.rb
@@ -1,42 +1,5 @@
 class PrePregnancyOperationalDataExtractor
   
-  # EMAIL                 Email.email
-  # 
-  # CELL_PHONE            Telephone.phone_nbr
-  # 
-  # CONTACT_FNAME_1       Person.first_name
-  # CONTACT_LNAME_1       Person.last_name
-  # 
-  # CONTACT_RELATE_1      ParticipantPersonLink.relationship_code   PERSON_PARTCPNT_RELTNSHP_CL1/CONTACT_RELATIONSHIP_CL2
-  # CONTACT_RELATE1_OTH   ParticipantPersonLink.relationship_other
-  # 
-  # C_ADDR_1_1            Address.address_one
-  # C_ADDR_2_1            Address.address_two
-  # C_UNIT_1              Address.unit
-  # C_CITY_1              Address.city
-  # C_STATE_1             Address.state_code                        STATE_CL1
-  # C_ZIP_1               Address.zip
-  # C_ZIP4_1              Address.zip4
-  # 
-  # CONTACT_PHONE_1       Telephone.phone_nbr
-  # 
-  # 
-  # CONTACT_FNAME_2       Person.first_name
-  # CONTACT_LNAME_2       Person.last_name
-  # 
-  # CONTACT_RELATE_2      ParticipantPersonLink.relationship_code   PERSON_PARTCPNT_RELTNSHP_CL1/CONTACT_RELATIONSHIP_CL2
-  # CONTACT_RELATE2_OTH   ParticipantPersonLink.relationship_other
-  # 
-  # C_ADDR_1_2            Address.address_one
-  # C_ADDR_2_2            Address.address_two
-  # C_UNIT_2              Address.unit
-  # C_CITY_2              Address.city
-  # C_STATE_2             Address.state_code                        STATE_CL1
-  # C_ZIP_2               Address.zip
-  # C_ZIP4_2              Address.zip4
-  # 
-  # CONTACT_PHONE_2       Telephone.phone_nbr
-  
   INTERVIEW_PREFIX = "PRE_PREG"
   
   PERSON_MAP = {
@@ -56,6 +19,54 @@ class PrePregnancyOperationalDataExtractor
     "#{INTERVIEW_PREFIX}.EMAIL"           => "email"
   }
   
+  CONTACT_1_PERSON_MAP = {
+    "#{INTERVIEW_PREFIX}.CONTACT_FNAME_1"     => "first_name",
+    "#{INTERVIEW_PREFIX}.CONTACT_LNAME_1"     => "last_name",
+  }
+  
+  CONTACT_1_RELATIONSHIP_MAP = {
+    "#{INTERVIEW_PREFIX}.CONTACT_RELATE_1"    => "relationship_code",
+    "#{INTERVIEW_PREFIX}.CONTACT_RELATE1_OTH" => "relationship_other",
+  }
+  
+  CONTACT_1_ADDRESS_MAP = {
+    "#{INTERVIEW_PREFIX}.C_ADDR_1_1"          => "address_one",
+    "#{INTERVIEW_PREFIX}.C_ADDR_2_1"          => "address_two",
+    "#{INTERVIEW_PREFIX}.C_UNIT_1"            => "unit",
+    "#{INTERVIEW_PREFIX}.C_CITY_1"            => "city",
+    "#{INTERVIEW_PREFIX}.C_STATE_1"           => "state_code",
+    "#{INTERVIEW_PREFIX}.C_ZIP_1"             => "zip",
+    "#{INTERVIEW_PREFIX}.C_ZIP4_1"            => "zip4",
+  }
+  
+  CONTACT_1_PHONE_MAP = {
+    "#{INTERVIEW_PREFIX}.CONTACT_PHONE_1"     => "phone_nbr",
+  }
+  
+  CONTACT_2_PERSON_MAP = {
+    "#{INTERVIEW_PREFIX}.CONTACT_FNAME_2"     => "first_name",
+    "#{INTERVIEW_PREFIX}.CONTACT_LNAME_2"     => "last_name",
+  }
+  
+  CONTACT_2_RELATIONSHIP_MAP = {
+    "#{INTERVIEW_PREFIX}.CONTACT_RELATE_2"    => "relationship_code",
+    "#{INTERVIEW_PREFIX}.CONTACT_RELATE2_OTH" => "relationship_other",
+  }
+  
+  CONTACT_2_ADDRESS_MAP = {
+    "#{INTERVIEW_PREFIX}.C_ADDR_1_2"          => "address_one",
+    "#{INTERVIEW_PREFIX}.C_ADDR_2_2"          => "address_two",
+    "#{INTERVIEW_PREFIX}.C_UNIT_2"            => "unit",
+    "#{INTERVIEW_PREFIX}.C_CITY_2"            => "city",
+    "#{INTERVIEW_PREFIX}.C_STATE_2"           => "state_code",
+    "#{INTERVIEW_PREFIX}.C_ZIP_2"             => "zip",
+    "#{INTERVIEW_PREFIX}.C_ZIP4_2"            => "zip4",
+  }
+  
+  CONTACT_2_PHONE_MAP = {
+    "#{INTERVIEW_PREFIX}.CONTACT_PHONE_2"     => "phone_nbr",
+  }
+  
   class << self
     
     def extract_data(response_set)
@@ -67,6 +78,17 @@ class PrePregnancyOperationalDataExtractor
       end
       cell_phone = Telephone.new(:person => person, :phone_type => Telephone.cell_phone_type)
       email = Email.new(:person => person)
+      
+      contact1 = Person.new
+      contact1phone = Telephone.new(:person => contact1)
+      contact1address = Address.new(:person => contact1, :dwelling_unit => DwellingUnit.new)
+      contact1relationship = ParticipantPersonLink.new(:person => contact1, :participant => participant)
+
+      contact2 = Person.new
+      contact2phone = Telephone.new(:person => contact2)
+      contact2address = Address.new(:person => contact2, :dwelling_unit => DwellingUnit.new)
+      contact2relationship = ParticipantPersonLink.new(:person => contact2, :participant => participant)
+
       
       response_set.responses.each do |r|
         
@@ -85,12 +107,112 @@ class PrePregnancyOperationalDataExtractor
           email.send("#{EMAIL_MAP[data_export_identifier]}=", value) unless value.blank?
         end
         
+        if CONTACT_1_PERSON_MAP.has_key?(data_export_identifier)
+          contact1.send("#{CONTACT_1_PERSON_MAP[data_export_identifier]}=", value) unless value.blank?
+        end
+        
+        if CONTACT_1_RELATIONSHIP_MAP.has_key?(data_export_identifier)        
+          contact1relationship.send("#{CONTACT_1_RELATIONSHIP_MAP[data_export_identifier]}=", contact_to_person_relationship(value)) unless value.blank?
+        end
+
+        if CONTACT_1_ADDRESS_MAP.has_key?(data_export_identifier)
+          contact1address.send("#{CONTACT_1_ADDRESS_MAP[data_export_identifier]}=", value) unless value.blank?
+        end
+
+        if CONTACT_1_PHONE_MAP.has_key?(data_export_identifier)
+          contact1phone.send("#{CONTACT_1_PHONE_MAP[data_export_identifier]}=", value) unless value.blank?
+        end
+        
+        if CONTACT_2_PERSON_MAP.has_key?(data_export_identifier)
+          contact2.send("#{CONTACT_2_PERSON_MAP[data_export_identifier]}=", value) unless value.blank?
+        end
+        
+        if CONTACT_2_RELATIONSHIP_MAP.has_key?(data_export_identifier)        
+          contact2relationship.send("#{CONTACT_2_RELATIONSHIP_MAP[data_export_identifier]}=", contact_to_person_relationship(value)) unless value.blank?
+        end
+
+        if CONTACT_2_ADDRESS_MAP.has_key?(data_export_identifier)
+          contact2address.send("#{CONTACT_2_ADDRESS_MAP[data_export_identifier]}=", value) unless value.blank?
+        end
+
+        if CONTACT_2_PHONE_MAP.has_key?(data_export_identifier)
+          contact2phone.send("#{CONTACT_2_PHONE_MAP[data_export_identifier]}=", value) unless value.blank?
+        end
+        
+      end
+      
+      if !contact1.to_s.blank? && !contact1relationship.relationship_code.blank?
+        contact1address.save! unless contact1address.to_s.blank?
+        contact1phone.save! unless contact1phone.phone_nbr.blank?
+        contact1.save!
+        contact1relationship.save!
       end
 
+      if !contact2.to_s.blank? && !contact2relationship.relationship_code.blank?
+        contact2address.save! unless contact2address.to_s.blank?
+        contact2phone.save! unless contact2phone.phone_nbr.blank?
+        contact2.save!
+        contact2relationship.save!
+      end
+
+      
       email.save! unless email.email.blank?
       cell_phone.save! unless cell_phone.phone_nbr.blank?
       person.save!
       
+    end
+    
+    ##
+    # Convert Contact Survey code to Person/Participant Relationship code
+    # 
+    # CONTACT_RELATIONSHIP_CL2
+    #   1 Mother/Father
+    #   2 Brother/Sister
+    #   3 Aunt/Uncle
+    #   4 Grandparent
+    #   5 Neighbor
+    #   6 Friend
+    #   -5  Other
+    # PERSON_PARTCPNT_RELTNSHP_CL1
+    #   1 Participant/Self
+    #   2 Biological Mother
+    #   3 Non-Biological Mother
+    #   4 Biological Father
+    #   5 Non-Biological Father
+    #   6 Spouse
+    #   7 Partner/Significant Other
+    #   8 Child
+    #   9 Sibling
+    #   10  Grandparent
+    #   11  Other relative
+    #   12  Friend
+    #   13  Neighbor
+    #   14  Co-Worker
+    #   15  Care-giver
+    #   16  Teacher
+    #   17  Primary health care provider
+    #   18  Other health care provider
+    #   -5  Other
+    def contact_to_person_relationship(value)
+      # TODO: FIXME: Determine how to handle Mother/Father value
+      case value
+      when 1  # Mother/Father
+        2       # Default to Biological Mother for now 
+      when 2  # Brother/Sister
+        9       # Sibling
+      when 3  # Aunt/Uncle
+        11      # Other relative
+      when 4  # Grandparent
+        10      # Grandparent
+      when 5  # Neighbor
+        13      # Neighbor
+      when 6  # Friend
+        12      # Friend
+      when -5, -4
+        value   # Other, Missing in Error
+      else 
+        nil     # No mapping value
+      end
     end
     
   end
--- spec/models/pre_pregnancy_operational_data_extractor_spec.rb
+++ spec/models/pre_pregnancy_operational_data_extractor_spec.rb
@@ -4,11 +4,14 @@ describe PrePregnancyOperationalDataExtractor do
 
   before(:each) do
     create_missing_in_error_ncs_codes(Instrument)
+    create_missing_in_error_ncs_codes(Person)
     create_missing_in_error_ncs_codes(Participant)
     create_missing_in_error_ncs_codes(PpgDetail)
     create_missing_in_error_ncs_codes(PpgStatusHistory)
     create_missing_in_error_ncs_codes(Telephone)
     create_missing_in_error_ncs_codes(Email)
+    create_missing_in_error_ncs_codes(Address)
+    create_missing_in_error_ncs_codes(DwellingUnit)
   end
   
   it "extracts person operational data from the survey responses" do
@@ -124,6 +127,236 @@ describe PrePregnancyOperationalDataExtractor do
     person.emails.first.email.should == "email@dev.null"
   end
   
+  context "extracting contact information from the survey responses" do
+    
+    before(:each) do
+      @person = Factory(:person)
+      @participant = Factory(:participant, :person => @person)
+      @ppl = Factory(:participant_person_link, :participant => @participant, :person => @person)
+      Factory(:ppg_detail, :participant => @participant)
+
+      # CONTACT_RELATIONSHIP_CL2
+      Factory(:ncs_code, :list_name => "CONTACT_RELATIONSHIP_CL2", :display_text => "Mother/Father", :local_code => 1)
+      Factory(:ncs_code, :list_name => "CONTACT_RELATIONSHIP_CL2", :display_text => "Brother/Sister", :local_code => 2)
+      @contact_aunt_uncle = Factory(:ncs_code, :list_name => "CONTACT_RELATIONSHIP_CL2", :display_text => "Aunt/Uncle", :local_code => 3)
+      @contact_grandparent = Factory(:ncs_code, :list_name => "CONTACT_RELATIONSHIP_CL2", :display_text => "Grandparent", :local_code => 4)
+      @contact_neighbor = Factory(:ncs_code, :list_name => "CONTACT_RELATIONSHIP_CL2", :display_text => "Neighbor", :local_code => 5)
+      @contact_friend = Factory(:ncs_code, :list_name => "CONTACT_RELATIONSHIP_CL2", :display_text => "Friend", :local_code => 6)
+      Factory(:ncs_code, :list_name => "CONTACT_RELATIONSHIP_CL2", :display_text => "Other", :local_code => -5)
+
+      # PERSON_PARTCPNT_RELTNSHP_CL1
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Participant/Self", :local_code => 1)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Biological Mother", :local_code => 2)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Non-Biological Mother", :local_code => 3)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Biological Father", :local_code => 4)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Non-Biological Father", :local_code => 5)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Spouse", :local_code => 6)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Partner/Significant Other", :local_code => 7)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Child", :local_code => 8)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Sibling", :local_code => 9)
+      @ppr_grandparent = Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Grandparent", :local_code => 10)
+      @ppr_other_rel = Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Other relative", :local_code => 11)
+      @ppr_friend = Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Friend", :local_code => 12)
+      @ppr_neighbor = Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Neighbor", :local_code => 13)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Co-Worker", :local_code => 14)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Care-giver", :local_code => 15)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Teacher", :local_code => 16)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Primary health care provider", :local_code => 17)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Other health care provider", :local_code => 18)
+      Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Other", :local_code => -5)
+
+      @survey = create_pre_pregnancy_survey_with_contact_operational_data
+      @survey_section = @survey.sections.first
+      @response_set = @person.start_instrument(@survey)
+
+      @response_set.responses.size.should == 0
+      @participant.person_relations.size.should == 1
+    end
+    
+    it "creates a new person record and associates it with the particpant" do
+      state = Factory(:ncs_code, :list_name => "STATE_CL1", :display_text => "IL", :local_code => 14)
+      
+      @survey_section.questions.each do |q|
+        case q.data_export_identifier
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_FNAME_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "Donna", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_LNAME_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "Noble", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_RELATE_1"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "#{@contact_friend.local_code}" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_ADDR_1_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "123 Easy St.", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_ADDR_2_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_UNIT_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_CITY_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "Chicago", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_STATE_1"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "#{state.local_code}" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_ZIP_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "65432", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_ZIP4_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "1234", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_PHONE_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "3125551212", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        end
+      end
+
+      @response_set.responses.reload
+      @response_set.responses.size.should == 11
+
+      PrePregnancyOperationalDataExtractor.extract_data(@response_set)
+
+      person  = Person.find(@person.id)
+      participant = person.participant
+      @participant.person_relations.size.should == 2
+      @participant.friends.size.should == 1
+      friend = @participant.friends.first
+      friend.first_name.should == "Donna"
+      friend.last_name.should == "Noble"
+      friend.telephones.first.should_not be_nil
+      friend.telephones.first.phone_nbr.should == "3125551212"
+      
+      friend.addresses.first.should_not be_nil
+      friend.addresses.first.to_s.should == "123 Easy St. Chicago IL 65432-1234"
+    end
+    
+    it "creates another new person record and associates it with the particpant" do
+      state = Factory(:ncs_code, :list_name => "STATE_CL1", :display_text => "IL", :local_code => 14)
+      
+      @survey_section.questions.each do |q|
+        case q.data_export_identifier
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_FNAME_2"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "Carole", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_LNAME_2"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "King", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_RELATE_2"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "#{@contact_neighbor.local_code}" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_ADDR_1_2"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "123 Tapestry St.", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_ADDR_2_2"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_UNIT_2"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_CITY_2"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "Chicago", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_STATE_2"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "#{state.local_code}" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_ZIP_2"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "65432", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.C_ZIP4_2"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "1234", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_PHONE_2"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "3125551212", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        end
+      end
+
+      @response_set.responses.reload
+      @response_set.responses.size.should == 11
+
+      PrePregnancyOperationalDataExtractor.extract_data(@response_set)
+
+      person  = Person.find(@person.id)
+      participant = person.participant
+      @participant.person_relations.size.should == 2
+      @participant.neighbors.size.should == 1
+      neighbor = @participant.neighbors.first
+      neighbor.first_name.should == "Carole"
+      neighbor.last_name.should == "King"
+      neighbor.telephones.first.should_not be_nil
+      neighbor.telephones.first.phone_nbr.should == "3125551212"
+      
+      neighbor.addresses.first.should_not be_nil
+      neighbor.addresses.first.to_s.should == "123 Tapestry St. Chicago IL 65432-1234"
+    end
+    
+    it "creates an other relative person record and associates it with the particpant" do
+      state = Factory(:ncs_code, :list_name => "STATE_CL1", :display_text => "IL", :local_code => 14)
+      
+      @survey_section.questions.each do |q|
+        case q.data_export_identifier
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_FNAME_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "Ivy", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_LNAME_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "Anderson", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_RELATE_1"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "#{@contact_aunt_uncle.local_code}" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        end
+      end
+
+      @response_set.responses.reload
+      @response_set.responses.size.should == 3
+
+      PrePregnancyOperationalDataExtractor.extract_data(@response_set)
+
+      person  = Person.find(@person.id)
+      participant = person.participant
+      @participant.person_relations.size.should == 2
+      @participant.other_relatives.size.should == 1
+      aunt = @participant.other_relatives.first
+      aunt.first_name.should == "Ivy"
+      aunt.last_name.should == "Anderson"      
+    end
+
+    it "creates a grandparent person record and associates it with the particpant" do
+      state = Factory(:ncs_code, :list_name => "STATE_CL1", :display_text => "IL", :local_code => 14)
+      
+      @survey_section.questions.each do |q|
+        case q.data_export_identifier
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_FNAME_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "Billie", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_LNAME_1"
+          answer = q.answers.select { |a| a.response_class == "string" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :string_value => "Holiday", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{PrePregnancyOperationalDataExtractor::INTERVIEW_PREFIX}.CONTACT_RELATE_1"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "#{@contact_grandparent.local_code}" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        end
+      end
+
+      @response_set.responses.reload
+      @response_set.responses.size.should == 3
+
+      PrePregnancyOperationalDataExtractor.extract_data(@response_set)
+
+      person  = Person.find(@person.id)
+      participant = person.participant
+      @participant.person_relations.size.should == 2
+      @participant.grandparents.size.should == 1
+      mimi = @participant.grandparents.first
+      mimi.first_name.should == "Billie"
+      mimi.last_name.should == "Holiday"      
+    end
+    
+    
+  end
   
   
 end
