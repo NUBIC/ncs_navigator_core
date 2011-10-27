ed3e69bc1cb3a8153f13c82e7cdd91d07c132118
Paul Friedman <p-friedman@northwestern.edu>
2011-10-27 15:34:26 -0500

    li pregnotpreg op data extractor

---
+++ app/models/low_intensity_pregnancy_visit_operational_data_extractor.rb
@@ -0,0 +1,56 @@
+class LowIntensityPregnancyVisitOperationalDataExtractor
+  
+  INTERVIEW_PREFIX = "PREG_VISIT_LI_2"
+  
+  BIRTH_ADDRESS_MAP = {
+    "#{INTERVIEW_PREFIX}.B_ADDR_1"            => "address_one",
+    "#{INTERVIEW_PREFIX}.B_ADDR_2"            => "address_two",
+    "#{INTERVIEW_PREFIX}.B_UNIT"              => "unit",
+    "#{INTERVIEW_PREFIX}.B_CITY"              => "city",
+    "#{INTERVIEW_PREFIX}.B_STATE"             => "state_code",
+    "#{INTERVIEW_PREFIX}.B_ZIPCODE"           => "zip",
+  }
+  
+  PPG_STATUS_MAP = {
+    "#{INTERVIEW_PREFIX}.PREGNANT"        => "ppg_status_code",
+    "#{INTERVIEW_PREFIX}.DUE_DATE"        => "orig_due_date",
+    "#{INTERVIEW_PREFIX}.TRYING"          => "ppg_status_code",
+    "#{INTERVIEW_PREFIX}.MED_UNABLE"      => "ppg_status_code"
+  }
+  
+  class << self
+    
+    def extract_data(response_set)
+      person = response_set.person
+      if person.participant.blank?
+        participant = Participant.create(:person => person) 
+      else
+        participant = person.participant
+      end
+      ppg_status_history = PpgStatusHistory.new(:participant => participant)
+
+      response_set.responses.each do |r|
+        value = OperationalDataExtractor.response_value(r)
+
+        data_export_identifier = r.question.data_export_identifier
+
+        # TODO: do not hard code ppg code
+        if PPG_STATUS_MAP.has_key?(data_export_identifier)
+          case data_export_identifier
+          when "#{INTERVIEW_PREFIX}.PREGNANT"
+            ppg_status_history.send("#{PPG_STATUS_MAP[data_export_identifier]}=", value)
+          end
+          
+          if (data_export_identifier == "#{INTERVIEW_PREFIX}.DUE_DATE") && !value.blank?
+            participant.ppg_details.first.update_due_date(value)
+          end
+        end
+
+      end
+
+      ppg_status_history.save! unless ppg_status_history.ppg_status_code.blank?
+      participant.save!
+      person.save!
+    end
+  end
+end
---
+++ spec/models/low_intensity_pregnancy_visit_operational_data_extractor_spec.rb
@@ -0,0 +1,160 @@
+require 'spec_helper'
+
+describe LowIntensityPregnancyVisitOperationalDataExtractor do
+  
+  before(:each) do
+    create_missing_in_error_ncs_codes(Instrument)
+    create_missing_in_error_ncs_codes(Participant)
+    create_missing_in_error_ncs_codes(PpgDetail)
+    create_missing_in_error_ncs_codes(PpgStatusHistory)
+  end
+
+  context "updating the ppg status history" do
+
+    before(:each) do
+      @person = Factory(:person)
+      @participant = Factory(:participant, :person => @person)
+      @ppl = Factory(:participant_person_link, :participant => @participant, :person => @person)
+      Factory(:ppg_detail, :participant => @participant)
+
+      @ppg1 = Factory(:ncs_code, :list_name => "PPG_STATUS_CL1", :display_text => "PPG Group 1", :local_code => 1)
+      @ppg2 = Factory(:ncs_code, :list_name => "PPG_STATUS_CL1", :display_text => "PPG Group 2", :local_code => 2)
+      @ppg3 = Factory(:ncs_code, :list_name => "PPG_STATUS_CL1", :display_text => "PPG Group 3", :local_code => 3)
+      @ppg4 = Factory(:ncs_code, :list_name => "PPG_STATUS_CL1", :display_text => "PPG Group 4", :local_code => 4)
+      @ppg5 = Factory(:ncs_code, :list_name => "PPG_STATUS_CL1", :display_text => "PPG Group 5", :local_code => 5)
+
+      @survey = create_li_pregnancy_screener_survey_with_ppg_status_history_operational_data
+      @survey_section = @survey.sections.first
+      @response_set = @person.start_instrument(@survey)
+
+      @response_set.responses.size.should == 0
+      @participant.ppg_status.local_code.should == 2
+    end
+    
+    it "updates the ppg status to 1 if the person responds that they are pregnant" do
+    
+      @survey_section.questions.each do |q|
+        case q.data_export_identifier
+        when "#{LowIntensityPregnancyVisitOperationalDataExtractor::INTERVIEW_PREFIX}.PREGNANT"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "1" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        when "#{LowIntensityPregnancyVisitOperationalDataExtractor::INTERVIEW_PREFIX}.DUE_DATE"
+          answer = q.answers.select { |a| a.response_class == "date" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :datetime_value => "2011-12-25", :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        end
+      end
+    
+      @response_set.responses.reload
+      @response_set.responses.size.should == 2
+    
+      LowIntensityPregnancyVisitOperationalDataExtractor.extract_data(@response_set)
+    
+      person  = Person.find(@person.id)
+      participant = person.participant
+      participant.ppg_status_histories.size.should == 1
+      participant.ppg_status_histories.first.ppg_status.local_code.should == 1
+      participant.ppg_status.local_code.should == 1
+      participant.due_date.should == "2011-12-25"
+    
+    end
+    
+    it "updates the ppg status to 3 if the person responds that they recently lost their child during pregnancy" do
+    
+      @survey_section.questions.each do |q|
+        case q.data_export_identifier
+        when "#{LowIntensityPregnancyVisitOperationalDataExtractor::INTERVIEW_PREFIX}.PREGNANT"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "3" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        end
+      end
+    
+      @response_set.responses.reload
+      @response_set.responses.size.should == 1
+    
+      LowIntensityPregnancyVisitOperationalDataExtractor.extract_data(@response_set)
+    
+      person  = Person.find(@person.id)
+      participant = person.participant
+      participant.ppg_status_histories.size.should == 1
+      participant.ppg_status_histories.first.ppg_status.local_code.should == 3
+      participant.ppg_status.local_code.should == 3
+      participant.due_date.should be_nil
+    
+    end
+    
+    it "updates the ppg status to 2 if the person responds that they are trying" do
+    
+      @survey_section.questions.each do |q|
+        case q.data_export_identifier
+        when "#{LowIntensityPregnancyVisitOperationalDataExtractor::INTERVIEW_PREFIX}.PREGNANT"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "2" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        end
+      end
+    
+      @response_set.responses.reload
+      @response_set.responses.size.should == 1
+    
+      LowIntensityPregnancyVisitOperationalDataExtractor.extract_data(@response_set)
+    
+      person  = Person.find(@person.id)
+      participant = person.participant
+      participant.ppg_status_histories.size.should == 1
+      participant.ppg_status_histories.first.ppg_status.local_code.should == 2
+      participant.ppg_status.local_code.should == 2
+      participant.due_date.should be_nil
+    
+    end
+
+    it "updates the ppg status to 4 if the person responds that they recently gave birth" do
+    
+      @survey_section.questions.each do |q|
+        case q.data_export_identifier
+        when "#{LowIntensityPregnancyVisitOperationalDataExtractor::INTERVIEW_PREFIX}.PREGNANT"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "4" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        end
+      end
+    
+      @response_set.responses.reload
+      @response_set.responses.size.should == 1
+    
+      LowIntensityPregnancyVisitOperationalDataExtractor.extract_data(@response_set)
+    
+      person  = Person.find(@person.id)
+      participant = person.participant
+      participant.ppg_status_histories.size.should == 1
+      participant.ppg_status_histories.first.ppg_status.local_code.should == 4
+      participant.ppg_status.local_code.should == 4
+      participant.due_date.should be_nil
+    
+    end
+    
+    it "updates the ppg status to 5 if the person responds that they are medically unable to become pregnant" do
+    
+      @survey_section.questions.each do |q|
+        case q.data_export_identifier
+        when "#{LowIntensityPregnancyVisitOperationalDataExtractor::INTERVIEW_PREFIX}.PREGNANT"
+          answer = q.answers.select { |a| a.response_class == "answer" && a.reference_identifier == "5" }.first
+          Factory(:response, :survey_section_id => @survey_section.id, :question_id => q.id, :answer_id => answer.id, :response_set_id => @response_set.id)
+        end
+      end
+    
+      @response_set.responses.reload
+      @response_set.responses.size.should == 1
+    
+      LowIntensityPregnancyVisitOperationalDataExtractor.extract_data(@response_set)
+    
+      person  = Person.find(@person.id)
+      participant = person.participant
+      participant.ppg_status_histories.size.should == 1
+      participant.ppg_status_histories.first.ppg_status.local_code.should == 5
+      participant.ppg_status.local_code.should == 5
+      participant.due_date.should be_nil
+    
+    end
+
+  end
+  
+  
+end
