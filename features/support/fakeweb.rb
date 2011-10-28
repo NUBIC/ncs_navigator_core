766745a3fa90aa5e8a9a8a07e7972dd9b42005c3
Rhett Sutphin <rhett@detailedbalance.net>
2011-10-28 11:05:07 -0500

    Ensure features will run independent of system config.

--- features/support/fakeweb.rb
+++ features/support/fakeweb.rb
@@ -1,24 +1,24 @@
 FakeWeb.allow_net_connect = %r[^https?://(localhost|127.0.0.1)]
 
-FakeWeb.register_uri(:get, /psc\/api\/v1\/subjects\/registered_with_psc$/, 
+FakeWeb.register_uri(:get, /\/api\/v1\/subjects\/registered_with_psc$/, 
                      :body => "#{Rails.root}/features/fixtures/fakeweb/registered_with_psc.json", :status => ["200", "OK"], :content_type => "application/json")
 
-FakeWeb.register_uri(:get, /psc\/api\/v1\/subjects\/registered_with_psc\/schedules.json$/, 
+FakeWeb.register_uri(:get, /\/api\/v1\/subjects\/registered_with_psc\/schedules.json$/, 
                     :body => "#{Rails.root}/features/fixtures/fakeweb/registered_with_psc_schedule.json", :status => ["200", "OK"], :content_type => "application/json")
 
-FakeWeb.register_uri(:get, /psc\/api\/v1\/subjects\/((?!registered_with_psc))/, :body => "Unknown", :status => ["401", "Unknown"])
+FakeWeb.register_uri(:get, /\/api\/v1\/subjects\/((?!registered_with_psc))/, :body => "Unknown", :status => ["401", "Unknown"])
 
-FakeWeb.register_uri(:get, /\/psc\/api\/v1\/studies.json$/,
+FakeWeb.register_uri(:get, /\/api\/v1\/studies.json$/,
                      :body => "#{Rails.root}/features/fixtures/fakeweb/studies.json", :content_type => "application/json")
 
-FakeWeb.register_uri(:get, /\/psc\/api\/v1\/sites/, 
+FakeWeb.register_uri(:get, /\/api\/v1\/sites/, 
                      :body => "#{Rails.root}/features/fixtures/fakeweb/sites.xml", :content_type => "text/xml")
 
-FakeWeb.register_uri(:get, /\/psc\/api\/v1\/studies\/(.*)\/template\/current.xml$/,
+FakeWeb.register_uri(:get, /\/api\/v1\/studies\/(.*)\/template\/current.xml$/,
                      :body => "#{Rails.root}/features/fixtures/fakeweb/template.xml", :content_type => "text/xml")
 
-FakeWeb.register_uri(:post, /\/psc\/api\/v1\/studies\/(.*)\/sites\/(.*)\/subject-assignments$/,
+FakeWeb.register_uri(:post, /\/api\/v1\/studies\/(.*)\/sites\/(.*)\/subject-assignments$/,
                     :body => "", :status => ["201", "Created"])
 
-FakeWeb.register_uri(:get, /\/psc\/api\/v1\/reports\/scheduled-activities.json/,
+FakeWeb.register_uri(:get, /\/api\/v1\/reports\/scheduled-activities.json/,
                     :body => "#{Rails.root}/features/fixtures/fakeweb/scheduled_activities.json", :status => ["200", "OK"])
