8e5dc18b6553c0536320fad9cb69fa771b02cd8c
Rhett Sutphin <rhett@detailedbalance.net>
2011-11-03 23:24:58 -0500

    Warehouse transformer now handles all fields in Person.
    The mapping is handled by the new `model_row` helper in the Database
    transformer mixin (provided by MDES Warehouse).

--- lib/ncs_navigator/warehouse/transformers/navigator_core.rb
+++ lib/ncs_navigator/warehouse/transformers/navigator_core.rb
@@ -4,23 +4,27 @@ require 'ncs_navigator/warehouse/models/two_point_zero'
 module NcsNavigator::Warehouse::Transformers
   class NavigatorCore
     include Database
-
     include NcsNavigator::Warehouse::Models::TwoPointZero
 
     bcdatabase :name => 'ncs_navigator_core'
 
-    def self.direct_map(row, model)
-      model.properties.inject({}) do |m, a|
-        if row.respond_to?(a.name)
-          m[a] = row.send(a.name)
-        end
-        m
-      end
-    end
+    on_unused_columns :fail
+    ignored_columns :id, :transaction_type, :updated_at, :created_at
 
     produce_records :people do |row|
-      Person.new(
-        direct_map(row, Person)
+      model_row(Person, row,
+        :column_map => {
+          :language_code => :person_lang,
+          :language_other => :person_lang_oth,
+          :marital_status_code => :maristat,
+          :marital_status_other => :maristat_oth,
+          :preferred_contact_method_code => :pref_contact,
+          :preferred_contact_method_other => :pref_contact_oth,
+          :planned_move_code => :plan_move
+        },
+        :ignored_or_used => %w(
+           person_dob_date date_move_date being_processed
+        )
       )
     end
   end
