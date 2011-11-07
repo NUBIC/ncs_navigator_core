fad335827a8a831955455b63028fefc1712d4b7a
Paul Friedman <p-friedman@northwestern.edu>
2011-11-07 13:14:27 -0600

    update configuration for assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -23,6 +23,12 @@ NcsNavigatorCore::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
   
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+  
   config.aker do
     static = Aker::Authorities::Static.from_file("/etc/nubic/ncs/staff_portal_users.yml")
     authorities :cas, static
