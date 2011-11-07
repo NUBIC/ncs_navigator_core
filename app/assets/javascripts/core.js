7b2dd0ce8ee05efbc93a281ec0ec7a293fcfdd0f
Paul Friedman <p-friedman@northwestern.edu>
2011-11-07 15:41:22 -0600

    more attempts at asset pipeline and precompilation

---
+++ app/assets/javascripts/core.js
@@ -0,0 +1,75 @@
+NCSCore = {};
+NCSCore.UI = {};
+
+// Used inside document ready method call to wire up selects with other fields
+function wire_up_select_other(select_id, other_id) {
+  check_select_for_other(select_id, other_id);
+  // Check on item change
+  $(select_id).change(function() {
+    check_select_for_other(select_id, other_id);
+  });
+}
+
+// Used to enable/disable 'other' input type text field
+function check_select_for_other(select_id, other_id) {
+  var s = $(select_id+" option:selected");
+  var o = $(other_id);
+
+  // making sure the object id's given above exist on page
+  if ( (o.size() > 0) && (s.size() > 0) ) {
+    // 'Other' = -5
+    if (s.val() == "-5") {
+      o.removeAttr('disabled');
+      // to make the change more visible
+      o.css('background-color', '#FAFAD2'); 
+      o.css('border', '2px solid #808080'); 
+    } else {
+      // clear the other field
+      o.val(''); 
+      o.attr('disabled', 'disabled');
+      // to make disabled more visible
+      o.css('border', '1px solid #ccc'); 
+      o.css('background-color', '#ccc'); 
+    }
+  }
+}
+
+function stop_default_action(e) {
+  if (e &&e.preventDefault) {
+    // to disable the default event we call the preventDefault() method of the event handler 
+    // (for those browsers that recognise standard event listeners)
+    e.preventDefault();
+  } else if (window.event && window.event.returnValue) {
+    // we set the eventReturnValue property to false for Internet Explorer 
+    // (which uses its own proprietary means of attaching events)
+    window.eventReturnValue = false;
+  }
+}
+
+$(document).ready(function() {
+  $('input.datetime').datetimepicker({
+  	showSecond: true,
+  	showMillisec: false,
+  	timeFormat: 'hh:mm:ss'
+  });
+  $('input.date').datepicker( { dateFormat: 'yy-mm-dd' } );
+  $('input.datepicker').datepicker( { dateFormat: 'yy-mm-dd' } );
+  $('input.time').timepicker({});
+
+  $('.mdes_documentation_link').click(function(event) {
+    var definition = $('#mdes_definition').val();
+    var title = $('#mdes_definition').attr('title');
+    $('<div id="dialog">' + definition + '</div>').appendTo('body');
+      event.preventDefault();
+      $("#dialog").dialog({
+        title: title,
+        width: 600,
+        modal: true,
+        close: function(event, ui) {
+          $("#dialog").remove();
+        }
+      });
+  });
+
+});
+
