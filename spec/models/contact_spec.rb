a4503e87f3892a655a4dc489070d4b8551595c3b
Paul Friedman <p-friedman@northwestern.edu>
2011-11-10 12:10:46 -0600

    contact knows about associated contact links and instruments show links for only instruments that are not completed

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -46,6 +46,8 @@ describe Contact do
   it { should belong_to(:contact_private) }
   it { should belong_to(:who_contacted) }  
   
+  it { should have_many(:contact_links) }
+  
   it "knows when it is 'closed'" do
     c = Factory(:contact)
     c.should_not be_closed
@@ -79,6 +81,46 @@ describe Contact do
       obj.contact_private.local_code.should == -4
       obj.who_contacted.local_code.should == -4
     end
+  end
+  
+  context "contact links and instruments" do
+    
+    before(:each) do
+      create_missing_in_error_ncs_codes(Instrument)
+    end
+    
+    it "knows all contact links associated with this contact" do
+      
+      c  = Factory(:contact)
+      l1 = Factory(:contact_link, :contact => c)
+      
+      c.contact_links.should == [l1]
+      
+      l2 = Factory(:contact_link, :contact => c)
+      
+      c.contact_links.reload
+      c.contact_links.should == [l1, l2]
+      
+    end
+    
+    it "knows all instruments associated with this contact" do
+      c  = Factory(:contact)
+      pers = Factory(:person)
+      rs, i1 = pers.start_instrument(create_li_pregnancy_screener_survey_with_ppg_status_history_operational_data)
+      l1 = Factory(:contact_link, :contact => c, :instrument => i1, :person => pers)
+      
+      c.contact_links.should == [l1]
+      c.instruments.should == [i1]
+      
+      rs, i2 = pers.start_instrument(create_pre_pregnancy_survey_with_email_operational_data)
+      l2 = Factory(:contact_link, :contact => c, :instrument => i2, :person => pers)
+      
+      c.contact_links.reload
+      c.instruments.reload
+      c.contact_links.should == [l1, l2]
+      c.instruments.should == [i1, i2]
+    end
+    
   end
   
 end
