44c2f367bdfd6336ea1554465ae8c05f5ba6db5b
Paul Friedman <p-friedman@northwestern.edu>
2011-11-11 10:01:00 -0600

    due_date returns Date not String

--- spec/models/pregnancy_screener_operational_data_extractor_spec.rb
+++ spec/models/pregnancy_screener_operational_data_extractor_spec.rb
@@ -334,7 +334,7 @@ describe PregnancyScreenerOperationalDataExtractor do
     participant.ppg_details.size.should == 1
     participant.ppg_details.first.ppg_first.local_code.should == 1
     participant.ppg_status.local_code.should == 1
-    participant.due_date.should == "2011-12-25"
+    participant.due_date.should == Date.parse("2011-12-25")
     
   end
 
