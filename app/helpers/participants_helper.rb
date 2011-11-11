6687cdfbf25513c6f740a8b3107c996029b8ae9c
Paul Friedman <p-friedman@northwestern.edu>
2011-11-11 10:01:29 -0600

    moved consent into instruments selection list

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -1,4 +1,5 @@
 module ParticipantsHelper
+  include ActionView::Helpers::UrlHelper
   
   def switch_arm_message(participant)
     msg = "Switch from Low Intensity to High Intensity"
@@ -8,6 +9,15 @@ module ParticipantsHelper
   
   def psc_assignment_path(assignment_id)
     "#{NcsNavigator.configuration.psc_uri}pages/subject?assignment=#{assignment_id}"
+  end
+  
+  def determine_participant_consent_path(activity, participant, contact_link)
+    if participant.consented?
+      consent = ParticipantConsent.where(:participant_id => participant.id).where(:contact_id => ContactLink.find(contact_link.id).contact.id).first
+      link_to activity, edit_participant_consent_path(consent, :contact_link_id => contact_link.id), :class => "edit_link icon_link"
+    else
+      link_to activity, new_participant_consent_path(:participant_id => participant.id, :contact_link_id => contact_link.id), :class => "edit_link icon_link"
+    end
   end
   
 end
