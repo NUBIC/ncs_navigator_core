95f6faf0ba6f6a855282c844faca09d0ec32f79f
Rhett Sutphin <rhett@detailedbalance.net>
2011-11-30 17:01:11 -0600

    Whitespace.

--- config/initializers/system_configuration.rb
+++ config/initializers/system_configuration.rb
@@ -3,32 +3,34 @@ require 'ncs_navigator/configuration'
 
 ##
 # Uses the ncs_navigator_configuration gem to pull these values
-# cf. /etc/nubic/ncs/navigator.ini 
+# cf. /etc/nubic/ncs/navigator.ini
 module NcsNavigatorCore
-  
   class << self
-    
     def footer_left_logo_path
-      NcsNavigator.configuration.footer_logo_left.to_s.split("/").last if NcsNavigator.configuration.footer_logo_left
+      if NcsNavigator.configuration.footer_logo_left
+        NcsNavigator.configuration.footer_logo_left.to_s.split("/").last
+      end
     end
-    
+
     def footer_right_logo_path
-      NcsNavigator.configuration.footer_logo_right.to_s.split("/").last if NcsNavigator.configuration.footer_logo_right
+      if NcsNavigator.configuration.footer_logo_right
+        NcsNavigator.configuration.footer_logo_right.to_s.split("/").last
+      end
     end
-    
+
     def psu
       NcsNavigator.configuration.psus.first.id unless NcsNavigator.configuration.psus.blank?
     end
     alias :psu_code :psu
-    
+
     def study_center_name
       NcsNavigator.configuration.core['study_center_name']
     end
-    
+
     def study_center_phone_number
       NcsNavigator.configuration.core['study_center_phone_number']
     end
-    
+
     ##
     # One of the following:
     # PB - Provider Based
@@ -37,11 +39,10 @@ module NcsNavigatorCore
     def recruitment_type
       NcsNavigator.configuration.core['recruitment_type']
     end
-    
+
     def with_specimens
-      NcsNavigator.configuration.core['with_specimens']      
+      NcsNavigator.configuration.core['with_specimens']
     end
-    
   end
 end
 
--- lib/tasks/mdes.rake
+++ lib/tasks/mdes.rake
@@ -1,17 +1,16 @@
 namespace :mdes do
-  
   desc 'loads 2.0 MDES codes from xsd'
   task :load_codes_from_schema => :environment do
     require 'mdes_data_loader'
     MdesDataLoader::load_codes_from_schema('2.0')
   end
-  
+
   desc 'loads 1. MDES codes from xsd'
   task :load_codes_from_schema_12 => :environment do
     require 'mdes_data_loader'
     MdesDataLoader::load_codes_from_schema('1.2')
   end
-  
+
   desc 'loads 2.0 MDES codes from xsd'
   task :load_codes_from_schema_20 => :environment do
     require 'mdes_data_loader'
@@ -19,8 +18,7 @@ namespace :mdes do
   end
 
   desc 'outputs the count of ncs data elements'
-  task :count_codes => :environment do 
+  task :count_codes => :environment do
     puts "There are #{NcsCode.count} codes"
   end
-
 end
