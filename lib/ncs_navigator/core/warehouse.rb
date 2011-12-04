4f0a63ad979d0fb1b45519a6a24152b37029c782
Rhett Sutphin <rhett@detailedbalance.net>
2011-12-04 01:16:20 -0600

    Enumerable for creating all surveyor-backed instrument records. Closes #1646.

--- lib/ncs_navigator/core/warehouse.rb
+++ lib/ncs_navigator/core/warehouse.rb
@@ -2,7 +2,11 @@ require 'ncs_navigator/core'
 require 'ncs_navigator/warehouse'
 
 module NcsNavigator::Core::Warehouse
-  autoload :Enumerator,        'ncs_navigator/core/warehouse/enumerator'
-  autoload :EnumeratorHelpers, 'ncs_navigator/core/warehouse/enumerator_helpers'
-  autoload :Importer,          'ncs_navigator/core/warehouse/importer'
+  autoload :Enumerator,           'ncs_navigator/core/warehouse/enumerator'
+  autoload :EnumeratorHelpers,    'ncs_navigator/core/warehouse/enumerator_helpers'
+  autoload :Importer,             'ncs_navigator/core/warehouse/importer'
+  autoload :InstrumentEnumerator, 'ncs_navigator/core/warehouse/instrument_enumerator'
+
+  # ResponseSetToWarehouse is not autoloaded because it needs to be
+  # explicitly required.
 end
