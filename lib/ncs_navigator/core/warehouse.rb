6f8b3aec4f67453e16c215d310728ca69d00c07b
Rhett Sutphin <rhett@detailedbalance.net>
2011-12-04 21:17:52 -0600

    Add unused instrument XML passthrough. #1645.
    This also includes an update to ncs_navigator_configuration 0.2.0, which
    adds recruitment type as a mandatory field. (This commit doesn't require this,
    but it requires a newer version of the warehouse gem which does.)

--- lib/ncs_navigator/core/warehouse.rb
+++ lib/ncs_navigator/core/warehouse.rb
@@ -2,10 +2,11 @@ require 'ncs_navigator/core'
 require 'ncs_navigator/warehouse'
 
 module NcsNavigator::Core::Warehouse
-  autoload :DatabaseEnumeratorHelpers, 'ncs_navigator/core/warehouse/database_enumerator_helpers'
-  autoload :InstrumentEnumerator,      'ncs_navigator/core/warehouse/instrument_enumerator'
-  autoload :OperationalEnumerator,     'ncs_navigator/core/warehouse/operational_enumerator'
-  autoload :OperationalImporter,       'ncs_navigator/core/warehouse/operational_importer'
+  autoload :DatabaseEnumeratorHelpers,   'ncs_navigator/core/warehouse/database_enumerator_helpers'
+  autoload :InstrumentEnumerator,        'ncs_navigator/core/warehouse/instrument_enumerator'
+  autoload :OperationalEnumerator,       'ncs_navigator/core/warehouse/operational_enumerator'
+  autoload :OperationalImporter,         'ncs_navigator/core/warehouse/operational_importer'
+  autoload :UnusedInstrumentPassthrough, 'ncs_navigator/core/warehouse/unused_instrument_passthrough'
 
   # ResponseSetToWarehouse is not autoloaded because it needs to be
   # explicitly required.
