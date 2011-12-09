84e2a1d7c3e2b16333d4656bdcc51dcec5c753cc
Rhett Sutphin <rhett@detailedbalance.net>
2011-12-08 19:31:12 -0600

    Don't rebuild the warehouse schema on each test run. Done with #1642.
    The warehouse schema doesn't change often enough to justify the 20-30 seconds
    this takes. Instead, there's now a rake task (db:test:prepare:warehouse) that
    you will need to run every time the ncs_mdes_warehouse gem is updated.

--- spec/lib/ncs_navigator/core/warehouse/importer_warehouse_setup.rb
+++ spec/lib/ncs_navigator/core/warehouse/importer_warehouse_setup.rb
@@ -1,43 +1,17 @@
 require 'spec_helper'
+require Rails.root + 'spec/warehouse_setup'
 
 shared_context :importer_spec_warehouse do
+  include NcsNavigator::Core::Spec::WarehouseSetup
+
   before(:all) do
     wh_init.set_up_repository(:both)
-    wh_init.replace_schema
     DatabaseCleaner[:data_mapper].strategy = :transaction
   end
 
-  # This is not a `let` due to https://github.com/rspec/rspec-core/issues/500
-  def wh_config
-    @wh_config ||= NcsNavigator::Warehouse::Configuration.new.tap do |config|
-      config.log_file = Rails.root + 'log/wh-import_test.log'
-      config.set_up_logs
-      config.output_level = :quiet
-      if bcdatabase_config[:group]
-        config.bcdatabase_group = bcdatabase_config[:group]
-      end
-      config.bcdatabase_entries.merge!(
-        # these are deliberately the same since replace_schema only
-        # works on the working database, while the importer runs
-        # against the reporting database.
-        :working   => :ncs_navigator_core_test_mdes_warehouse,
-        :reporting => :ncs_navigator_core_test_mdes_warehouse
-        )
-    end
-  end
-
-  # This is not a `let` due to https://github.com/rspec/rspec-core/issues/500
-  def wh_init
-    @wh_init ||= NcsNavigator::Warehouse::DatabaseInitializer.new(wh_config)
-  end
-
-  # This is not a `let` due to https://github.com/rspec/rspec-core/issues/500
-  def bcdatabase_config
-    @bcdatabase_config ||=
-      if Rails.env == 'ci'
-        { :group => :public_ci_postgresql9 }
-      else
-        { :name => :ncs_navigator_core_test }
-      end
+  def all_missing_attributes(model)
+    model.properties.
+      select { |p| p.required? }.
+      inject({}) { |h, prop| h[prop.name] = '-4'; h }.merge(:psu_id => '20000030')
   end
 end
