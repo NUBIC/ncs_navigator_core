a84c561c61c8900a0f1ba47ce98bf022f77957a1
Rhett Sutphin <rhett@detailedbalance.net>
2011-12-12 14:45:06 -0600

    Update plugin test infra. Done with #1680.
    * Use bcdatabase for database config
    * Public IDs are strings now
    * date_fields tested behavior out of sync with actual behavior
    * Ignore logs
    * Some whitespace fixes

--- vendor/plugins/mdes_record/test/schema.rb
+++ vendor/plugins/mdes_record/test/schema.rb
@@ -2,16 +2,16 @@
 ActiveRecord::Schema.define(:version => 0) do
   create_table :foos, :force => true do |t|
     t.string :name
-    t.binary :uuid
+    t.string :uuid
   end
   create_table :bars, :force => true do |t|
     t.string :name
-    t.binary :bar_id
+    t.string :bar_id
   end
   create_table :date_foos, :force => true do |t|
     t.string :name
-    t.binary :uuid
-    t.date :start_date
-    t.string :start
+    t.string :uuid
+    t.date :start_date_date
+    t.string :start_date
   end
 end
