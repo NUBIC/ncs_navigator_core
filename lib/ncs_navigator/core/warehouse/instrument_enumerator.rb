f186ed36f648e20d20066d73b2a6086ea6c29179
Rhett Sutphin <rhett@detailedbalance.net>
2011-12-16 01:52:24 -0600

    Configure the instrument transformer to use a subprocess. #1646.
    Since the enumerator is very dependent on the Surveyor API, and the Surveyor
    API won't work outside of a full-up Rails app, the instrument enumerator
    needs to run with the core rails app environment loaded. Doing that within
    the ETL ruby process directly would be a huge mess, so do it in a separate
    process using `rails runner`.

--- lib/ncs_navigator/core/warehouse/instrument_enumerator.rb
+++ lib/ncs_navigator/core/warehouse/instrument_enumerator.rb
@@ -1,23 +1,94 @@
 require 'ncs_navigator/core/warehouse'
-
 require 'ncs_navigator/core/warehouse/response_set_to_warehouse'
+require 'forwardable'
 
 module NcsNavigator::Core::Warehouse
   ##
   # Incrementally builds and yields MDES records for every ResponseSet
   # in the system.
   class InstrumentEnumerator
+    extend Forwardable
     include Enumerable
 
     def self.create_transformer(wh_config)
-      NcsNavigator::Warehouse::Transformers::EnumTransformer.new(wh_config, new)
+      NcsNavigator::Warehouse::Transformers::SubprocessTransformer.new(
+        wh_config,
+        %w(bundle exec rails runner script/instrument_transformer) +
+          [wh_config.configuration_file.to_s],
+        :directory => File.expand_path('../../../../..', __FILE__)
+      )
+    end
+
+    def initialize(wh_config)
+      @wh_config = wh_config
     end
 
     def each
+      progress = ProgressTracker.new(@wh_config)
       ResponseSet.find_each do |rs|
+        progress.increment_response_sets
+        progress.increment_responses(rs.responses.size)
         rs.to_mdes_warehouse_records.each do |record|
+          progress.increment_records
           yield record
         end
+      end
+      progress.complete
+    end
+
+    # @private
+    class ProgressTracker
+      extend Forwardable
+
+      def_delegators :@wh_config, :log, :shell
+
+      def initialize(wh_config)
+        @wh_config = wh_config
+        @response_set_count = 0
+        @response_count = 0
+        @record_count = 0
+        @start = Time.now
+        log.info "Transforming survey responses into MDES records."
+      end
+
+      def increment_records
+        @record_count += 1
+        say_progress
+      end
+
+      def increment_response_sets
+        @response_set_count += 1
+        say_progress
+      end
+
+      def increment_responses(ct)
+        @response_set_count += ct
+        say_progress
+      end
+
+      def say_progress
+        shell.clear_line_then_say(
+          "Transforming surveys. %3d set(s), %3d resp => %3d record(s). %.1f/s." % [
+            @response_set_count, @response_count, @record_count, rate
+          ])
+      end
+
+      def complete
+        msg = "Transformed %d set(s), %d resp into %d record(s) in %ds (%.1f/s)." % [
+          @response_set_count, @response_count, @record_count, elapsed, rate
+        ]
+        log.info msg
+        shell.clear_line_then_say msg + "\n"
+      end
+
+      private
+
+      def rate
+        @record_count / elapsed
+      end
+
+      def elapsed
+        Time.now - @start
       end
     end
   end
--- spec/lib/ncs_navigator/core/warehouse/instrument_enumerator_spec.rb
+++ spec/lib/ncs_navigator/core/warehouse/instrument_enumerator_spec.rb
@@ -2,26 +2,54 @@ require 'spec_helper'
 
 module NcsNavigator::Core::Warehouse
   describe InstrumentEnumerator do
+    let(:configuration) {
+      NcsNavigator::Warehouse::Configuration.new.tap { |c|
+        c.configuration_file = '/foo/b.rb'
+        c.output_level = :quiet
+        c.log_file = Rails.root + 'log/wh.log'
+      }
+    }
+
     it 'is enumerable' do
       InstrumentEnumerator.ancestors.should include(Enumerable)
     end
 
     describe '.create_transformer' do
+      subject { InstrumentEnumerator.create_transformer(configuration) }
+
       it 'creates a transformer' do
-        InstrumentEnumerator.create_transformer(nil).should respond_to(:transform)
+        subject.should respond_to(:transform)
+      end
+
+      describe 'the transformer' do
+        it 'executes the wrapper script with rails r' do
+          subject.exec_and_args.should == [
+            'bundle', 'exec', 'rails', 'runner', 'script/instrument_transformer',
+            '/foo/b.rb'
+          ]
+        end
+
+        it 'sets the directory to the rails root' do
+          subject.directory.to_s.should == Rails.root.to_s
+        end
       end
     end
 
     describe '#each' do
+      subject { InstrumentEnumerator.new(configuration) }
+
       it 'converts every response set in turn' do
         rs1 = mock(ResponseSet)
         rs1.should_receive(:to_mdes_warehouse_records).and_return(%w(A B C))
         rs2 = mock(ResponseSet)
         rs2.should_receive(:to_mdes_warehouse_records).and_return(%w(H4 H7))
+        [rs1, rs2].each do |m|
+          m.stub(:responses).and_return([])
+        end
 
         ResponseSet.should_receive(:find_each).and_yield(rs1).and_yield(rs2)
 
-        InstrumentEnumerator.new.to_a.should == %w(A B C H4 H7)
+        subject.to_a.should == %w(A B C H4 H7)
       end
     end
   end
