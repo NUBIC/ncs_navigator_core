579b280512d16f8be48841f309c51fb39455a354
Paul Friedman <p-friedman@northwestern.edu>
2011-12-29 12:43:09 -0600

    renamed method to better express intent

--- app/controllers/surveyor_controller.rb
+++ app/controllers/surveyor_controller.rb
@@ -5,7 +5,7 @@ class SurveyorController < ApplicationController
   def surveyor_finish
     OperationalDataExtractor.process(@response_set)
     update_participant_based_on_survey(@response_set)
-    psc.mark_activity_for_instrument(InstrumentEventMap.activity_for_instrument(@response_set.survey.title), @response_set.person.participant, PatientStudyCalendar::ACTIVITY_OCCURRED) if @response_set.survey
+    psc.update_activity_state(InstrumentEventMap.name_of_instrument(@response_set.survey.title), @response_set.person.participant, PatientStudyCalendar::ACTIVITY_OCCURRED) if @response_set.survey
     contact_link = ContactLink.where(:instrument_id => @response_set.instrument_id).first
     edit_instrument_contact_link_path(contact_link.id)
   end
