2a058ff5fd6375728e951b3a5f6a1c2e609c16b7
Paul Friedman <p-friedman@northwestern.edu>
2012-01-04 14:59:53 -0600

    whitespace

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,20 +1,20 @@
 class WelcomeController < ApplicationController
-  
+
   def index
     @scheduled_activities = psc.scheduled_activities_report({ :current_user => current_staff, :end_date => 5.months.from_now.to_date.to_s })
   end
-  
+
   def summary
     @dwellings    = DwellingUnit.next_to_process
     @participants = Participant.all
   end
-  
+
   def start_pregnancy_screener_instrument
     person = Person.create(:psu_code => @psu_code)
     participant = Participant.create(:psu_code => @psu_code)
     participant.person = person
     participant.save!
-    
+
     resp = psc.assign_subject(participant)
     if resp && resp.status.to_i < 299
       redirect_to new_person_contact_path(person)
@@ -25,7 +25,7 @@ class WelcomeController < ApplicationController
       flash[:warning] = error_msg
       redirect_to :controller => "welcome", :action => "summary"
     end
-    
+
   end
-  
+
 end
