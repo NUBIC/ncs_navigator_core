6f0d7a46f78e0b542afcf8ea791c8577aa53526f
Paul Friedman <p-friedman@northwestern.edu>
2012-01-26 17:34:15 -0600

    Case Status Report Task #1763

---
+++ lib/reporting/case_status_report.rb
@@ -0,0 +1,176 @@
+require 'fastercsv'
+
+module Reporting
+  class CaseStatusReport
+
+    REPORT_HEADERS = [
+      "Participant ID",
+      "First Name",
+      "Last Name",
+      "Next Event Date",
+      "Event Name",
+      "Last Contact Date",
+      "Last Contact Start Time",
+      "Last Contact End Time",
+      "PPG Status",
+      "Language",
+      "Last Contact Disposition",
+      "Phone Number",
+      "Address One",
+      "Address Two",
+      "City",
+      "State",
+      "Zip"
+    ]
+
+    attr_accessor :psc
+    attr_accessor :options
+
+    ##
+    # Creates a new instance of the CaseStatusReport
+    # @param[PatientStudyCalendar]
+    # @param[Hash] - {:start_date => x, :end_date => y}
+    def initialize(psc, options = {})
+      self.psc = psc
+      defaults = {
+        :start_date => Date.today.to_s(:db),
+        :end_date   => 1.week.from_now.to_date.to_s(:db)
+      }
+      self.options = defaults.merge(options)
+    end
+
+    ##
+    # Use Patient Study Calendar to get the scheduled activities for the given dates
+    # Default dates to the upcoming week
+    # @return [Array<String>] - scheduled_study_segment grid_ids
+    def scheduled_study_segment_identifiers
+      scheduled_study_segment_ids = []
+
+      rpt = psc.scheduled_activities_report(options)
+      rpt["rows"].each do |row|
+        scheduled_study_segment_ids << row["scheduled_study_segment"]["grid_id"]
+      end
+      scheduled_study_segment_ids.uniq
+    end
+
+    ##
+    # Runs a query to get the Participant data for those with scheduled activities
+    # in the given date range
+    # @return [Array<Participant>]
+    def case_statuses
+      ids = scheduled_study_segment_identifiers.map {|i| "'#{i}'" }.join(',')
+      sql = <<-SQL
+        select part.id as q_id, part.p_id, pers.first_name as q_first_name, pers.last_name as q_last_name,
+         max(e.event_start_date) as q_event_date, event_code.display_text as q_event_name,
+         t.phone_nbr as q_phone,
+         a.address_one as q_address_one, a.address_two as q_address_two, a.city as q_city,
+         state_code.display_text as q_state, a.zip as q_zip
+         from participants part
+         left outer join participant_person_links ppl on ppl.participant_id = part.id
+         left outer join people pers on pers.id = ppl.person_id
+         left outer join events e on e.participant_id = part.id
+         left outer join telephones t on t.person_id = pers.id
+         left outer join addresses a on a.person_id = pers.id
+         left outer join ncs_codes state_code on state_code.local_code = a.state_code
+         left outer join ncs_codes event_code on event_code.local_code = e.event_type_code
+         where ppl.relationship_code = 1
+         and e.scheduled_study_segment_identifier in (#{ids})
+         and state_code.list_name = 'STATE_CL1'
+         and event_code.list_name = 'EVENT_TYPE_CL1'
+         group by part.id, part.p_id, pers.first_name, pers.last_name,
+         e.event_start_date, event_code.display_text, t.phone_nbr,
+         a.address_one, a.address_two, a.city, state_code.display_text, a.zip
+         order by p_id
+      SQL
+
+      Participant.find_by_sql(sql)
+    end
+
+    ##
+    # Delegate to Contact#last_contact and collect those into a Hash keyed by participant id
+    # @param [Array<Integer>]
+    # @return [Hash<Integer, Contact>]
+    def last_contacts(participant_identifiers)
+      result = Hash.new
+      Contact.last_contact(participant_identifiers).each do |c|
+        result[c.participant_id.to_i] = c
+      end
+      result
+    end
+
+    ##
+    # Delegate to PpgStatusHistory#current_status and collect those into a Hash keyed by participant id
+    # @param [Array<Integer>]
+    # @return [Hash<Integer, PpgStatusHistory>]
+    def ppg_statuses(participant_identifiers)
+      result = Hash.new
+      PpgStatusHistory.current_status(participant_identifiers).each do |ppg|
+        result[ppg.participant_id.to_i] = ppg
+      end
+      result
+    end
+
+    ##
+    # Generate a csv of the data collected
+    # @return [FasterCSV]
+    def generate_report
+      statuses = case_statuses
+      p_ids = statuses.collect { |c| c.q_id }
+
+      last_contacts = last_contacts(p_ids)
+      current_ppg_statuses = ppg_statuses(p_ids)
+
+      FasterCSV.generate do |csv|
+        csv << REPORT_HEADERS
+        statuses.each do |c|
+          last_contact = last_contacts[c.q_id.to_i]
+          ppg_status = current_ppg_statuses[c.q_id.to_i]
+          csv << [
+            c.p_id,
+            c.q_first_name,
+            c.q_last_name,
+            c.q_event_date,
+            c.q_event_name,
+            last_contact.nil? ? "n/a" : last_contact.contact_date.to_s,
+            last_contact.nil? ? "n/a" : last_contact.contact_start_time,
+            last_contact.nil? ? "n/a" : last_contact.contact_end_time,
+            ppg_status.nil?   ? "n/a" : ppg_status.ppg_status_code,
+            last_contact.nil? ? "n/a" : last_contact.contact_language.to_s,
+            last_contact.nil? ? "n/a" : last_contact.contact_disposition,
+            c.q_phone,
+            c.q_address_one,
+            c.q_address_two,
+            c.q_city,
+            c.q_state,
+            c.q_zip
+          ]
+        end
+      end
+
+    end
+
+  end
+end
+
+
+# select part.id as q_id, part.p_id, pers.first_name as q_first_name, pers.last_name as q_last_name,
+#        max(e.event_start_date) as q_event_date, event_code.display_text as q_event_name,
+#        t.phone_nbr as q_phone,
+#        a.address_one as q_address_one, a.address_two as q_address_two, a.city as q_city,
+#        state_code.display_text as q_state, a.zip as q_zip
+# from participants part
+#   left outer join participant_person_links ppl on ppl.participant_id = part.id
+#   left outer join people pers on pers.id = ppl.person_id
+#   left outer join events e on e.participant_id = part.id
+#   left outer join telephones t on t.person_id = pers.id
+#   left outer join addresses a on a.person_id = pers.id
+#   left outer join ncs_codes state_code on state_code.local_code = a.state_code
+#   left outer join ncs_codes event_code on event_code.local_code = e.event_type_code
+# where ppl.relationship_code = 1
+#   and scheduled_study_segment_identifier in ('b3e4b432-b4f6-4b27-ad6b-6a37e17da5ab', 'fdbf8c20-7805-4d7f-b82c-ee3624641509')
+#   and state_code.list_name = 'STATE_CL1'
+#   and event_code.list_name = 'EVENT_TYPE_CL1'
+# group by part.id, part.p_id, pers.first_name, pers.last_name,
+#          e.event_start_date, event_code.display_text, t.phone_nbr,
+#          a.address_one, a.address_two, a.city, state_code.display_text, a.zip
+# order by p_id
---
+++ spec/lib/reporting/case_status_report_spec.rb
@@ -0,0 +1,120 @@
+require 'spec_helper'
+
+describe Reporting::CaseStatusReport do
+
+  before(:each) do
+    psc_config ||= NcsNavigator.configuration.instance_variable_get("@application_sections")["PSC"]
+    @uri  = psc_config["uri"]
+    @user = mock(:username => "user", :cas_proxy_ticket => "PT-cas-ticket")
+    Factory(:ncs_code, :list_name => "PERSON_PARTCPNT_RELTNSHP_CL1", :display_text => "Self", :local_code => 1)
+    @state = Factory(:ncs_code, :list_name => "STATE_CL1", :display_text => "ILLINOIS", :local_code => 14)
+    @pv1 = Factory(:ncs_code, :list_name => "EVENT_TYPE_CL1", :display_text => "PV1", :local_code => 13)
+    @loi = Factory(:ncs_code, :list_name => "EVENT_TYPE_CL1", :display_text => "Lo I Quex", :local_code => 33)
+  end
+
+  let(:subject) { PatientStudyCalendar.new(@user) }
+
+  let(:ppg1) { Factory(:ncs_code, :list_name => "PPG_STATUS_CL1", :display_text => "PPG Group 1: Pregnant and Eligible", :local_code => 1) }
+  let(:ppg2) { Factory(:ncs_code, :list_name => "PPG_STATUS_CL1", :display_text => "PPG Group 2: High Probability – Trying to Conceive", :local_code => 2) }
+
+  let(:scheduled_study_segment_identifier_1) { 'b3e4b432-b4f6-4b27-ad6b-6a37e17da5ab' } # LUCY
+  let(:scheduled_study_segment_identifier_2) { 'fdbf8c20-7805-4d7f-b82c-ee3624641509' } # HENRIETTA
+  let(:reporter) { Reporting::CaseStatusReport.new(subject, { :start_date => '2012-02-01', :end_date => '2012-02-07' }) }
+
+  it "uses the scheduled activities report from psc to get event identifiers" do
+
+    VCR.use_cassette('psc/case_status_report') do
+      ids = reporter.scheduled_study_segment_identifiers
+      ids.size.should == 2
+      ids.should include scheduled_study_segment_identifier_1
+      ids.should include scheduled_study_segment_identifier_2
+    end
+
+  end
+
+  context "creating a status report for a given period" do
+
+    before(:each) do
+      create_missing_in_error_ncs_codes(Person)
+      create_missing_in_error_ncs_codes(Participant)
+      create_missing_in_error_ncs_codes(Address)
+      create_missing_in_error_ncs_codes(Telephone)
+      create_missing_in_error_ncs_codes(Event)
+      create_missing_in_error_ncs_codes(Contact)
+      create_missing_in_error_ncs_codes(ContactLink)
+
+      @per1 = Factory(:person, :first_name => 'LUCY', :last_name => 'VANPELT')
+      Factory(:address, :person => @per1, :state => @state, :address_one => "1 Peanut Drive")
+      Factory(:telephone, :person => @per1, :phone_nbr => "3125551212")
+
+      @per2 = Factory(:person, :first_name => 'HENRIETTA', :last_name => 'BROWN')
+      Factory(:address, :person => @per2, :state => @state, :address_one => "2 Main St")
+      Factory(:telephone, :person => @per2, :phone_nbr => "3125559999")
+
+      @p1 = Factory(:participant)
+      @p1.person = @per1
+      @p1.save!
+
+      @p2 = Factory(:participant)
+      @p2.person = @per2
+      @p2.save!
+
+      @e1 = Factory(:event, :participant => @p1, :event_type => @pv1, :scheduled_study_segment_identifier => scheduled_study_segment_identifier_1)
+      @e2 = Factory(:event, :participant => @p2, :event_type => @loi, :scheduled_study_segment_identifier => scheduled_study_segment_identifier_2)
+    end
+
+    it "gets data for all scheduled case statuses" do
+      VCR.use_cassette('psc/case_status_report') do
+
+        case_statuses = reporter.case_statuses
+        case_statuses.size.should == 2
+
+        cs1 = case_statuses.first
+        cs1.q_first_name.should == @per1.first_name
+        cs1.q_phone.should == @per1.telephones.first.phone_nbr
+
+        cs2 = case_statuses.last
+        cs2.q_last_name.should == @per2.last_name
+        cs2.q_address_one.should == @per2.addresses.first.address_one
+      end
+    end
+
+    it "gets the last contact for those participants who have scheduled events" do
+      VCR.use_cassette('psc/case_status_report') do
+        c1 = Factory(:contact, :contact_date_date => 1.day.ago)
+        l1 = Factory(:contact_link, :contact => c1, :person => @per1, :event => @e1)
+        c2 = Factory(:contact, :contact_date_date => 1.week.ago)
+        l2 = Factory(:contact_link, :contact => c2, :person => @per2, :event => @e2)
+
+        p_ids = reporter.case_statuses.collect { |c| c.q_id }
+
+        contacts = reporter.last_contacts(p_ids)
+        contacts.size.should == 2
+        contacts.keys.should include @p1.id
+        contacts.keys.should include @p2.id
+        contacts[@p1.id].contact_date.should == c1.contact_date
+        contacts[@p2.id].contact_date.should == c2.contact_date
+      end
+    end
+
+    it "gets the current ppg_status for those participants who have scheduled events" do
+      VCR.use_cassette('psc/case_status_report') do
+
+        Factory(:ppg_status_history, :participant => @p1, :ppg_status => ppg1, :ppg_status_date => 1.day.ago.to_date.to_s(:db))
+        Factory(:ppg_status_history, :participant => @p2, :ppg_status => ppg2, :ppg_status_date => 1.week.ago.to_date.to_s(:db))
+
+        p_ids = reporter.case_statuses.collect { |c| c.q_id }
+
+        ppgs = reporter.ppg_statuses(p_ids)
+        ppgs.size.should == 2
+        ppgs.keys.should include @p1.id
+        ppgs.keys.should include @p2.id
+        ppgs[@p1.id].ppg_status.should == ppg1
+        ppgs[@p2.id].ppg_status.should == ppg2
+      end
+    end
+
+
+  end
+
+end
