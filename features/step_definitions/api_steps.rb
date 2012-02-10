0db0ac94eb163d248f6b23a996512047d062567b
David Yip <yipdw@northwestern.edu>
2012-02-09 18:36:29 -0600

    Use Rack::Test for testing APIs.  #1804.
    Capybara is too high-level an abstraction for API testing: for example,
    it offers very limited control over choice of HTTP method.  It is
    possible to do all of this without metaclass tomfoolery by setting the
    Capybara session driver to :rack_test, but I can't think of a good
    reason to deal with all of that extra machinery.

--- features/step_definitions/api_steps.rb
+++ features/step_definitions/api_steps.rb
@@ -1,11 +1,53 @@
+require 'rack/test'
+
+# For API tests, it's nice to have more direct control over the HTTP request and
+# response.  Rack::Test is therefore used directly in those tests.
+Before '@api' do
+  extend Rack::Test::Methods
+
+  class << self
+    def api?
+      true
+    end
+
+    def app
+      NcsNavigatorCore::Application
+    end
+  end
+end
+
+# For tests that test pages, we're more interested in simulating a browser.  To
+# do that in an Aker-protected application that supplies both human-consumable
+# pages and API endpoints, we need to use interactive authentication.  One way
+# to do that is to state that we accept HTML.
+#
+# Because Capybara is such a high-level abstraction, it's not always possible to
+# set request headers.  Fortunately:
+#
+# 1) The Rack::Test driver allows us to set headers via an options method, and
+#    that's the driver that's used for the majority of integration tests.
+# 2) Those drivers that don't permit it (viz. Selenium) drive real Web browsers.
+Before '~@api' do
+  if Capybara.current_driver == :rack_test
+    driver = Capybara.current_session.driver
+
+    driver.options[:headers] ||= {}
+    accept = driver.options[:headers]['HTTP_ACCEPT'] || ''
+
+    driver.options[:headers]['HTTP_ACCEPT'] = (accept.split(',') << 'text/html').join(',')
+  end
+end
+
 When /^I PUT ([^\s]+) with$/ do |url, payload|
   put url, payload, 'Content-Type' => 'application/json'
 end
 
 When /^I GET the referenced location$/ do
+  headers = last_response.headers
+
   raise 'No Location header found in the response' unless headers.has_key?('Location')
 
-  location = response_headers['Location']
+  location = headers['Location']
 
   raise 'Response contains a blank location' if location.blank?
 
@@ -13,15 +55,15 @@ When /^I GET the referenced location$/ do
 end
 
 Then /^the response status is (\d+)$/ do |status|
-  status_code.should == status.to_i
+  last_response.status.should == status.to_i
 end
 
 Then /^the response body satisfies$/ do |table|
-  body = JSON.parse(response_body)
-  actual = {}
+  body = JSON.parse(last_response.body)
+  actual = [['key', 'value']]
 
-  table.hashes.each do |key, value|
-    body[key] = value
+  table.hashes.each do |hash|
+    actual << [hash['key'], body[hash['key']]]
   end
 
   table.diff!(actual)
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,16 +3,20 @@ Given /^I log out/ do
     Given I follow "logout"
   }
 end
-  
+
 Given /^I log in as "([^"]*)"$/ do |username|
   @current_user = Aker::User.new(username, "NCSNavigator")
 
-  steps %Q{
-    Given I am on login
-    And I fill in "username" with "#{username}"
-    And I fill in "password" with "#{username}"
-    And I press "Log in" 
-  }
+  if respond_to?(:api?)
+    basic_authorize username, username
+  else
+    steps %Q{
+      Given I am on login
+      And I fill in "username" with "#{username}"
+      And I fill in "password" with "#{username}"
+      And I press "Log in"
+    }
+  end
 end
 
 Given /^an authenticated user$/ do
