f5aa9813ebc4e735a2a96e124647d4503300045f
Paul Friedman <p-friedman@northwestern.edu>
2012-02-17 14:25:59 -0600

    only use scheduled activity report to build welcome index page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,46 +1,13 @@
 class WelcomeController < ApplicationController
 
   def index
-    @start_date = 1.day.ago.to_date.to_s
-    @end_date = 2.weeks.from_now.to_date.to_s
-    criteria = { :current_user => current_staff,
-                 :start_date => @start_date,
-                 :end_date => @end_date }
-
-    if scheduled_activities = psc.scheduled_activities_report(criteria)
-      scheduled_study_segment_ids = []
-      if rows = scheduled_activities['rows']
-        Rails.logger.info("~~~ scheduled_activities['rows'] = #{rows.size}")
-        rows.each do |row|
-          if row['scheduled_study_segment']
-            Rails.logger.info("~~~ adding #{row['scheduled_study_segment']['grid_id']} to ids")
-            scheduled_study_segment_ids << row['scheduled_study_segment']['grid_id']
-          end
-        end
-      end
-
-      events = Event.where("event_end_date is null").
-                     where("scheduled_study_segment_identifier in (?)", scheduled_study_segment_ids.uniq.compact).
-                     order("event_start_date").all
-      @events = {}
-      events.each do |e|
-        if @events.has_key?(e.event_start_date)
-          @events[e.event_start_date] << e
-        else
-          @events[e.event_start_date] = [e]
-        end
-      end
+    if scheduled_activities = get_upcoming_activities
+      @events = join_scheduled_events_by_date(parse_scheduled_activities(scheduled_activities))
     end
-
   end
 
   def upcoming_activities
-    @start_date = 1.day.ago.to_date.to_s
-    @end_date = 2.weeks.from_now.to_date.to_s
-    criteria = { :current_user => current_staff,
-                 :start_date => @start_date,
-                 :end_date => @end_date }
-    @scheduled_activities = psc.scheduled_activities_report(criteria)
+    @scheduled_activities = get_upcoming_activities
   end
 
 
@@ -76,5 +43,45 @@ class WelcomeController < ApplicationController
     end
 
   end
+
+  private
+
+    def get_upcoming_activities
+      @start_date = 1.day.ago.to_date.to_s
+      @end_date = 2.weeks.from_now.to_date.to_s
+      criteria = { :current_user => current_staff,
+                   :start_date => @start_date,
+                   :end_date => @end_date }
+      psc.scheduled_activities_report(criteria)
+    end
+
+    def parse_scheduled_activities(scheduled_activities)
+      events = []
+      if rows = scheduled_activities['rows']
+        rows.each do |row|
+          person = Person.find_by_person_id(row['subject']['person_id'])
+          if person
+            event_label = Event.parse_label(row['labels'].first)
+            events << ScheduledEvent.new(row['scheduled_date'], person, event_label.titleize) if event_label
+          end
+        end
+      end
+      events
+    end
+
+    def join_scheduled_events_by_date(events)
+      result = {}
+      events.uniq.each do |e|
+        date = e.date
+        if result.has_key?(date)
+          result[date] << e
+        else
+          result[date] = [e]
+        end
+      end
+      result
+    end
+
+    ScheduledEvent = Struct.new(:date, :person, :event_type)
 
 end
