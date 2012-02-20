061ab4c1441531061de6fa7effc8eb91dbf4d971
Rhett Sutphin <rhett@detailedbalance.net>
2012-02-20 10:27:52 -0600

    Add spec for acceptable multi-segment events. #1817.
    The importer, given an event that doesn't correspond to an existing scheduled
    segment, need to either be able to uniquely determine which segment to
    schedule or safely defer the event until all other events are scheduled. This
    spec encapsulates what that requires from the structure of the template.
    
    It also reveals that the birth visit will have to be special-cased in the
    importer, since it unavoidably occurs alone on two separate segments.

--- spec/lib/psc_template_spec.rb
+++ spec/lib/psc_template_spec.rb
@@ -41,22 +41,27 @@ describe 'PSC template' do
         code_list.collect { |cl| cl.label.strip.downcase.gsub(/\s+/, '_') }
     }
 
-    it 'covers only known event types' do
-      (events_named_in_labels - event_types_from_mdes).uniq.should == []
-    end
-
-    it 'schedules any given event type only once per day per segment' do
-      event_labels_by_segment_by_day = template_xml.css('study-segment').inject({}) do |segments, seg|
+    let(:event_labels_by_segment_by_day) {
+      template_xml.css('study-segment').inject({}) do |segments, seg|
         segments[seg['name']] = seg.css('planned-activity').inject({}) do |by_day, pa|
-          one_day = (by_day[pa['day'].to_i] ||= Set.new)
+          one_day = (by_day[pa['day'].to_i] ||= [])
           pa.css('label').collect { |l| l['name'] }.select { |n| n =~ prefix }.each do |event|
-            one_day << event
+            # uniq! instead of a set because sets don't behave like
+            # arrays in all situations (e.g., no #flatten)
+            (one_day << event).uniq!
           end
           by_day
         end
         segments
       end
+    }
 
+
+    it 'covers only known event types' do
+      (events_named_in_labels - event_types_from_mdes).uniq.should == []
+    end
+
+    it 'schedules any given event type only once per day per segment' do
       errors = []
       event_labels_by_segment_by_day.each do |segment_name, event_labels_by_day|
         event_labels_by_day.each do |day_1, event_labels_1|
@@ -70,6 +75,44 @@ describe 'PSC template' do
         end
       end
       errors.should == []
+    end
+
+    # This invariant is required by the importer, which needs to be
+    # able to take an event and do one of these:
+    #
+    #   - Find an existing segment with a corresponding event
+    #   - Find a single corresponding segment to schedule
+    #   - Defer the event until the first condition is true
+    #
+    # It special-cases the birth event by lo vs. hi, so this test
+    # special-cases it also.
+    it 'schedules any event that occurs on multiple days alongside an event that occurs on only one day' do
+      pending 'Until Child Consent segment is removed'
+
+      multiply_scheduled_event_and_counts = event_labels_by_segment_by_day.inject({}) do |counts, (segment_name, event_labels_by_day)|
+        event_labels_by_day.each do |day, event_labels|
+          event_labels.each { |event| counts[event] = (counts[event] || 0) + 1 }
+        end
+        counts
+      end.select { |event, count| count > 1 }
+
+      # special case for Hi-Lo birth; see above
+      if template_xml.css('study-snapshot').first['assigned-identifier'] =~ /Hi-Lo/
+        multiply_scheduled_event_and_counts -= [['event:birth', 2]]
+      end
+
+      multiply_scheduled_events = multiply_scheduled_event_and_counts.collect { |e, c| e }
+
+      problems = event_labels_by_segment_by_day.collect do |segment_name, event_labels_by_day|
+        multiply_scheduled_events.collect do |mult_event|
+          event_labels_by_day.
+            select { |day, events| events.include?(mult_event) }.
+            select { |day, events| (events - multiply_scheduled_events).empty? }.
+            collect  { |day, _| "#{mult_event} occurs without any one-day events on day #{day} of #{segment_name}" }
+        end
+      end.flatten.compact
+
+      problems.should == []
     end
   end
 
