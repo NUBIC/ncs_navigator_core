45a11021a292c330f76f67c68edbe7f2dc71c0e9
Paul Friedman <p-friedman@northwestern.edu>
2012-03-01 14:32:00 -0600

    Use surveyor's new helper override ability

---
+++ app/helpers/surveyor_helper.rb
@@ -0,0 +1,12 @@
+module SurveyorHelper
+  include Surveyor::Helpers::SurveyorHelperMethods
+
+  def rc_to_as(type_sym)
+    case type_sym.to_s
+    when /(integer|float)/ then :string
+    when /(datetime)/ then :string
+    else type_sym
+    end
+  end
+
+end
