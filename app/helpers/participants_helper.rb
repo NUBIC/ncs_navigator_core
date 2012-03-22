15df6a936cd51217d351c9703a567311d520108e
Paul Friedman <p-friedman@northwestern.edu>
2012-03-22 15:24:02 -0500

    fix to helper to return correct consent by consent type

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -15,13 +15,13 @@ module ParticipantsHelper
     return nil if should_hide_consent?(consent_type_text)
     consent_type = consent_type_text.underscore.gsub(' ', '_')
     if participant.consented?(NcsCode.for_attribute_name_and_local_code(:consent_type_code, consent_type_code))
-      consent = ParticipantConsent.where(:participant_id => participant.id).where(:contact_id => ContactLink.find(contact_link.id).contact.id).first
+      consent = ParticipantConsent.where(:participant_id => participant.id).where(:consent_type_code => consent_type_code).first
       link_to consent_type_text, edit_participant_consent_path(consent, :contact_link_id => contact_link.id, :consent_type => consent_type, :consent_type_code => consent_type_code), :class => "edit_link icon_link"
     else
       link_to consent_type_text, new_participant_consent_path(:participant_id => participant.id, :contact_link_id => contact_link.id, :consent_type => consent_type, :consent_type_code => consent_type_code), :class => "add_link icon_link"
     end
   end
-  
+
   def should_hide_consent?(consent_type_text)
     consent_type_text.include?("collect") && NcsNavigatorCore.with_specimens == "false"
   end
