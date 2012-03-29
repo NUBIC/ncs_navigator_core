7b812c6097f5a69dcc4770a1fb18336b76a75e1a
Paul Friedman <p-friedman@northwestern.edu>
2012-03-29 13:40:24 -0500

    psc_segment_name was a vestige on the Event class when the coupling of PSC with Core was less strong
    This commit removes this call deferring all calls to the event.event_type (or event.to_s)
    
    Closes bug#1943.

--- config/initializers/event_configuration.rb
+++ config/initializers/event_configuration.rb
@@ -1,2 +1,1 @@
 INSTRUMENT_EVENT_CONFIG  = YAML.load(File.read("#{Rails.root}/config/instrument_event_map.yml"))
-PSC_SEGMENT_EVENT_CONFIG = YAML.load(File.read("#{Rails.root}/config/psc_segment_event_map.yml"))
