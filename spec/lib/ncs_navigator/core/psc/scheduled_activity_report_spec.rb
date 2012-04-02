3e459ee7f1bdd1d551b54e6d228af490ff9aa8a6
David Yip <yipdw@northwestern.edu>
2012-04-02 14:00:56 -0500

    PSC -> Core entity mapping for ScheduledActivityReport.  #1863.

--- spec/lib/ncs_navigator/core/psc/scheduled_activity_report_spec.rb
+++ spec/lib/ncs_navigator/core/psc/scheduled_activity_report_spec.rb
@@ -2,14 +2,36 @@ require 'spec_helper'
 
 module NcsNavigator::Core::Psc
   describe ScheduledActivityReport do
-    let(:report) do
+    let(:psc_data) do
       {
         'filters' => {
           'end_date' => '2012-03-01',
           'start_date' => '2012-02-01',
           'states' => ['Scheduled']
         },
-        'rows' => []
+        'rows' => [
+          {"activity_name"=>"Pregnancy Probability Group Follow-Up SAQ",
+           "activity_status"=>"Scheduled",
+           "activity_type"=>"Instrument",
+           "grid_id"=>"cbcc8575-b1b9-4bd1-8a3c-51b885b903b6",
+           "ideal_date"=>"2012-02-16",
+           "labels"=>
+            ["event:pregnancy_probability",
+             "instrument:ins_que_ppgfollup_saq_ehpbhili_p2_v1.1"],
+           "last_change_reason"=>"Participant requested",
+           "responsible_user"=>"pfr957",
+           "scheduled_date"=>"2012-02-29",
+           "scheduled_study_segment"=>
+            {"grid_id"=>"976f857d-10e6-40e1-9767-4f9eb8f239eb",
+             "start_date"=>"2012-02-16",
+             "start_day"=>1},
+           "site"=>"GCSC",
+           "study"=>"NCS Hi-Lo",
+           "subject"=>
+            {"grid_id"=>"cdabfc40-578f-4dd5-8152-c5c501fcdf10",
+             "name"=>"Betty Boop",
+             "person_id"=>"b9696270-3586-012f-ca18-58b035fb69ca"}}
+        ]
       }
     end
 
@@ -27,7 +49,7 @@ module NcsNavigator::Core::Psc
       before do
         psc.should_receive(:scheduled_activities_report).
           with(:start_date => '2012-02-01', :end_date => '2012-03-01', :state =>
-               PatientStudyCalendar::ACTIVITY_SCHEDULED).and_return(report)
+               PatientStudyCalendar::ACTIVITY_SCHEDULED).and_return(psc_data)
 
         @report = ScheduledActivityReport.from_psc(psc, filters)
       end
@@ -37,11 +59,231 @@ module NcsNavigator::Core::Psc
       end
 
       it "sets the report's filters" do
-        @report.filters.should == report['filters']
+        @report.filters.should == psc_data['filters']
       end
 
       it "sets the report's rows" do
-        @report.rows.should == report['rows']
+        @report.rows.should == psc_data['rows'].map { |r| ScheduledActivityReport::Row.new(r) }
+      end
+    end
+
+    context 'entity mapping' do
+      let(:participant) { Factory(:participant) }
+      let(:person) { Factory(:person, :person_id => 'b9696270-3586-012f-ca18-58b035fb69ca') }
+      let(:report) { ScheduledActivityReport.new }
+
+      before do
+        Factory(:participant_person_link,
+                :person => person,
+                :participant => participant,
+                :relationship_code => 1)
+
+        report.rows = psc_data['rows'].map { |r| ScheduledActivityReport::Row.new(r) }
+      end
+
+      describe '#map_persons' do
+        let(:rows) do
+          report.map_persons
+          report.rows
+        end
+
+        it 'maps subject/person_id to Participant' do
+          rows[0].participant.should == participant
+        end
+
+        it 'stores the Person representing the Participant' do
+          rows[0].person.should == person
+        end
+
+        it 'maps unknown person IDs to nil' do
+          person.update_attribute(:person_id, 'foo')
+
+          rows[0].participant.should be_nil
+        end
+      end
+
+      describe '#map_events' do
+        let!(:event_type) { Factory(:ncs_code, :list_name => 'EVENT_TYPE_CL1', :display_text => 'Pregnancy Probability') }
+        let!(:event) { Factory(:event, :participant => participant, :event_start_date => Date.new(2012, 2, 16), :event_type => event_type) }
+
+        it 'maps the first event label to an event on the participant' do
+          report.map_persons
+          report.map_events
+
+          report.rows[0].event.should == event
+        end
+
+        it 'maps unknown labels to nil' do
+          event.destroy
+
+          report.map_persons
+          report.map_events
+
+          report.rows[0].event.should be_nil
+        end
+      end
+
+      describe '#map_instruments' do
+        let!(:event_type) { Factory(:ncs_code, :list_name => 'EVENT_TYPE_CL1', :display_text => 'Pregnancy Probability') }
+        let!(:event) { Factory(:event, :participant => participant, :event_start_date => Date.new(2012, 2, 16), :event_type => event_type) }
+        let!(:survey) { Factory(:survey, :title => 'ins_que_ppgfollup_saq_ehpbhili_p2_v1.1', :access_code => 'ins_que_ppgfollup_saq_ehpbhili_p2') }
+        let!(:response_set) { Factory(:response_set, :instrument => instrument, :survey => survey, :person => person) }
+        let!(:instrument) { Factory(:instrument, :survey => survey) }
+
+        let(:rows) do
+          report.map_persons
+          report.map_events
+          report.map_instruments
+
+          report.rows
+        end
+
+        it 'maps the first instrument label to an instrument' do
+          rows[0].instrument.should == instrument
+        end
+
+        it 'builds an instrument if an appropriate one cannot be found' do
+          response_set.destroy
+
+          rows[0].instrument.should be_new_record
+        end
+
+        describe "if the row's event is nil" do
+          before do
+            event.destroy
+          end
+
+          it "sets the row's instrument to nil" do
+            rows[0].instrument.should be_nil
+          end
+        end
+
+        describe "if the row's person is nil" do
+          before do
+            ParticipantPersonLink.all.each(&:destroy)
+            response_set.destroy
+            person.destroy
+          end
+
+          it "sets the row's instrument to nil" do
+            rows[0].instrument.should be_nil
+          end
+        end
+
+        describe "if the row's survey is nil" do
+          before do
+            instrument.update_attribute(:survey, nil)
+            response_set.destroy
+            survey.destroy
+          end
+
+          it "sets the row's instrument to nil" do
+            rows[0].instrument.should be_nil
+          end
+        end
+      end
+
+      describe '#map_contacts' do
+        let!(:event_type) { Factory(:ncs_code, :list_name => 'EVENT_TYPE_CL1', :display_text => 'Pregnancy Probability') }
+        let!(:event) { Factory(:event, :participant => participant, :event_start_date => Date.new(2012, 2, 16), :event_type => event_type) }
+
+        let(:rows) do
+          report.map_persons
+          report.map_events
+          report.map_contacts
+
+          report.rows
+        end
+
+        describe "if there is a non-closed contact with date equal to the row's scheduled date" do
+          let!(:contact) do
+            Factory(:contact, :contact_date => Date.new(2012, 2, 29))
+          end
+
+          before do
+            Factory(:contact_link, :event => event, :contact => contact)
+          end
+
+          it 'maps that contact' do
+            rows[0].contact.should == contact
+          end
+        end
+
+        shared_examples_for 'a contact builder' do
+          let(:row) { rows[0] }
+          let(:row_contact) { row.contact }
+
+          it 'builds a contact' do
+            row_contact.should be_new_record
+          end
+
+          it "sets the contact's date to the row's scheduled date" do
+            row_contact.contact_date.should == row.scheduled_date
+          end
+
+          it "adds the contact to the row's event" do
+            row.event.contact_links.map(&:contact).should include(contact)
+          end
+
+          it 'does not set a contact end time' do
+            row_contact.contact_end_time.should be_blank
+          end
+        end
+
+        describe "if there is not a contact with date equal to the row's scheduled date" do
+          let!(:contact) do
+            Factory(:contact, :contact_date => Date.new(2011, 1, 1))
+          end
+
+          before do
+            Factory(:contact_link, :event => event, :contact => contact)
+          end
+
+          it_should_behave_like 'a contact builder'
+        end
+
+        describe "if there is not a non-closed contact with date equal to the row's scheduled date" do
+          let!(:contact) do
+            Factory(:contact, :contact_date => Date.new(2012, 2, 29), :contact_end_time => '17:55')
+          end
+
+          before do
+            Factory(:contact_link, :event => event, :contact => contact)
+
+            report.map_persons
+            report.map_events
+            report.map_contacts
+          end
+
+          it_should_behave_like 'a contact builder'
+        end
+
+        describe 'if two rows share the same event and scheduled date' do
+          before do
+            report.rows = [
+              OpenStruct.new(:event => event, :scheduled_date => '2011-02-28'),
+              OpenStruct.new(:event => event, :scheduled_date => '2011-02-28')
+            ]
+          end
+
+          describe 'and there is no suitable existing contact' do
+            it 'shares the instantiated contact' do
+              report.map_contacts
+
+              report.rows.map { |r| r.contact.object_id }.uniq.length.should == 1
+            end
+          end
+        end
+
+        describe "if the row's event is nil" do
+          before do
+            event.destroy
+          end
+
+          it "sets the row's contact to nil" do
+            rows[0].contact.should be_nil
+          end
+        end
       end
     end
   end
