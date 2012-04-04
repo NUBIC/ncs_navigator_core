02f2122742c5be64c9805a941366da7a6085bfec
David Yip <yipdw@northwestern.edu>
2012-04-04 14:08:39 -0500

    Add and initialize Resque.  #1806.
    This commit also publicizes Rails.application.redis_url so that it can
    be used from outside the Rails::Application for Core.  (In an
    after_initialize block, self == main.)

---
+++ config/initializers/resque.rb
@@ -0,0 +1,10 @@
+##
+# NB: This is in an after_initialize block because we need
+# deferrable_redis_connection.rb to have run before we do any Resque configuration.
+#
+# Lexicographical ordering means we're fine for now (d < r), but why rely on
+# that when you don't have to?
+Rails.application.config.after_initialize do
+  Resque.redis = Rails.application.redis_url
+  Resque.redis.namespace = 'nubic:ncs_navigator_core:resque'
+end
