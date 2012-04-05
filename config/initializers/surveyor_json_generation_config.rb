a7b71a4898095ddc42609a17a80a5f14dc4e6598
David Yip <yipdw@northwestern.edu>
2012-04-05 15:57:58 -0500

    Disable generation of rooted objects for Surveyor.  #1806.
    This appears to be the current best way to handle this.

---
+++ config/initializers/surveyor_json_generation_config.rb
@@ -0,0 +1,6 @@
+# Disable JSON root generation for Surveyor.
+# FIXME: This should really live somewhere else, like an #as_json option.
+
+require 'rabl'
+Rabl.register!
+Rabl.configure {|config| config.include_json_root = false }
