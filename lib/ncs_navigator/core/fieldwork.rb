0b0e04b216ea3f94c08b7f3cc74bac0bb885d1f7
David Yip <yipdw@northwestern.edu>
2012-04-04 19:51:33 -0500

    Proposed merge model.  #1806.

--- lib/ncs_navigator/core/fieldwork.rb
+++ lib/ncs_navigator/core/fieldwork.rb
@@ -2,6 +2,8 @@ require 'ncs_navigator/core'
 
 module NcsNavigator::Core
   module Fieldwork
-    autoload :Validator,  'ncs_navigator/core/fieldwork/validator'
+    autoload :Validator,      'ncs_navigator/core/fieldwork/validator'
+    autoload :Superposition,  'ncs_navigator/core/fieldwork/superposition'
+    autoload :MergeTheirs,    'ncs_navigator/core/fieldwork/merge_theirs'
   end
 end
