cbf55a81cc157bd5ab0f909a0c2223b09f468cf6
David Yip <yipdw@northwestern.edu>
2012-04-12 15:53:19 -0500

    Stub out adapters.  #1984.

--- lib/ncs_navigator/core/fieldwork.rb
+++ lib/ncs_navigator/core/fieldwork.rb
@@ -2,6 +2,7 @@ require 'ncs_navigator/core'
 
 module NcsNavigator::Core
   module Fieldwork
+    autoload :Adapters,       'ncs_navigator/core/fieldwork/adapters'
     autoload :Validator,      'ncs_navigator/core/fieldwork/validator'
     autoload :Superposition,  'ncs_navigator/core/fieldwork/superposition'
     autoload :MergeTheirs,    'ncs_navigator/core/fieldwork/merge_theirs'
