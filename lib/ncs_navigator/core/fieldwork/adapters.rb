12367db526227ab411aaf79d48ba8b9133b1bcac
David Yip <yipdw@northwestern.edu>
2012-04-13 14:43:29 -0500

    Update adapters for latest ncs_navigator_schema.  #1984.
    This commit also eliminates the unnecessary attribute writers on the
    hash adapters.

--- lib/ncs_navigator/core/fieldwork/adapters.rb
+++ lib/ncs_navigator/core/fieldwork/adapters.rb
@@ -3,14 +3,28 @@ require 'ncs_navigator/core'
 ##
 # These adapters were bootstrapped from the fieldwork JSON schema.
 #
-# Schema revision: 4ffbaa28e32983d8dad3b333a9dbe8d509f2a174
+# Schema revision: 5e5db04601d3081d44b20e6233c27eadcec384fb
 module NcsNavigator::Core::Fieldwork::Adapters
+  def adapt_hash(type, o)
+    case type
+    when :contact; ContactHashAdapter.new(o)
+    when :event; EventHashAdapter.new(o)
+    when :person; PersonHashAdapter.new(o)
+    when :response; ResponseHashAdapter.new(o)
+    when :response_set; ResponseSetHashAdapter.new(o)
 
-  def adapt_contact(obj)
-    case obj
-    when ActiveRecord::Base; ContactModelAdapter.new(obj)
-    when Hash; ContactHashAdapter.new(obj)
-    else raise "No adapter known for #{obj.inspect}"
+    end
+  end
+
+  def adapt_model(m)
+    case m
+    when Contact; ContactModelAdapter.new(m)
+    when Event; EventModelAdapter.new(m)
+    when Person; PersonModelAdapter.new(m)
+    when Response; ResponseModelAdapter.new(m)
+    when ResponseSet; ResponseSetModelAdapter.new(m)
+
+    end
   end
 
   class ContactModelAdapter < Struct.new(:target)
@@ -57,6 +71,13 @@ module NcsNavigator::Core::Fieldwork::Adapters
     def type=(val)
     end
 
+    def to_model
+      target
+    end
+
+    def ==(other)
+      to_model == other.to_model
+    end
   end
 
   class ContactHashAdapter < Struct.new(:target)
@@ -65,65 +86,37 @@ module NcsNavigator::Core::Fieldwork::Adapters
       target[%q{contact_date}]
     end
 
-    def contact_date=(val)
-      raise NotImplementedError
-    end
-
     def contact_id
       target[%q{contact_id}]
-    end
-
-    def contact_id=(val)
-      raise NotImplementedError
     end
 
     def disposition
       target[%q{disposition}]
     end
 
-    def disposition=(val)
-      raise NotImplementedError
-    end
-
     def end_time
       target[%q{end_time}]
-    end
-
-    def end_time=(val)
-      raise NotImplementedError
     end
 
     def person_id
       target[%q{person_id}]
     end
 
-    def person_id=(val)
-      raise NotImplementedError
-    end
-
     def start_time
       target[%q{start_time}]
-    end
-
-    def start_time=(val)
-      raise NotImplementedError
     end
 
     def type
       target[%q{type}]
     end
 
-    def type=(val)
-      raise NotImplementedError
+    def to_hash
+      target
     end
 
-  end
-
-  def adapt_event(obj)
-    case obj
-    when ActiveRecord::Base; EventModelAdapter.new(obj)
-    when Hash; EventHashAdapter.new(obj)
-    else raise "No adapter known for #{obj.inspect}"
+    def ==(other)
+      to_hash == other.to_hash
+    end
   end
 
   class EventModelAdapter < Struct.new(:target)
@@ -176,6 +169,13 @@ module NcsNavigator::Core::Fieldwork::Adapters
     def start_time=(val)
     end
 
+    def to_model
+      target
+    end
+
+    def ==(other)
+      to_model == other.to_model
+    end
   end
 
   class EventHashAdapter < Struct.new(:target)
@@ -184,73 +184,41 @@ module NcsNavigator::Core::Fieldwork::Adapters
       target[%q{disposition}]
     end
 
-    def disposition=(val)
-      raise NotImplementedError
-    end
-
     def disposition_category
       target[%q{disposition_category}]
-    end
-
-    def disposition_category=(val)
-      raise NotImplementedError
     end
 
     def end_date
       target[%q{end_date}]
     end
 
-    def end_date=(val)
-      raise NotImplementedError
-    end
-
     def end_time
       target[%q{end_time}]
-    end
-
-    def end_time=(val)
-      raise NotImplementedError
     end
 
     def event_id
       target[%q{event_id}]
     end
 
-    def event_id=(val)
-      raise NotImplementedError
-    end
-
     def name
       target[%q{name}]
-    end
-
-    def name=(val)
-      raise NotImplementedError
     end
 
     def start_date
       target[%q{start_date}]
     end
 
-    def start_date=(val)
-      raise NotImplementedError
-    end
-
     def start_time
       target[%q{start_time}]
     end
 
-    def start_time=(val)
-      raise NotImplementedError
+    def to_hash
+      target
     end
 
-  end
-
-  def adapt_person(obj)
-    case obj
-    when ActiveRecord::Base; PersonModelAdapter.new(obj)
-    when Hash; PersonHashAdapter.new(obj)
-    else raise "No adapter known for #{obj.inspect}"
+    def ==(other)
+      to_hash == other.to_hash
+    end
   end
 
   class PersonModelAdapter < Struct.new(:target)
@@ -315,6 +283,13 @@ module NcsNavigator::Core::Fieldwork::Adapters
     def zip_code=(val)
     end
 
+    def to_model
+      target
+    end
+
+    def ==(other)
+      to_model == other.to_model
+    end
   end
 
   class PersonHashAdapter < Struct.new(:target)
@@ -323,89 +298,49 @@ module NcsNavigator::Core::Fieldwork::Adapters
       target[%q{cell_phone}]
     end
 
-    def cell_phone=(val)
-      raise NotImplementedError
-    end
-
     def city
       target[%q{city}]
-    end
-
-    def city=(val)
-      raise NotImplementedError
     end
 
     def email
       target[%q{email}]
     end
 
-    def email=(val)
-      raise NotImplementedError
-    end
-
     def home_phone
       target[%q{home_phone}]
-    end
-
-    def home_phone=(val)
-      raise NotImplementedError
     end
 
     def name
       target[%q{name}]
     end
 
-    def name=(val)
-      raise NotImplementedError
-    end
-
     def person_id
       target[%q{person_id}]
-    end
-
-    def person_id=(val)
-      raise NotImplementedError
     end
 
     def relationship_code
       target[%q{relationship_code}]
     end
 
-    def relationship_code=(val)
-      raise NotImplementedError
-    end
-
     def state
       target[%q{state}]
-    end
-
-    def state=(val)
-      raise NotImplementedError
     end
 
     def street
       target[%q{street}]
     end
 
-    def street=(val)
-      raise NotImplementedError
-    end
-
     def zip_code
       target[%q{zip_code}]
     end
 
-    def zip_code=(val)
-      raise NotImplementedError
+    def to_hash
+      target
     end
 
-  end
-
-  def adapt_response(obj)
-    case obj
-    when ActiveRecord::Base; ResponseModelAdapter.new(obj)
-    when Hash; ResponseHashAdapter.new(obj)
-    else raise "No adapter known for #{obj.inspect}"
+    def ==(other)
+      to_hash == other.to_hash
+    end
   end
 
   class ResponseModelAdapter < Struct.new(:target)
@@ -446,6 +381,13 @@ module NcsNavigator::Core::Fieldwork::Adapters
     def value=(val)
     end
 
+    def to_model
+      target
+    end
+
+    def ==(other)
+      to_model == other.to_model
+    end
   end
 
   class ResponseHashAdapter < Struct.new(:target)
@@ -454,57 +396,33 @@ module NcsNavigator::Core::Fieldwork::Adapters
       target[%q{answer_id}]
     end
 
-    def answer_id=(val)
-      raise NotImplementedError
-    end
-
     def api_id
       target[%q{api_id}]
-    end
-
-    def api_id=(val)
-      raise NotImplementedError
     end
 
     def created_at
       target[%q{created_at}]
     end
 
-    def created_at=(val)
-      raise NotImplementedError
-    end
-
     def question_id
       target[%q{question_id}]
-    end
-
-    def question_id=(val)
-      raise NotImplementedError
     end
 
     def updated_at
       target[%q{updated_at}]
     end
 
-    def updated_at=(val)
-      raise NotImplementedError
-    end
-
     def value
       target[%q{value}]
     end
 
-    def value=(val)
-      raise NotImplementedError
+    def to_hash
+      target
     end
 
-  end
-
-  def adapt_response_set(obj)
-    case obj
-    when ActiveRecord::Base; ResponseSetModelAdapter.new(obj)
-    when Hash; ResponseSetHashAdapter.new(obj)
-    else raise "No adapter known for #{obj.inspect}"
+    def ==(other)
+      to_hash == other.to_hash
+    end
   end
 
   class ResponseSetModelAdapter < Struct.new(:target)
@@ -533,6 +451,13 @@ module NcsNavigator::Core::Fieldwork::Adapters
     def survey_id=(val)
     end
 
+    def to_model
+      target
+    end
+
+    def ==(other)
+      to_model == other.to_model
+    end
   end
 
   class ResponseSetHashAdapter < Struct.new(:target)
@@ -541,33 +466,24 @@ module NcsNavigator::Core::Fieldwork::Adapters
       target[%q{api_id}]
     end
 
-    def api_id=(val)
-      raise NotImplementedError
-    end
-
     def completed_at
       target[%q{completed_at}]
-    end
-
-    def completed_at=(val)
-      raise NotImplementedError
     end
 
     def created_at
       target[%q{created_at}]
     end
 
-    def created_at=(val)
-      raise NotImplementedError
-    end
-
     def survey_id
       target[%q{survey_id}]
     end
 
-    def survey_id=(val)
-      raise NotImplementedError
+    def to_hash
+      target
     end
 
+    def ==(other)
+      to_hash == other.to_hash
+    end
   end
 end
