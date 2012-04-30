627675ded5aed438efdfdab667d9b03919829d6f
Paul Friedman <p-friedman@northwestern.edu>
2012-04-30 11:53:46 -0500

    update ini configuration use core configuration for core mailer

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -1,8 +1,9 @@
 require 'ncs_navigator/configuration'
+require "#{Rails.root}/lib/development_mail_interceptor"
 
 ActionMailer::Base.smtp_settings = NcsNavigator.configuration.action_mailer_smtp_settings
-ActionMailer::Base.default :from => NcsNavigator.configuration.staff_portal['mail_from']
-ActionMailer::Base.default_url_options[:host] = NcsNavigator.configuration.staff_portal_uri.host
+ActionMailer::Base.default :from => NcsNavigator.configuration.core['mail_from']
+ActionMailer::Base.default_url_options[:host] = NcsNavigator.configuration.corel_uri.host
 
 if Rails.env.development?
   ActionMailer::Base.default_url_options[:port] = "3000"
