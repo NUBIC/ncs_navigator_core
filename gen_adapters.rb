c4db59fc8e9254bc0f96693e20a7cf8a4f266248
David Yip <yipdw@northwestern.edu>
2012-05-02 11:27:44 -0500

    A tool to generate the merge adapters.  #1975.

---
+++ gen_adapters.rb
@@ -0,0 +1,44 @@
+# Generates adapter classes from the fieldwork schema.
+require 'active_support/inflector'
+require 'erb'
+require 'json'
+require 'yaml'
+
+def properties_for(schema)
+  schema.reject { |k, v| v['items'] }.map(&:first).sort
+end
+
+schema_file = File.expand_path('../vendor/ncs_navigator_schema/fieldwork_schema.json', __FILE__)
+
+schema = JSON.parse(File.read(schema_file))
+rev = Dir.chdir(File.dirname(schema_file)) { `git show --format=oneline HEAD | head -n1 | cut -c1-40` }.chomp
+
+@contact_properties = properties_for(schema['properties']['contacts']['items']['properties'])
+@event_properties = properties_for(schema['properties']['contacts']['items']['properties']['events']['items']['properties'])
+@person_properties = properties_for(schema['properties']['participants']['items']['properties']['persons']['items']['properties'])
+@instrument_properties = properties_for(schema['properties']['contacts']['items']['properties']['events']['items']['properties']['instruments']['items']['properties'])
+
+schema_file = File.expand_path('../vendor/ncs_navigator_schema/response_set_schema.json', __FILE__)
+schema = JSON.parse(File.read(schema_file))
+
+@response_set_properties = properties_for(schema['properties'])
+@response_properties = properties_for(schema['properties']['responses']['items']['properties'])
+
+def attributes
+  { 'Contact' => @contact_properties,
+    'Event' => @event_properties,
+    'Instrument' => @instrument_properties,
+    'ResponseSet' => @response_set_properties,
+    'Response' => @response_properties
+  }
+end
+
+attribute_map = YAML.load(File.read('fieldwork_to_model.yml'))
+
+def classes
+  attributes.keys.sort
+end
+
+template = ERB.new(File.read('adapters.rb.erb'), nil, '<>')
+
+puts template.result(binding)
