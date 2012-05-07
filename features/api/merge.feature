c64f2b06824a4598b437350eb254e65db01d8f01
David Yip <yipdw@northwestern.edu>
2012-05-07 13:09:02 -0500

    A smoke test for sync.  #1975.

---
+++ features/api/merge.feature
@@ -0,0 +1,53 @@
+@merge @wip
+Feature: Merging data from field clients
+  In order to collect data in the field
+  Field workers
+  need to be able to gather and sync data back to NCS Core.
+
+  See features/fixtures/fieldwork.json for values used in this test.
+
+  Background:
+    Given an authenticated user
+
+  Scenario: Changes to contacts, events, and instruments are stored
+    Given the participant
+      | person/first_name | Bessie              |
+      | person/last_name  | Smith               |
+      | person/person_id  | registered_with_psc |
+    And the event
+      | event_id         | 883d5830-91ed-4dd4-9303-903eee737082 |
+      | event_type       | Pregnancy Visit 2                    |
+      | event_start_date | 2005-07-15                           |
+    And the survey
+      | title           | pregnancy_survey_v1.0 |
+    And I complete the fieldwork set
+      | start_date          | 2005-07-01         |
+      | end_date            | 2005-07-30         |
+      | client_id           | 1234567890         |
+      | with                | fieldwork.json.erb |
+
+    When the merge runs
+    And I go to the participant page
+    Then I should see "Bessie Smith"
+
+    # event name
+    And I should see "Pregnancy Visit 2"
+
+    # event start and end times
+    And I should see "13:30"
+    And I should see "13:35"
+
+    # event start and end dates
+    And I should see "2005-07-17"
+    And I should see "2005-07-18"
+
+    # event disposition
+    And I should see "Participant cognitively unable to provide informed consent/complete interview"
+
+    # instrument name
+    And I should see "pregnancy_survey_v1.0"
+
+    # check responses
+    When I follow "pregnancy_survey_v1.0"
+    Then the "r_1_string_value" field should contain "Jeff January"
+    And the "r_2_string_value" field should contain "04/19/2012 13:33"
---
+++ features/step_definitions/merge_steps.rb
@@ -0,0 +1,98 @@
+require 'erb'
+require 'facets/random'
+
+def link_participant_to_associated_entities
+  @p.person = @person
+  @p.events << @event
+
+  @p.save!
+end
+
+# We want direct Rack::Test access, but it's not really an API-only
+# integration test.
+Before '@merge' do
+  extend Forwardable
+
+  def browser
+    Capybara.current_session.driver.browser
+  end
+
+  def_delegators :browser, *Rack::Test::Methods::METHODS
+end
+
+Given /^the participant$/ do |table|
+  person_attrs, p_attrs = table.raw.partition { |k, v| k =~ /^person/ }
+  person_attrs.map! { |k, v| [k.sub('person/', ''), v] }
+
+  @person = Person.create!(Hash[*person_attrs.flatten])
+  @p = Participant.create!(Hash[*p_attrs.flatten])
+end
+
+Given /^the event$/ do |table|
+  data = table.rows_hash
+  type_name = data.delete('event_type')
+
+  code = NcsCode.for_list_name_and_display_text('EVENT_TYPE_CL1', type_name)
+  raise "Unknown event type #{type_name}" unless code
+
+  @event = Event.create!(data.merge(:event_type => code,
+                                    :event_start_date => data['event_start_date']))
+end
+
+Given /^the survey$/ do |table|
+  data = table.rows_hash
+
+  str = %Q{
+survey '#{data['title']}' do
+  section 'Questions' do
+    q 'Question 1'
+    a_1 :string
+    q 'Question 2'
+    a_1 :string
+    q 'Question 3'
+    a_1 :string
+  end
+end
+  }
+
+  Surveyor::Parser.new.parse(str)
+end
+
+Given /^I complete the fieldwork set$/ do |table|
+  link_participant_to_associated_entities
+
+  data = table.rows_hash
+
+  fixtures_root = File.expand_path('../../fixtures', __FILE__)
+  data_file = File.expand_path(data.delete('with'), fixtures_root)
+
+  steps %Q{
+    When I POST /api/v1/fieldwork.json with
+      | start_date            | end_date            | client_id            |
+      | #{data['start_date']} | #{data['end_date']} | #{data['client_id']} |
+    Then the response status is 200
+  }
+
+  context = {
+    'contact_id' => Contact.last.public_id,
+    'instrument_id' => Instrument.last.public_id,
+    'person_id' => Person.last.public_id,
+    'response_set_id' => ResponseSet.last.api_id,
+    'survey_id' => Survey.last.api_id,
+    'question_ids' => Question.all.map(&:api_id),
+    'answer_ids' => Answer.all.map(&:api_id)
+  }
+
+  fieldwork_data = ERB.new(File.read(data_file)).result(binding)
+
+  uri = last_response.headers['Location']
+  put uri, fieldwork_data
+end
+
+When /^the merge runs$/ do
+  ok = NcsNavigator::Core::Fieldwork::MergeWorker.jobs.all? do |job|
+    job['class'].constantize.new.perform(*job['args'])
+  end
+
+  ok.should be_true
+end
