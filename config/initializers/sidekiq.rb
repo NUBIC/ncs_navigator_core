73519d49ea9d1f5a9ae11277080c2551b9939f68
David Yip <yipdw@northwestern.edu>
2012-05-10 13:30:50 -0500

    Configure the Sidekiq client (i.e. sidekiq-in-webapp).  #1975.
    The :size => 1 is present because, at present, we only have one thread
    that can access Sidekiq's Redis structures, and as such only need one
    connection.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,6 +4,10 @@
 # NB: This is in an after_initialize block because we need
 # deferrable_redis_connection.rb to have run before we do any Resque configuration.
 Rails.application.config.after_initialize do
+  Sidekiq.configure_client do |config|
+    config.redis = { :url => Rails.application.redis_url, :namespace => 'nubic:ncs_navigator_core:sidekiq', :size => 1 }
+  end
+
   Sidekiq.configure_server do |config|
     config.redis = { :url => Rails.application.redis_url, :namespace => 'nubic:ncs_navigator_core:sidekiq' }
   end
