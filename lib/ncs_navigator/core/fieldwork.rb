d33a72bf1961cf74fa8197def6560b5050156b5a
David Yip <yipdw@northwestern.edu>
2012-05-14 14:08:42 -0500

    Remove useless Associations module.

--- lib/ncs_navigator/core/fieldwork.rb
+++ lib/ncs_navigator/core/fieldwork.rb
@@ -5,7 +5,6 @@ require 'ncs_navigator/core'
 module NcsNavigator::Core
   module Fieldwork
     autoload :Adapters,      'ncs_navigator/core/fieldwork/adapters'
-    autoload :Associations,  'ncs_navigator/core/fieldwork/associations'
     autoload :Merge,         'ncs_navigator/core/fieldwork/merge'
     autoload :MergeWorker,   'ncs_navigator/core/fieldwork/merge_worker'
     autoload :ResponseGroup, 'ncs_navigator/core/fieldwork/response_group'
