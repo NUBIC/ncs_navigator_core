e2b970fc3d25fd0bfe514ccf9f6702641778e27a
David Yip <yipdw@northwestern.edu>
2012-05-15 12:37:05 -0500

    Clear Sidekiq job queue before each scenario.  #1975.
    Some scenarios (i.e. the PUT /api/v1/fieldwork scenarios) leave
    unprocessed jobs around.  This is expected behavior, but we don't want
    those to affect other tests.
    (cherry picked from commit f5e47bb0d2e466dd98641b94beaa57803f6a00eb)

--- features/support/sidekiq.rb
+++ features/support/sidekiq.rb
@@ -3,3 +3,8 @@
 # Disconnects Sidekiq from the network and places jobs into an in-memory
 # queue.
 require 'sidekiq/testing'
+
+# Isolates tests.
+Before do
+  NcsNavigator::Core::Fieldwork::MergeWorker.jobs.clear
+end
