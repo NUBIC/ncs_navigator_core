89703b93951f8213659165370e90c4bdcdf65c1e
Paul Friedman <p-friedman@northwestern.edu>
2012-05-15 12:54:14 -0500

    There was a conflict between the newly updated surveyor with versioning and core's use of paper_trail. This commit updates the paper_trail initializer to exclude Survey from paper_trail versioning. The survey_spec was updated to match changes to surveyor.

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -884,7 +883,7 @@ ActiveRecord::Schema.define(:version => 20120514231257) do
     t.integer  "provider_id"
     t.integer  "provider_ncs_role_code",                :null => false
     t.string   "provider_ncs_role_other"
-    t.string   "transaction_type",        :limit => 36
+    t.string   "transaction_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
