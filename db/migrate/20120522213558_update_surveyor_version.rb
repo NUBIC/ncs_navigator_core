4279fc1f9f42073be2dca41020558c7c6bf13ec6
Paul Friedman <p-friedman@northwestern.edu>
2012-05-22 16:45:25 -0500

    also the index should be renamed

--- db/migrate/20120522213558_update_surveyor_version.rb
+++ db/migrate/20120522213558_update_surveyor_version.rb
@@ -1,11 +1,12 @@
 class UpdateSurveyorVersion < ActiveRecord::Migration
   def self.up
     rename_column :surveys, :version, :survey_version
-    add_index(:surveys, [ :access_code, :survey_version], :name => 'surveys_access_code_version_idx', :unique => true)
+    remove_index( :surveys, :name => 'surveys_access_code_version_idx' )
+    add_index(:surveys, [ :access_code, :survey_version], :name => 'surveys_access_code_survey_version_idx', :unique => true)
   end
 
   def self.down
     rename_column :surveys, :survey_version, :version
-    remove_index( :surveys, :name => 'surveys_access_code_version_idx' )
+    remove_index( :surveys, :name => 'surveys_access_code_survey_version_idx' )
   end
 end
