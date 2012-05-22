30a851c167151f6a40470cda6e199d4e53725a0a
Paul Friedman <p-friedman@northwestern.edu>
2012-05-22 16:41:46 -0500

    I wanted rename_column not change_column

--- db/migrate/20120522213558_update_surveyor_version.rb
+++ db/migrate/20120522213558_update_surveyor_version.rb
@@ -1,11 +1,11 @@
 class UpdateSurveyorVersion < ActiveRecord::Migration
   def self.up
-    change_column :surveys, :survey_version, :integer, :default => 0
+    rename_column :surveys, :version, :survey_version, :integer, :default => 0
     add_index(:surveys, [ :access_code, :survey_version], :name => 'surveys_access_code_version_idx', :unique => true)
   end
 
   def self.down
-    change_column :surveys, :survey_version, :integer
+    rename_column :surveys, :survey_version, :version
     remove_index( :surveys, :name => 'surveys_access_code_version_idx' )
   end
 end
