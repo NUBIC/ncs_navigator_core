bde9ad0f095593012c7b62efb8bba513388472f1
David Yip <yipdw@member.fsf.org>
2012-05-29 14:50:22 -0400

    Update Fakeweb lines for merge (and fieldwork) stubs; removed @wip tag.
    Commit 916c2ea7 introduced a reponsible-user parameter in the scheduled
    activity report, which earlier versions of these Fakeweb lines weren't
    accounting for.  I'm not quite sure how the merge feature passed in the
    first place with that missing parameter, but explicitly accounting for
    it gets things passing for me.

--- features/support/fakeweb.rb
+++ features/support/fakeweb.rb
@@ -23,8 +23,8 @@ FakeWeb.register_uri(:post, /\/api\/v1\/studies\/(.*)\/sites\/(.*)\/subject-assi
 
 # PSC scheduled activity report
 [
-  %w(end-date=2012-03-01&start-date=2012-02-01&state=scheduled scheduled_activities_for_february.json),
-  %w(end-date=2005-07-30&start-date=2005-07-01&state=scheduled scheduled_activities_for_july_2005.json)
+  %w(end-date=2012-03-01&responsible-user=test_user&start-date=2012-02-01&state=scheduled scheduled_activities_for_february.json),
+  %w(end-date=2005-07-30&responsible-user=test_user&start-date=2005-07-01&state=scheduled scheduled_activities_for_july_2005.json)
 ].each do |qs, fn|
   FakeWeb.register_uri(:get, %r[/api/v1/reports/scheduled-activities\.json\?#{qs}$],
                        :body => File.expand_path("../../fixtures/fakeweb/#{fn}", __FILE__),
