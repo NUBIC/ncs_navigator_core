d8166a1c072d1bcceb9a438a5956603c4ad5cf14
David Yip <yipdw@northwestern.edu>
2012-06-12 16:52:41 -0500

    Alphabetize NcsNavigator::Core modules.

--- lib/ncs_navigator/core.rb
+++ lib/ncs_navigator/core.rb
@@ -5,9 +5,9 @@ module NcsNavigator
     autoload :Fieldwork,            'ncs_navigator/core/fieldwork'
     autoload :MdesCodeListLoader,   'ncs_navigator/core/mdes_code_list_loader'
     autoload :MdesInstrumentSurvey, 'ncs_navigator/core/mdes_instrument_survey'
-    autoload :Psc,                  'ncs_navigator/core/psc'
-    autoload :Warehouse,            'ncs_navigator/core/warehouse'
     autoload :Mustache,             'ncs_navigator/core/mustache'
     autoload :Pbs,                  'ncs_navigator/core/pbs'
+    autoload :Psc,                  'ncs_navigator/core/psc'
+    autoload :Warehouse,            'ncs_navigator/core/warehouse'
   end
 end
