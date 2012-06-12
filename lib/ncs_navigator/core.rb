1615e2b2959d7064836693f1d579fd14a4937282
David Yip <yipdw@northwestern.edu>
2012-06-12 16:52:17 -0500

    The Jobs module doesn't exist.

--- lib/ncs_navigator/core.rb
+++ lib/ncs_navigator/core.rb
@@ -3,7 +3,6 @@
 module NcsNavigator
   module Core
     autoload :Fieldwork,            'ncs_navigator/core/fieldwork'
-    autoload :Jobs,                 'ncs_navigator/core/jobs'
     autoload :MdesCodeListLoader,   'ncs_navigator/core/mdes_code_list_loader'
     autoload :MdesInstrumentSurvey, 'ncs_navigator/core/mdes_instrument_survey'
     autoload :Psc,                  'ncs_navigator/core/psc'
