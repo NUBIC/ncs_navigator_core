6dd7c2333117ec2fb9eb04af8e19bb81a6ca0bb7
Paul Friedman <p-friedman@northwestern.edu>
2012-06-22 10:33:46 -0500

    update text on pbs list index page

--- app/helpers/pbs_lists_helper.rb
+++ app/helpers/pbs_lists_helper.rb
@@ -2,10 +2,10 @@ module PbsListsHelper
 
   def provider_recruitment_link(pbs)
     if pbs.recruitment_started?
-      txt = 'Continue Provider Recruitment'
+      txt = 'Continue'
       cls = 'edit_link icon_link'
     else
-      txt = 'Initiate Provider Recruitment'
+      txt = 'Start'
       cls = 'add_link icon_link'
     end
     link_to txt, recruit_provider_pbs_list_path(pbs), :class => cls
