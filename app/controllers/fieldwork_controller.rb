698580dbebdfee3335bdeb293a9bc941ea286b0b
David Yip <yipdw@northwestern.edu>
2012-06-27 12:39:12 -0500

    Stub out GET /fieldwork/:id.  #2148.
    This commit also alters the response to GET /fieldwork to reference
    fieldwork sets by public ID, because
    
    1. it's symmetrical with PUT/GET /api/v1/fieldwork/:id
    2. it eliminates the need to futz with internal IDs from integration
       tests

--- app/controllers/fieldwork_controller.rb
+++ app/controllers/fieldwork_controller.rb
@@ -2,4 +2,8 @@ class FieldworkController < ApplicationController
   def index
     @fieldworks = Fieldwork.for_report.paginate(:page => params[:page], :per_page => 20)
   end
+
+  def show
+    @fieldwork = Fieldwork.find_by_fieldwork_id(params[:id])
+  end
 end
