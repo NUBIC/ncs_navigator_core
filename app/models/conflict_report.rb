5bc1673f80107d4bbb1b010ee279f0a9625f0904
David Yip <yipdw@northwestern.edu>
2012-06-29 15:37:23 -0500

    Reimplement FieldworkHelper#ordered_conflict_report in the model.  #2148.
    This is a bit longer, but it lets us iterate over a conflict report from
    outside the view layer.  Admittedly, we have no such use for that
    capability, but IME these sorts of things tend to crop up down the line.

--- app/models/conflict_report.rb
+++ app/models/conflict_report.rb
@@ -22,8 +22,35 @@ class ConflictReport
     @raw.to_json
   end
 
+  def each
+    @raw.keys.sort.map do |type|
+      entities = @raw[type]
+
+      entities.keys.sort.map do |public_id|
+        attributes = entities[public_id]
+
+        e = Entity.new(type, public_id)
+        as = attributes.keys.sort.map do |name|
+          state = attributes[name]
+
+          Attribute.new(name, state['current'], state['original'], state['proposed'])
+        end
+
+        yield e, as
+      end
+    end
+  end
+
   # Redefining #to_s messes with #inspect.
   def inspect
     "#<#{self.class.name}:0x#{object_id.to_s(16)} @raw=#{@raw}>"
+  end
+
+  Entity = Struct.new(:type, :public_id)
+
+  class Attribute < Struct.new(:name, :current, :original, :proposed)
+    def humanize
+      name.humanize
+    end
   end
 end
