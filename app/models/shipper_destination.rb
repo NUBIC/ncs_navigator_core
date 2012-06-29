2b723b0c6240475bd630c731b228794b1c6f9e85
Nataliya Shurupova <n-shurupova@northwestern.edu>
2012-06-29 10:12:01 -0500

    Commit #2230 - removed TWQ and TWF labs from selection. Keeping selection as an array until we know get more info on the weekend shipping

--- app/models/shipper_destination.rb
+++ app/models/shipper_destination.rb
@@ -2,9 +2,10 @@ module ShipperDestination
 
   # TODO: setup these values in configuration when determined what they are !
   NCS_REPOSITORY = ["NCS Repository","001"]
-  TWQ_LAB        = ["TWQ lab", "002"]
-  TWF_LAB        = ["TWF lab","003"]
+  # TODO : potentially adding NCS Repository - Sunday Delivery
+  # TWQ_LAB        = ["TWQ lab", "002"]
+  # TWF_LAB        = ["TWF lab","003"]
   
-  LOCATIONS = [NCS_REPOSITORY, TWQ_LAB, TWF_LAB]
+  LOCATIONS = [NCS_REPOSITORY]
   
 end
