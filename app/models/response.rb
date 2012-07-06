3353916e284668f345cccc31e33546ffbdc50378
David Yip <yipdw@northwestern.edu>
2012-07-06 13:10:41 -0500

    Remove useless (and inaccurate) schema information block.

--- app/models/response.rb
+++ app/models/response.rb
@@ -23,35 +23,6 @@
 #  text_value        :text
 #  unit              :string(255)
 #  updated_at        :datetime
-#
-
-
-#
-# == Schema Information
-# Schema version: 20120515181518
-#
-# Table name: responses
-#
-#  id                :integer         not null, primary key
-#  response_set_id   :integer         not null
-#  question_id       :integer
-#  answer_id         :integer
-#  datetime_value    :datetime
-#  integer_value     :integer
-#  float_value       :float
-#  unit              :string(255)
-#  text_value        :text
-#  string_value      :string(255)
-#  response_other    :string(255)
-#  response_group    :string(255)
-#  created_at        :datetime
-#  updated_at        :datetime
-#  survey_section_id :integer
-#  source_mdes_table :string(100)
-#  source_mdes_id    :string(36)
-#  api_id            :string(255)
-#  lock_version      :integer         default(0)
-#
 class Response < ActiveRecord::Base
   include Surveyor::Models::ResponseMethods
 
