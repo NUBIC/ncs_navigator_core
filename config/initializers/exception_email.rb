a7a91b77deb5e01d5f0252a68d1fc1d9dc992f74
Rhett Sutphin <rhett@detailedbalance.net>
2012-07-06 19:40:23 -0500

    Correct exception notifier initializer. Closes #2268.

--- config/initializers/exception_email.rb
+++ config/initializers/exception_email.rb
@@ -1,9 +1,7 @@
-require 'ncs_navigator/configuration'
-
 recipients = NcsNavigator.configuration.exception_email_recipients
 unless recipients.empty?
-  config.middleware.use ExceptionNotifier,
+  Rails.application.config.middleware.insert_before 'Rack::Runtime', ExceptionNotifier,
     :email_prefix => NcsNavigatorCore.email_prefix,
-    :sender_address => NcsNavigator.configuration.core_mail_from,
+    :sender_address => NcsNavigatorCore.suite_configuration.core_mail_from,
     :exception_recipients => recipients
 end
