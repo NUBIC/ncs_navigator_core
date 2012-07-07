65fc2e0bc68c46ccfb4f1e47b5d4945d1719cf2d
Rhett Sutphin <rhett@detailedbalance.net>
2012-07-06 20:10:06 -0500

    Add secure exception generation middleware. Done with #2268.
    This is for testing the exception mailer, so it is only defined when that
    middleware is present.

--- config/initializers/exception_email.rb
+++ config/initializers/exception_email.rb
@@ -4,4 +4,20 @@ unless recipients.empty?
     :email_prefix => NcsNavigatorCore.email_prefix,
     :sender_address => NcsNavigatorCore.suite_configuration.core_mail_from,
     :exception_recipients => recipients
+
+  class NcsNavigator::Core::ExceptionGeneratingMiddleware
+    def initialize(app)
+      @app = app
+    end
+
+    def call(env)
+      aker = env['aker.check']
+      if env['PATH_INFO'] == '/admin/fail' && aker && aker.permit?('System Administrator')
+        fail "One exception, coming up"
+      end
+      @app.call(env)
+    end
+  end
+
+  Rails.application.config.middleware.use NcsNavigator::Core::ExceptionGeneratingMiddleware
 end
