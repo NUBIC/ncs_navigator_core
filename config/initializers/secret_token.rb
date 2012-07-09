3f30f178466726d97c18ebe786ecd48d7723eb7e
Rhett Sutphin <rhett@detailedbalance.net>
2012-07-09 14:47:04 -0500

    Allow secret to be set externally.
    It is mandatory to use an external secret outside of development or test.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 
-
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-NcsNavigatorCore::Application.config.secret_token = '9eaf8e0a24f34bbf56a850b5a6dd2ba8c8d127b2064d88495765dd1f261d9a7ae249650b2206d457e64ae070964935d5c4a0f7c9e3cfd699dee2e6b38739e9e2'
+default_secret = 'cases' * 30
+secret_name = 'CORE_SECRET'
+
+if %w(development test ci).include?(Rails.env)
+  NcsNavigatorCore::Application.config.secret_token = ENV[secret_name] || default_secret
+else
+  NcsNavigatorCore::Application.config.secret_token = ENV[secret_name] ||
+    fail("#{secret_name} is mandatory for #{Rails.env}")
+end
