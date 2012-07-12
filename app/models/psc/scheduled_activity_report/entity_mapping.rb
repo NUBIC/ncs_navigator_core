5a9faa3cb085836c6426cbdf418620e8246065f0
David Yip <yipdw@northwestern.edu>
2012-07-12 16:17:36 -0500

    Map events, instruments, people, surveys.  #2255.

--- app/models/psc/scheduled_activity_report/entity_mapping.rb
+++ app/models/psc/scheduled_activity_report/entity_mapping.rb
@@ -1,4 +1,270 @@
+require 'ostruct'
+require 'set'
+
 class Psc::ScheduledActivityReport
   module EntityMapping
+    attr_reader :contact_links
+    attr_reader :contacts
+    attr_reader :events
+    attr_reader :instruments
+    attr_reader :people
+    attr_reader :surveys
+
+    def process
+      @contacts = Collection.new(self)
+      @contact_links = Collection.new(self)
+      @events = EventCollection.new(self)
+      @instruments = InstrumentCollection.new(self)
+      @people = PersonCollection.new(self)
+      @surveys = SurveyCollection.new(self)
+
+      rows.each do |row|
+        p = add_person(row)
+        c = add_contact(row, p)
+        e = add_event(row, c, p)
+        s = add_survey(row)
+        i = add_instrument(row, s, e, p) if s && e
+
+        add_contact_link(p, c, e, i)
+      end
+    end
+
+    ##
+    # Finds or builds model objects that correspond to the entities derived by
+    # #process.
+    def resolve_models
+      cache = Cache.new
+
+      [ people,
+        events,
+        surveys,
+        instruments
+      ].each { |c| c.resolve_models(cache) }
+    end
+
+    ##
+    # @private
+    def add_person(row)
+      people << Person.new(row['subject']['person_id'])
+    end
+
+    ##
+    # @private
+    def add_contact(row, person)
+      contacts << Contact.new(row['scheduled_date'], person)
+    end
+
+    ##
+    # @private
+    def add_event(row, contact, person)
+      el = row['labels'].detect { |r| r.starts_with?('event:') }
+
+      events << Event.new(el, row['ideal_date'], contact, person) if el
+    end
+
+    ##
+    # @private
+    def add_instrument(row, survey, event, person)
+      instruments << Instrument.new(survey, row['activity_name'], event, person)
+    end
+
+    ##
+    # @private
+    def add_survey(row)
+      il = row['labels'].detect { |r| r.starts_with?('instrument:') }
+
+      surveys << Survey.new(il) if il
+    end
+
+    ##
+    # @private
+    def add_contact_link(person, contact, event, instrument)
+      contact_links << ContactLink.new(person, contact, event, instrument)
+    end
+
+    class Collection
+      include Enumerable
+
+      attr_reader :host
+
+      def initialize(host)
+        @set = {}
+        @host = host
+      end
+
+      ##
+      # Given two value objects v1 and v2 that are equal but not eq, selects
+      # the first of [v1, v2] added to the collection, and returns it for all
+      # subsequent << operations.
+      #
+      # We do this because mutating non-comparable state on value objects is
+      # quite convenient when it comes to model resolution.
+      def <<(item)
+        if @set.has_key?(item)
+          @set[item]
+        else
+          @set[item] = item
+        end
+
+        @set[item]
+      end
+
+      def each
+        @set.values.each { |v| yield v }
+      end
+
+      ##
+      # For testing.
+      def ==(other)
+        Set.new(@set.values) == Set.new(other)
+      end
+
+      ##
+      # Also for testing.
+      def models
+        Set.new(map(&:model))
+      end
+
+      def logger
+        @host.logger
+      end
+    end
+
+    class Cache
+      attr_reader :people
+
+      def add_people(people_index)
+        @people = people_index
+      end
+
+      def participant_for(person)
+        @people[person.person_id].try(:participant)
+      end
+    end
+
+    class PersonCollection < Collection
+      ASSOCIATIONS = {
+        :participant_person_links => {
+          :participant => [
+            {
+              :events => [
+                :instruments, :contacts
+              ],
+            },
+            {
+              :people => [
+                { :addresses => :state },
+                :telephones,
+                :emails
+              ]
+            }
+          ]
+        }
+      }
+
+      def resolve_models(cache)
+        ids = map(&:person_id)
+        found = index ::Person.where(:person_id => ids).includes(ASSOCIATIONS)
+
+        each do |p|
+          p.model = found[p.person_id]
+
+          if !p.model
+            logger.error "Cannot map {person ID = #{p.person_id}} to a person"
+          end
+        end
+
+        cache.add_people(found)
+      end
+
+      def index(people)
+        {}.tap do |h|
+          people.each { |p| h[p.person_id] = p }
+        end
+      end
+    end
+
+    class EventCollection < Collection
+      def resolve_models(cache)
+        each { |e| resolve_model(e, cache) }
+      end
+
+      def resolve_model(event, cache)
+        participant = cache.participant_for(event.person)
+
+        if !participant
+          return
+        end
+
+        possible = participant.events
+        expected = OpenStruct.new(:labels => event.label, :ideal_date => event.ideal_date)
+        accepted = possible.detect { |event| event.matches_activity(expected) }
+
+        if accepted
+          event.model = accepted
+        else
+          logger.error %Q{Cannot map {label = #{event.label}, ideal date = #{event.ideal_date}, participant = #{participant.p_id}} to an event}
+        end
+      end
+    end
+
+    class SurveyCollection < Collection
+      # TODO: eliminate n-query behavior
+      def resolve_models(cache)
+        each do |survey|
+          survey.model = ::Survey.most_recent_for_access_code(survey.access_code)
+
+          if !survey.model
+            logger.error %Q{Cannot map {access code = #{survey.access_code}} to a survey}
+          end
+        end
+      end
+    end
+
+    class InstrumentCollection < Collection
+      def resolve_models(cache)
+        each do |instrument|
+          args = [instrument.person, instrument.survey, instrument.event].map(&:model)
+
+          instrument.model = ::Instrument.start(*args) if args.all?
+        end
+      end
+    end
+
+    module HasModel
+      attr_accessor :model
+
+      def resolved?
+        model
+      end
+    end
+
+    class Contact < Struct.new(:scheduled_date, :person)
+      include HasModel
+    end
+
+    class ContactLink < Struct.new(:person, :contact, :event, :instrument)
+      include HasModel
+    end
+
+    class Event < Struct.new(:label, :ideal_date, :contact, :person)
+      include HasModel
+    end
+
+    class Instrument < Struct.new(:survey, :name, :event, :person)
+      include HasModel
+    end
+
+    class Person < Struct.new(:person_id)
+      include HasModel
+    end
+
+    class Survey < Struct.new(:instrument_label)
+      include HasModel
+
+      def access_code
+        instrument_label.match(/^instrument:(.+)_v[\d\.]+/i)[1]
+      end
+    end
   end
 end
--- spec/models/psc/scheduled_activity_report/entity_mapping_spec.rb
+++ spec/models/psc/scheduled_activity_report/entity_mapping_spec.rb
@@ -37,6 +37,12 @@ class Psc::ScheduledActivityReport
         report.contacts.should == Set.new([EM::Contact.new(scheduled_date, person)])
       end
 
+      it 'finds surveys' do
+        report.surveys.should == Set.new([
+          EM::Survey.new(instrument_pregnotpreg)
+        ])
+      end
+
       it 'finds events' do
         person = EM::Person.new(person_id)
         contact = EM::Contact.new(scheduled_date, person)
@@ -51,20 +57,10 @@ class Psc::ScheduledActivityReport
         person = EM::Person.new(person_id)
         contact = EM::Contact.new(scheduled_date, person)
         event = EM::Event.new(event_data_collection, ideal_date, contact, person)
-        
+        survey = EM::Survey.new(instrument_pregnotpreg)
+
         report.instruments.should == Set.new([
-          EM::Instrument.new(instrument_pregnotpreg, activity_name, event, person)
-        ])
-      end
-
-      it 'finds surveys' do
-        person = EM::Person.new(person_id)
-        contact = EM::Contact.new(scheduled_date, person)
-        event = EM::Event.new(event_data_collection, ideal_date, contact, person)
-        instrument = EM::Instrument.new(instrument_pregnotpreg, activity_name, event, person)
-
-        report.surveys.should == Set.new([
-          EM::Survey.new(instrument)
+          EM::Instrument.new(survey, activity_name, event, person)
         ])
       end
 
@@ -75,12 +71,139 @@ class Psc::ScheduledActivityReport
         contact = EM::Contact.new(scheduled_date, person)
         event1 = EM::Event.new(event_informed_consent, ideal_date, contact, person)
         event2 = EM::Event.new(event_data_collection, ideal_date, contact, person)
-        instrument = EM::Instrument.new(instrument_pregnotpreg, activity_name, event2, person)
+        survey = EM::Survey.new(instrument_pregnotpreg)
+        instrument = EM::Instrument.new(survey, activity_name, event2, person)
 
         report.contact_links.should == Set.new([
           EM::ContactLink.new(person, contact, event1, nil),
           EM::ContactLink.new(person, contact, event2, instrument)
         ])
+      end
+    end
+
+    describe '#resolve_models' do
+      let(:sio) { StringIO.new }
+      let(:log) { sio.string }
+
+      before do
+        report.logger = ::Logger.new(sio)
+
+        report.process
+      end
+
+      describe 'for people' do
+        it 'finds people in Cases' do
+          p = Factory(:person, :person_id => person_id)
+
+          report.resolve_models
+
+          report.people.models.should == Set.new([p])
+        end
+
+        it 'logs an error if a person cannot be found' do
+          report.resolve_models
+
+          log.should =~ /cannot map {person ID = #{person_id}} to a person/i
+        end
+      end
+
+      describe 'for surveys' do
+        it 'finds surveys in Cases' do
+          s = Factory(:survey, :access_code => 'ins_que_lipregnotpreg_int_li_p2')
+
+          report.resolve_models
+
+          report.surveys.models.should == Set.new([s])
+        end
+
+        it 'logs an error if a survey cannot be found' do
+          report.resolve_models
+
+          log.should =~ /cannot map {access code = ins_que_lipregnotpreg_int_li_p2} to a survey/i
+        end
+      end
+
+      describe 'for events' do
+        let!(:p) { Factory(:person, :person_id => person_id) }
+        let!(:pa) { Factory(:participant) }
+
+        before do
+          # Link up.
+          p.participant = pa
+          p.save!
+        end
+
+        it 'finds events in Cases' do
+          # 33 => low-intensity data collection, 10 => informed consent.
+          et1 = NcsCode.for_list_name_and_local_code('EVENT_TYPE_CL1', 33)
+          e1 = Factory(:event, :participant => pa, :event_start_date => ideal_date, :event_type => et1)
+          et2 = NcsCode.for_list_name_and_local_code('EVENT_TYPE_CL1', 10)
+          e2 = Factory(:event, :participant => pa, :event_start_date => ideal_date, :event_type => et2)
+
+          report.resolve_models
+
+          report.events.models.should == Set.new([e1, e2])
+        end
+
+        it 'logs an error if an event cannot be found' do
+          report.resolve_models
+
+          log.should =~ /cannot map {label = event:low_intensity_data_collection, ideal date = #{ideal_date}, participant = #{pa.p_id}} to an event/i
+          log.should =~ /cannot map {label = event:informed_consent, ideal date = #{ideal_date}, participant = #{pa.p_id}} to an event/i
+        end
+      end
+
+      shared_context 'one existing event' do
+        let!(:p) { Factory(:person, :person_id => person_id) }
+        let!(:pa) { Factory(:participant) }
+
+        # 33 => low-intensity data collection.
+        let!(:et) { NcsCode.for_list_name_and_local_code('EVENT_TYPE_CL1', 33) }
+        let!(:e) { Factory(:event, :participant => pa, :event_start_date => ideal_date, :event_type => et) }
+
+        before do
+          # Link up.
+          p.participant = pa
+          p.save!
+        end
+      end
+
+      describe 'for instruments' do
+        describe 'if the instrument has a person, survey, and event' do
+          include_context 'one existing event'
+
+          let!(:s) { Factory(:survey, :access_code => 'ins_que_lipregnotpreg_int_li_p2', :title => instrument_pregnotpreg) }
+
+          it 'builds an instrument' do
+            report.resolve_models
+
+            report.instruments.models.first.should_not be_nil
+          end
+
+          describe 'the built instrument' do
+            let(:instrument) { report.instruments.models.first }
+
+            before do
+              report.resolve_models
+            end
+
+            it 'is a new record' do
+              instrument.should be_new_record
+            end
+
+            it 'has a response set' do
+              instrument.response_set.should_not be_nil
+            end
+
+            it 'is linked to the event' do
+              instrument.event.should == e
+            end
+
+            it 'is linked to the survey' do
+              instrument.survey.should == s
+            end
+          end
+        end
       end
     end
   end
