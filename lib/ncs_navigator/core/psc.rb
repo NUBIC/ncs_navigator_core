fe904cea217bd7bc02465bfb6c390cbd31c197a0
David Yip <yipdw@northwestern.edu>
2012-07-18 14:19:59 -0500

    Remove old ScheduledActivityReport code.  #2255.

--- lib/ncs_navigator/core/psc.rb
+++ lib/ncs_navigator/core/psc.rb
@@ -6,5 +6,4 @@ require 'ncs_navigator/core'
 module NcsNavigator::Core::Psc
   autoload :Logger,                   'ncs_navigator/core/psc/logger'
   autoload :Retry,                    'ncs_navigator/core/psc/retry'
-  autoload :ScheduledActivityReport,  'ncs_navigator/core/psc/scheduled_activity_report'
 end
