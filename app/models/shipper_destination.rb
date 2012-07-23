acfb25afed16f55ed7994010aadf14a5741af5f2
Nataliya Shurupova <n-shurupova@northwestern.edu>
2012-07-23 14:30:23 -0500

    Commit #2305 - changed send to site to be defaulted to one value

--- app/models/shipper_destination.rb
+++ app/models/shipper_destination.rb
@@ -4,9 +4,7 @@ module ShipperDestination
   # TODO: setup these values in configuration when determined what they are !
   NCS_REPOSITORY = ["NCS Repository","001"]
   # TODO : potentially adding NCS Repository - Sunday Delivery
-  # TWQ_LAB        = ["TWQ lab", "002"]
-  # TWF_LAB        = ["TWF lab","003"]
   
-  LOCATIONS = [NCS_REPOSITORY]
+  SPECIMEN_LOCATIONS = [NCS_REPOSITORY]
   
 end
