4aad75484fd3d2c59ed48a2d205e85604b49b319
David Yip <yipdw@member.fsf.org>
2012-07-31 13:26:07 -0500

    Stub out Psc::ScheduledActivityCollection.

---
+++ app/models/psc/scheduled_activity_collection.rb
@@ -0,0 +1,29 @@
+module Psc
+  ##
+  # An orderable collection of {ScheduledActivity} objects.
+  #
+  # ScheduledActivityCollection may be initialized with:
+  #
+  # * nothing (.new)
+  # * a PSC participant schedule report (.from_schedule)
+  # * a PSC scheduled activity report (.from_report)
+  #
+  # Sorting
+  # =======
+  #
+  # By default, a ScheduledActivityCollection's activities are ordered as they
+  # were received from PSC.  ScheduledActivityCollection is an enumerable
+  # object, so you may define other orderings via #sort_by or #sort.  (Note,
+  # however, that {ScheduledActivity} objects have no natural order, so you
+  # MUST supply a block to #sort.)
+  #
+  # Entity resolution
+  # =================
+  #
+  # This class provides methods to resolve Cases entities from activities and
+  # their labels.  Where possible, it is RECOMMENDED that you use these
+  # methods, as they are far more efficient than performing individual database
+  # queries.
+  class ScheduledActivityCollection
+  end
+end
