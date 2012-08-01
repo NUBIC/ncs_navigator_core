3582be1daecc9918788e35ecc368171625729553
David Yip <yipdw@northwestern.edu>
2012-08-01 13:38:03 -0500

    Build ScheduledActivityCollections from SA reports and schedules.

--- spec/models/psc/scheduled_activity_collection_spec.rb
+++ spec/models/psc/scheduled_activity_collection_spec.rb
@@ -3,17 +3,29 @@ require 'spec_helper'
 require File.expand_path('../scheduled_activity_collection_contexts', __FILE__)
 
 module Psc
+  ##
+  # These tests rely quite heavily on values present in the test fixtures.  It
+  # is RECOMMENDED that you keep scheduled_activity_collection_contexts open in
+  # a split pane while reading these examples.
   describe ScheduledActivityCollection do
     describe '.from_report' do
       include_context 'from report'
 
-      it 'creates a collection from a scheduled activity report' do
-        ScheduledActivityCollection.from_report(source).should be_instance_of(ScheduledActivityCollection)
+      let(:report) { ScheduledActivityCollection.from_report(source) }
+
+      it 'creates one ScheduledActivity per report row' do
+        report.length.should == 2
       end
     end
 
     describe '.from_schedule' do
-      it 'creates a collection from a participant schedule'
+      include_context 'from schedule'
+
+      let(:report) { ScheduledActivityCollection.from_schedule(source) }
+
+      it 'creates one ScheduledActivity per activity set' do
+        report.length.should == 6
+      end
     end
   end
 end
