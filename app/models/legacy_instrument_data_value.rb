a7697ca01c63422f7f096a3acbe5073231ed60ec
Rhett Sutphin <rhett@detailedbalance.net>
2012-09-07 01:14:05 -0500

    Look up corresponding surveyor qs. #2442.

--- app/models/legacy_instrument_data_value.rb
+++ app/models/legacy_instrument_data_value.rb
@@ -12,4 +12,58 @@
 
 class LegacyInstrumentDataValue < ActiveRecord::Base
   belongs_to :legacy_instrument_data_record, :inverse_of => :values
+
+  alias :record :legacy_instrument_data_record
+
+  ##
+  # @return [Question,false] A surveyor question whose data export identifier
+  #   indicates it matches the variable for this value.
+  def question
+    if @question.nil?
+      result = Question.where(
+        "data_export_identifier ILIKE '#{record.mdes_table_name}%.#{mdes_variable_name}'").first
+      # memoize to false when no matches
+      @question = result || false
+    else
+      @question
+    end
+  end
+
+  ##
+  # @return [String,nil] The text for {#question} or `nil` if there is none.
+  def question_text
+    question.text if question
+  end
+
+  ##
+  # @return [Answer,false] The answer to {#question} whose reference identifier
+  #   matches this value.
+  def answer
+    if @answer.nil?
+      result =
+        if question && !value.blank?
+          question.answers.where('reference_identifier = ?', answer_reference_identifier).first
+        end
+      # memoize to false when no matches
+      @answer = result || false
+    else
+      @answer
+    end
+  end
+
+  ##
+  # @return [String,nil] The text for {#answer} or `nil` if there is none.
+  def answer_text
+    answer.text if answer
+  end
+
+  def answer_reference_identifier
+    case value
+    when /^-\d+$/
+      value.sub(/^-/, 'neg_')
+    else
+      value
+    end
+  end
+  private :answer_reference_identifier
 end
