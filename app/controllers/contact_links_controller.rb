978e732c3902e55ede4ef38d32807b383c940a37
Paul Friedman <p-friedman@northwestern.edu>
2012-09-13 14:31:30 -0500

    contact links w/ provider recruitment event
    handle contact links where the event is provider recruitment 
    an event where there is no participant
    
    #2371

--- app/controllers/contact_links_controller.rb
+++ app/controllers/contact_links_controller.rb
@@ -30,10 +30,12 @@ class ContactLinksController < ApplicationController
     @contact = @contact_link.contact
     @survey  = @response_set.survey if @response_set
 
-    @event.populate_post_survey_attributes(@contact, @response_set) if @response_set
-    @event.event_repeat_key = @event.determine_repeat_key
-
-    @event_activities = psc.activities_for_event(@event)
+    # Some events (e.g. provider recruitment do not have a participant)
+    if @event.participant
+      @event.populate_post_survey_attributes(@contact, @response_set) if @response_set
+      @event.event_repeat_key = @event.determine_repeat_key
+      @event_activities = psc.activities_for_event(@event)
+    end
 
     set_time_and_dates
     set_disposition_group
@@ -188,27 +190,24 @@ class ContactLinksController < ApplicationController
     ##
     # Disposition group based on specific events
     def set_disposition_group_for_event
-      if @event.event_disposition_category_code.to_i > 0
-	      @disposition_group =
-	        DispositionMapper.for_event_disposition_category_code(@event.event_disposition_category_code)
-	    else
-  	    case @event.event_type.to_s
-  	    when "Pregnancy Screener"
-          @disposition_group = DispositionMapper::PREGNANCY_SCREENER_EVENT
-        when "Informed Consent"
-          @disposition_group = disposition_group_for_study_arm(@event)
-        when "Low Intensity Data Collection"
-          @disposition_group = disposition_group_for_study_arm(@event)
-        when "Low to High Conversion"
-          contact = @contact_link.contact
-          if contact && contact.contact_type
-            @disposition_group = @contact_link.contact.contact_type.to_s
-          else
-            @disposition_group = DispositionMapper::GENERAL_STUDY_VISIT_EVENT
-          end
+	    case @event.event_type.to_s
+	    when "Pregnancy Screener"
+        @disposition_group = DispositionMapper::PREGNANCY_SCREENER_EVENT
+      when "Informed Consent"
+        @disposition_group = disposition_group_for_study_arm(@event)
+      when "Low Intensity Data Collection"
+        @disposition_group = disposition_group_for_study_arm(@event)
+      when "Low to High Conversion"
+        contact = @contact_link.contact
+        if contact && contact.contact_type
+          @disposition_group = @contact_link.contact.contact_type.to_s
         else
-          set_disposition_group_for_contact_link
+          @disposition_group = DispositionMapper::GENERAL_STUDY_VISIT_EVENT
         end
+      when "Provider-Based Recruitment"
+        @disposition_group = DispositionMapper::PROVIDER_RECRUITMENT_EVENT
+      else
+        set_disposition_group_for_contact_link
       end
     end
 
@@ -220,13 +219,21 @@ class ContactLinksController < ApplicationController
     ##
     # Default logic for setting of disposition group
     def set_disposition_group_for_contact_link
-      instrument = @contact_link.instrument
-      contact = @contact_link.contact
-      if contact && contact.contact_type
-        @disposition_group = @contact_link.contact.contact_type.to_s
-      end
-      if instrument && instrument.survey
-        @disposition_group = instrument.survey.title
+      # First check if the event category was selected
+      if @event.event_disposition_category_code.to_i > 0
+        @disposition_group =
+          DispositionMapper.for_event_disposition_category_code(
+            @event.event_disposition_category_code)
+      # otherwise choose from the contact or instrument
+      else
+        instrument = @contact_link.instrument
+        contact = @contact_link.contact
+        if contact && contact.contact_type
+          @disposition_group = @contact_link.contact.contact_type.to_s
+        end
+        if instrument && instrument.survey
+          @disposition_group = instrument.survey.title
+        end
       end
     end
 
