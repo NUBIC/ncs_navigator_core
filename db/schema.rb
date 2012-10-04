6bc0e95c5e4939130fd01492826cbe2893b3c331
Paul Friedman <p-friedman@northwestern.edu>
2012-10-04 13:58:24 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -773,7 +773,7 @@ ActiveRecord::Schema.define(:version => 20121003205524) do
     t.string   "transaction_type",             :limit => 36
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "sample_person_code",                         :null => false
+    t.integer  "sampled_person_code",                        :null => false
     t.integer  "pre_screening_status_code",                  :null => false
     t.string   "date_first_visit"
     t.date     "date_first_visit_date"
