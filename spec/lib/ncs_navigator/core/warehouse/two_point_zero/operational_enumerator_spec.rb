c88ed89d9e86b5ad273a77a021666bf0519e1857
Rhett Sutphin <rhett@detailedbalance.net>
2012-10-05 16:44:43 -0500

    Factor out shared groups. #2089.
    These shared groups are very tied to the implementation of the
    operational enumerator specs, so I'd be more comfortable leaving them
    inline in each spec. However, RSpec shared groups are apparently
    globally scoped after RSpec 2.6[1]. This behavior seems unfortunate
    to me, but that's a fight for another day.
    
    [1]: http://stackoverflow.com/questions/6831227/rspec-shared-examples-must-not-be-shared

--- spec/lib/ncs_navigator/core/warehouse/two_point_zero/operational_enumerator_spec.rb
+++ spec/lib/ncs_navigator/core/warehouse/two_point_zero/operational_enumerator_spec.rb
@@ -2,6 +2,7 @@
 
 
 require 'spec_helper'
+require File.expand_path('../../operational_enumerator_spec_support', __FILE__)
 
 require 'ncs_navigator/core/warehouse'
 
@@ -39,35 +40,6 @@ module NcsNavigator::Core::Warehouse::TwoPointZero
     }
     let(:producer_names) { [] }
     let(:results) { enumerator.to_a(*producer_names) }
-
-    shared_examples 'one to one' do
-      it 'creates one record per source entry' do
-        results.collect(&:class).should == [warehouse_model]
-      end
-    end
-
-    shared_context 'mapping test' do
-      before do
-        # ignore unused so we can see the mapping failures
-        OperationalEnumerator.on_unused_columns :ignore
-      end
-
-      after do
-        OperationalEnumerator.on_unused_columns :fail
-      end
-
-      def verify_mapping(core_field, core_value, wh_field, wh_value=nil)
-        wh_value ||= core_value
-        core_model.last.tap { |p| p.send("#{core_field}=", core_value) }.save!
-        results.last.send(wh_field).should == wh_value
-      end
-
-      def self.verify_mapping(core_field, core_value, wh_field, wh_value=nil, addtl_msg=nil)
-        it "maps #{core_field} to #{wh_field} #{addtl_msg}" do
-          verify_mapping(core_field, core_value, wh_field, wh_value)
-        end
-      end
-    end
 
     describe 'for ListingUnit' do
       let(:producer_names) { [:listing_units] }
