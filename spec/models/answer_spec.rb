bd4a1d9c1ca9aecb85ec7c909f29a5db25587f73
bhavi_vedula <bhavi13may@gmail.com>
2012-11-05 14:18:27 -0600

    Update Schema

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,3 +1,30 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  api_id                 :string(255)
+#  common_identifier      :string(255)
+#  common_namespace       :string(255)
+#  created_at             :datetime
+#  custom_class           :string(255)
+#  custom_renderer        :string(255)
+#  data_export_identifier :string(255)
+#  default_value          :string(255)
+#  display_length         :integer
+#  display_order          :integer
+#  display_type           :string(255)
+#  help_text              :text
+#  id                     :integer          not null, primary key
+#  is_exclusive           :boolean
+#  question_id            :integer
+#  reference_identifier   :string(255)
+#  response_class         :string(255)
+#  short_text             :text
+#  text                   :text
+#  updated_at             :datetime
+#  weight                 :integer
+#
+
 require 'spec_helper'
 
 require File.expand_path('../../shared/models/a_publicly_identified_record', __FILE__)
