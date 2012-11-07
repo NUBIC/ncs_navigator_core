ede78cd8c6a83e24a88bee102d4d8c5ec6fd0a89
Paul Friedman <p-friedman@northwestern.edu>
2012-11-07 11:25:47 -0600

    populate on preg screener (non PBS)

--- lib/ncs_navigator/core/response_set_populator/pregnancy_screener.rb
+++ lib/ncs_navigator/core/response_set_populator/pregnancy_screener.rb
@@ -3,5 +3,10 @@ require 'ncs_navigator/core'
 
 module NcsNavigator::Core::ResponseSetPopulator
   class PregnancyScreener < Base
+
+    def populate
+      # NOOP - not need to pre-populate pregnancy_screener
+    end
+
   end
 end
