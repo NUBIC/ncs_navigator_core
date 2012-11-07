f2d5ac959ce5a1d8afd4768c31beeba122f8d5ba
Paul Friedman <p-friedman@northwestern.edu>
2012-11-07 16:41:07 -0600

    Instruments edit needs fewer objects
    The instrument edit action was pulled from contact_links edit instrument.
    The contact_links controller needed more information to help user with workflow
    but the stripped down edit action needs only the instrument.
    
    Closes #2774

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -3,20 +3,15 @@
 
 class InstrumentsController < ApplicationController
 
-##
+ ##
  # Displays the form for the Instrument record
- # Posts the form to the finalize_instrument path
   def edit
-    @instrument   = Instrument.find(params[:id])
-    @person       = @instrument.person
-    @participant  = @person.participant
-    @response_set = @instrument.response_set
-    @survey       = @response_set.survey
+    @instrument = Instrument.find(params[:id])
   end
 
   ##
   # Updates the Instrument record
-  # Afterwards redirects the user to the /contact_links/:id/decision_page
+  # Afterwards redirects the user to the participant or person page
   def update
     @instrument   = Instrument.find(params[:id])
     @person       = @instrument.person
@@ -25,7 +20,9 @@ class InstrumentsController < ApplicationController
     respond_to do |format|
       if @instrument.update_attributes(params[:instrument])
 
-      	format.html { redirect_to(participant_path(@participant), :notice => 'Instrument was successfully updated.') }
+        redirect_path = @participant ? participant_path(@participant) : person_path(@person)
+
+      	format.html { redirect_to(redirect_path, :notice => 'Instrument was successfully updated.') }
         format.json { render :json => @instrument }
       else
         format.html { render :action => "edit" }
