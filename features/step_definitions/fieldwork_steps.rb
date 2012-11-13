36b804f2be4d4cdc170ed85a1c7da47a87e3925d
David Yip <yipdw@northwestern.edu>
2012-11-13 11:34:37 -0600

    Fix content negotiation for fieldwork features.
    For those watching: If you need to work with content negotiation in your
    own tests, don't do it with extensions unless it's too much of a pain to
    set the correct headers for each request.  You should, in my opinion, do
    this via the Accept (and Accept-Language) headers, as that's what they
    were designed for.
    
    (All major Web browers totally screw up those headers, yes, but that's
    irrelevant.  With API clients we can specify tighter expectations on
    this sort of thing.)

--- features/step_definitions/fieldwork_steps.rb
+++ features/step_definitions/fieldwork_steps.rb
@@ -19,7 +19,7 @@ Given /^a fieldwork packet for "([^"]*)"$/ do |id|
     """
     {}
     """
-    When I PUT the payload to /api/v1/fieldwork/#{id} with
+    When I PUT the payload to /api/v1/fieldwork/#{id}.json with
       | header:X-Client-ID | foo |
     Then the response status is 202
   }
