2eb0159093848ff4bf35380775525230f8cc8637
David Yip <yipdw@northwestern.edu>
2012-11-21 01:49:39 -0700

    Generate Psc::ScheduledActivityReports from schedule data.  #2812.
    This commit also restructures Psc::ScheduledActivityReport with a
    zero-argument initializer and methods that modify the report state,
    which helped to clean up the specs.

--- spec/models/psc/scheduled_activity_report_spec.rb
+++ spec/models/psc/scheduled_activity_report_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
-require 'stringio'
+
+require File.expand_path('../../../shared/models/logger', __FILE__)
 
 module Psc
   describe ScheduledActivityReport do
-    describe '.from_psc' do
+    let(:report) { ScheduledActivityReport.new(logger) }
+
+    describe '#populate_from_psc' do
       let(:psc) { mock }
 
       let(:filters) do
@@ -23,11 +26,28 @@ module Psc
           with(:start_date => '2012-02-01', :end_date => '2012-03-01', :state =>
                Psc::ScheduledActivity::SCHEDULED).and_return(data)
 
-        @report = ScheduledActivityReport.from_psc(psc, filters)
+        report.populate_from_psc(psc, filters)
       end
 
-      it "sets the report's filters" do
-        @report.filters.should == data['filters']
+      it 'builds a ScheduledActivityCollection' do
+        report.activities.should_not be_nil
+      end
+
+      it 'sets filters' do
+        report.filters.should == data['filters']
+      end
+    end
+
+    describe '#populate_from_schedule' do
+      let(:data_file) { File.expand_path('../../../fixtures/psc/schedule_preview.json', __FILE__) }
+      let(:data) { JSON.parse(File.read(data_file)) }
+
+      before do
+        report.populate_from_schedule(data)
+      end
+
+      it 'builds a ScheduledActivityCollection' do
+        report.activities.should_not be_nil
       end
     end
   end
