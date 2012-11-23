c32f9f77dde578f70cf7fba434e1f6a388f1bcad
David Yip <yipdw@northwestern.edu>
2012-11-22 20:44:35 -0700

    Somehow, Fieldwork#event_templates was missing.  #2812.

--- db/migrate/20121119234608_more_fieldwork_state.rb
+++ db/migrate/20121119234608_more_fieldwork_state.rb
@@ -1,11 +1,12 @@
 class MoreFieldworkState < ActiveRecord::Migration
   def up
-    add_column :fieldworks, :generated_for, :string
     add_column :fieldworks, :contact_links, :text
     add_column :fieldworks, :contacts, :text
+    add_column :fieldworks, :event_templates, :text
     add_column :fieldworks, :events, :text
-    add_column :fieldworks, :instruments, :text
+    add_column :fieldworks, :generated_for, :string
     add_column :fieldworks, :instrument_plans, :text
+    add_column :fieldworks, :instruments, :text
     add_column :fieldworks, :people, :text
     add_column :fieldworks, :surveys, :text
   end
@@ -13,11 +14,12 @@ class MoreFieldworkState < ActiveRecord::Migration
   def down
     remove_column :fieldworks, :surveys
     remove_column :fieldworks, :people
-    remove_column :fieldworks, :instrument_plans
     remove_column :fieldworks, :instruments
+    remove_column :fieldworks, :instrument_plans
+    remove_column :fieldworks, :generated_for
     remove_column :fieldworks, :events
+    remove_column :fieldworks, :event_templates
     remove_column :fieldworks, :contacts
     remove_column :fieldworks, :contact_links
-    remove_column :fieldworks, :generated_for
   end
 end
