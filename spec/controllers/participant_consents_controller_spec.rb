387312c89c172009ad3c10b605fe063324f50c06
Paul Friedman <p-friedman@northwestern.edu>
2012-12-04 15:33:52 -0600

    Create an Informed Consent Event #2709
    When a ParticipantConsent record is created, create an
    Informed Consent Event behind the scenes.

--- spec/controllers/participant_consents_controller_spec.rb
+++ spec/controllers/participant_consents_controller_spec.rb
@@ -11,12 +11,8 @@ describe ParticipantConsentsController do
       :consent_type_code               => 1,
       :consent_form_type_code          => 1,
       :consent_given_code              => 1,
-      :consent_withdraw_code           => 2,
-      :consent_withdraw_type_code      => 1,
-      :consent_withdraw_reason_code    => 2,
       :consent_language_code           => 1,
       :who_consented_code              => 2,
-      :who_wthdrw_consent_code         => 2,
       :consent_translate_code          => 1,
       :reconsideration_script_use_code => 1,
       :consent_version                 => "1.2"
@@ -26,9 +22,12 @@ describe ParticipantConsentsController do
   context "with an authenticated user" do
 
     let(:mother_person) { Factory(:person) }
-    let(:mother) { Factory(:participant) }
+    let(:mother) { Factory(:participant, :enroll_status_code => nil) }
     let(:event) { Factory(:event, :participant => mother) }
-    let(:contact_link) { Factory(:contact_link, :event => event, :instrument => nil) }
+    let(:contact) { Factory(:contact) }
+    let(:contact_link) { Factory(:contact_link, :event => event,
+                                 :instrument => nil, :contact => contact,
+                                 :person => mother_person) }
 
     before(:each) do
       login(user_login)
@@ -57,7 +56,95 @@ describe ParticipantConsentsController do
         get :new, :contact_link_id => contact_link.id, :participant_id => mother.id
         assigns[:participant_consent].participant_consent_samples.size.should == 3
       end
+    end
 
+    describe "POST create" do
+      describe "with valid params" do
+        describe "with html request" do
+          it "creates a new ParticipantConsent" do
+            expect {
+              post :create, :contact_link_id => contact_link.id,
+                   :participant_consent => valid_attributes.merge({:participant_id => mother.id})
+            }.to change(ParticipantConsent, :count).by(1)
+          end
+
+          it "assigns a newly created ParticipantConsent as @participant_consent" do
+            post :create, :contact_link_id => contact_link.id,
+                 :participant_consent => valid_attributes.merge({:participant_id => mother.id})
+            assigns(:participant_consent).should be_a(ParticipantConsent)
+            assigns(:participant_consent).should be_persisted
+          end
+
+          it "redirects to the contact link decision page" do
+            post :create, :contact_link_id => contact_link.id,
+                 :participant_consent => valid_attributes.merge({:participant_id => mother.id})
+            response.should redirect_to(decision_page_contact_link_path(contact_link))
+          end
+
+          it "updates the participant into a enrolled state" do
+            mother.should_not be_enrolled
+            post :create, :contact_link_id => contact_link.id,
+                 :participant_consent => valid_attributes.merge({:participant_id => mother.id})
+            Participant.find(mother.id).should be_enrolled
+          end
+
+          describe "Informed Consent event" do
+            it "creates an Informed Consent Event record and associates it with the contact link" do
+              ic = Event.where(:participant_id => mother.id, :event_type_code => Event.informed_consent_code).first
+              ic.should be_nil
+
+              post :create, :contact_link_id => contact_link.id,
+                   :participant_consent => valid_attributes.merge({:participant_id => mother.id})
+
+              ic = Event.where(:participant_id => mother.id, :event_type_code => Event.informed_consent_code).last
+              ic.should_not be_nil
+              ic_cl = ic.contact_links.first
+              ic_cl.contact.should  == contact_link.contact
+              ic_cl.staff_id.should == contact_link.staff_id
+              ic_cl.person.should   == contact_link.person
+            end
+
+            it "is created only once for the participant for any particular contact" do
+              ic = Event.where(:participant_id => mother.id, :event_type_code => Event.informed_consent_code).first
+              ic.should be_nil
+
+              3.times do
+                post :create, :contact_link_id => contact_link.id,
+                     :participant_consent => valid_attributes.merge({:participant_id => mother.id})
+              end
+
+              Event.where(:participant_id => mother.id,
+                          :event_type_code => Event.informed_consent_code).count.should == 1
+            end
+
+            it "is created for the participant for each individual contact" do
+              ic = Event.where(:participant_id => mother.id, :event_type_code => Event.informed_consent_code).first
+              ic.should be_nil
+
+              3.times do
+                post :create, :contact_link_id => contact_link.id,
+                     :participant_consent => valid_attributes.merge({:participant_id => mother.id})
+              end
+
+              Event.where(:participant_id => mother.id,
+                          :event_type_code => Event.informed_consent_code).count.should == 1
+
+              new_contact = Factory(:contact)
+              new_contact_link = Factory(:contact_link,
+                                         :event => event,
+                                         :contact => new_contact,
+                                         :person => mother_person)
+
+              post :create, :contact_link_id => new_contact_link.id,
+                   :participant_consent => valid_attributes.merge({:participant_id => mother.id})
+
+              Event.where(:participant_id => mother.id,
+                          :event_type_code => Event.informed_consent_code).count.should == 2
+            end
+
+          end
+        end
+      end
     end
 
     describe "GET new_child" do
@@ -104,7 +191,7 @@ describe ParticipantConsentsController do
               :contact_link_id => contact_link.id, :participant_id => mother.id,
               :participant_consent => valid_attributes,
               :person => { :first_name => "fn", :last_name => "ln"}
-          }.to change(Person, :count).by(2) # the creation of the mother and the child
+          }.to change(Person, :count).by(1)
           child = Person.last
           child.first_name.should == "fn"
           child.last_name.should == "ln"
