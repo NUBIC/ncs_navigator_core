bb8f821589809c99f8c0c7de970bd249aa8c6d5e
Steven Gallagher <s.thomas.gallagher@gmail.com>
2012-12-14 14:35:45 -0600

    temporary fix to get the CI tests to pass. new implementation of a solution to the problems posited in Change #2761 is in the works

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -3,14 +3,14 @@
 module PeopleHelper
 
   def unique_contact_mode_entries(contact_mode_entries)
-    filter_criteria = contact_mode_entries.first.filter_criteria
+    filter_criteria = contact_mode_entries.first.filter_criteria if !contact_mode_entries.first.nil?
     contact_mode_entries.uniq_by(&filter_criteria)
   end
 
   def highest_ranking_contact_mode_entry(contact_mode_entries)
     highest_ranking = []
-    type_code = contact_mode_entries.first.type_code
-    rank_code = contact_mode_entries.first.rank_code
+    type_code = contact_mode_entries.first.type_code if !contact_mode_entries.first.nil?
+    rank_code = contact_mode_entries.first.rank_code if !contact_mode_entries.first.nil?
     gt = lambda { |new_rank, old_rank| ranks = [1, 2, -5, 4, -4]; ranks.index(new_rank) < ranks.index(old_rank) }
     highest_ranking_hash = contact_mode_entries.each_with_object({}) { |entries, h| c = h[entries.send(type_code)]; h[entries.send(type_code)] = entries if !c || gt[entries.send(rank_code), c.send(rank_code)] }
     highest_ranking_hash.each_value { |entry| highest_ranking << entry }
