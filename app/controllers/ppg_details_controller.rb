1f25573d2950b4e1ea3780dfaf658b99e04c9e44
Steven Gallagher <s.thomas.gallagher@gmail.com>
2013-01-04 09:58:53 -0600

    deleted unused code from controller

--- app/controllers/ppg_details_controller.rb
+++ app/controllers/ppg_details_controller.rb
@@ -1,14 +1,13 @@
 class PpgDetailsController < ApplicationController
 
   def edit
-    params.each_pair { |k, v| p "key = #{k}, value = #{v}"}
     @participant = Participant.find(params[:id])
     @person = @participant.person
     @ppg_detail = @participant.ppg_details.first
 
     respond_to do |format|
       format.html
-      format.json { render :json => @email }
+      format.json { render :json => @ppg_detail }
     end
   end
 
@@ -28,16 +27,5 @@ class PpgDetailsController < ApplicationController
       end
     end
   end
-
-  private
-
-    def load_participant
-      return unless params[:id]
-      @participant =
-        Participant.find_by_id(params[:id]) ||
-        Participant.find_by_p_id(params[:id]) ||
-        Person.includes(:participant_person_links => :participant).where(:person_id => params[:id]).first.try(:participant) ||
-        raise(ActiveRecord::RecordNotFound, "Couldn't find Participant with id=#{params[:id]} or p_id=#{params[:id]} or self person_id=#{params[:id]}")
-    end
 
 end
