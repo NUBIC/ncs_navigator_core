f3c57e28fdaa17546e8a2f2da4ae1a63c151c6af
Steven Gallagher <s.thomas.gallagher@gmail.com>
2013-01-04 13:38:23 -0600

    corrected controller variable sources, paths and parameters associated with the paths

--- app/controllers/ppg_details_controller.rb
+++ app/controllers/ppg_details_controller.rb
@@ -1,9 +1,9 @@
 class PpgDetailsController < ApplicationController
 
   def edit
-    @participant = Participant.find(params[:id])
+    @participant = Participant.find(params[:participant_id])
     @person = @participant.person
-    @ppg_detail = @participant.ppg_details.first
+    @ppg_detail = PpgDetail.find(params[:id])
 
     respond_to do |format|
       format.html
@@ -12,14 +12,14 @@ class PpgDetailsController < ApplicationController
   end
 
   def update
-    @participant = Participant.find(params[:id])
+    @participant = Participant.find(params[:participant_id])
     @person = @participant.person
-    @ppg_details = @participant.ppg_details.first
+    @ppg_detail = PpgDetail.find(params[:id])
 
     respond_to do |format|
-      if @ppg_details.update_attributes(params[:orig_due_date])
+      if @ppg_detail.update_attributes(params[:ppg_detail])
         flash[:notice] = 'Original Due Date was successfully updated.'
-        format.html { redirect_to(participant_path) }
+        format.html { redirect_to(participant_path(@participant.id)) }
         format.json  { render :json => @ppg_details }
       else
         format.html { render :action => "edit" }
