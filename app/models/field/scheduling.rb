9526849fcd82462306d82d7fa011505e394e02f1
David Yip <yipdw@northwestern.edu>
2013-01-11 13:38:19 -0600

    Add participant scheduling to merge process.  #3128.
    I justify the lack of tests by noting that all this code does is glue
    stuff together.  Hopefully this won't come back to bite me.

---
+++ app/models/field/scheduling.rb
@@ -0,0 +1,23 @@
+module Field
+  ##
+  # Methods for scheduling participants.
+  #
+  # Expects its host object to respond to:
+  #
+  # * {#login_to_psc} with a {PatientStudyCalendar} instance
+  # * {#current_participants} with a list of {Participant} objects
+  module Scheduling
+    include PscSync
+
+    def advance_participant_schedules
+      psc = login_to_psc
+
+      # Ensure we have up-to-date data; we're entering this method out of a
+      # lot of manipulations.
+      current_participants.each(&:reload)
+
+      # Advance.
+      current_participants.each { |p| p.advance(psc) }
+    end
+  end
+end
