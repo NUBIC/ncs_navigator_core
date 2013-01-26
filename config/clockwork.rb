99b93bacb8ac8d8ed4d66d1d0028cf49d1fb1506
David Yip <yipdw@northwestern.edu>
2013-01-25 19:53:53 -0600

    Clockwork configuration.  #3127.

---
+++ config/clockwork.rb
@@ -0,0 +1,40 @@
+# This is the Clockwork configuration file for a Cases instance.
+#
+# While you MAY refer to Cases application libraries in this file, you SHOULD
+# NOT load any files that require the Rails application environment to be
+# loaded.
+
+# Make libraries and workers available.
+$LOAD_PATH.unshift(File.expand_path('../../app/workers', __FILE__))
+$LOAD_PATH.unshift(File.expand_path('../../lib', __FILE__))
+
+require 'clockwork'
+require 'ncs_navigator/core'
+require 'pathname'
+require 'watchdog_worker'
+
+LOG = Logger.new($stderr)
+
+# The bare necessities.
+module Rails
+  module_function
+
+  def env
+    (ENV['RAILS_ENV'] || 'development').downcase
+  end
+
+  def root
+    Pathname.new(File.expand_path('../..', __FILE__))
+  end
+
+  def logger
+    LOG
+  end
+end
+
+include Clockwork
+include NcsNavigator::Core::SidekiqConfiguration
+include NcsNavigator::Core::WorkerWatchdog
+
+handler { |job| job.perform_async }
+sidekiq_configure_client
