a2e91ad371ad346122ab473b881d9ba94ce87753
David Yip <yipdw@northwestern.edu>
2013-01-29 12:52:27 -0600

    Eliminate application dependencies from RedisConfiguration.  #3127.

--- lib/ncs_navigator/core/redis_configuration.rb
+++ lib/ncs_navigator/core/redis_configuration.rb
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
+require 'bcdatabase'
+require 'erb'
 require 'ncs_navigator/core'
+require 'redis'
+require 'yaml'
 
+##
+# Configuration for Cases' Redis instance.
+#
+# This module is used by Cases' scheduler and therefore MUST be usable without
+# the Rails application environment loaded.  This means:
+#
+# * limited usage of the Rails module: .env, .root, and .logger is all you get
+# * no dependencies on ActiveSupport's autoloading code
+# * no direct or indirect references to configured ActiveRecord objects
 module NcsNavigator::Core::RedisConfiguration
   attr_reader :redis
 
@@ -25,34 +38,37 @@ module NcsNavigator::Core::RedisConfiguration
     @redis_options = opts.symbolize_keys
   end
 
+  private
+
   def redis_connection
     Rails.logger.info "Connecting to redis using #{redis_options.inspect}"
     Redis.new(redis_options)
   end
-  private :redis_connection
+
+  def redis_configurations
+    db_config = File.read(Rails.root.join("config/database.yml"))
+
+    @redis_configurations ||= YAML.load(ERB.new(db_config).result(binding))
+  end
 
   def default_redis_options
-    ActiveRecord::Base.configurations["redis_#{Rails.env}"] or
+    redis_configurations["redis_#{Rails.env}"] or
       fail("No redis options for #{Rails.env} environment")
   end
-  private :default_redis_options
 
   def redis_host
     redis_options[:host] || '127.0.0.1'
   end
-  private :redis_host
 
   def redis_port
     redis_options[:port] || 6379
   end
-  private :redis_port
 
   def redis_db
     redis_options[:db] || 0
   end
-  private :redis_db
 end
 
-if defined?(Rails)
+if defined?(Rails.application)
   Rails.application.send(:extend, NcsNavigator::Core::RedisConfiguration)
 end
