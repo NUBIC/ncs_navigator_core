373530d31d7e226fb28c6a4539c48321e67114ef
Paul Friedman <p-friedman@northwestern.edu>
2013-02-05 16:15:37 -0600

    Use known date time in test data #2374

--- spec/controllers/sample_receipt_stores_controller_spec.rb
+++ spec/controllers/sample_receipt_stores_controller_spec.rb
@@ -11,9 +11,9 @@ describe SampleReceiptStoresController do
 
 
     def valid_attributes
-      { :sample_id => @sample.id, :sample_receipt_shipping_center_id => "123ABC", 
-        :staff_id => "someone special", :receipt_datetime =>Time.now, 
-        :placed_in_storage_datetime =>Time.now}
+      { :sample_id => @sample.id, :sample_receipt_shipping_center_id => "123ABC",
+        :staff_id => "someone special", :receipt_datetime => DateTime.parse('2012-12-25 12:00:00'),
+        :placed_in_storage_datetime => DateTime.parse('2012-12-25 12:00:00')}
     end
 
     describe "GET new" do
@@ -22,13 +22,13 @@ describe SampleReceiptStoresController do
         assigns(:sample_receipt_store).should be_a_new(SampleReceiptStore)
       end
     end
-    
+
     describe "GET edit" do
       it "assigns the requested sample_receipt_store as @sample_receipt_store" do
         get :edit, :id => @sample_receipt_store.sample_id.to_s
         assigns(:sample_receipt_store).should eq(@sample_receipt_store)
       end
-    end    
+    end
 
     describe "POST create" do
       describe "with valid params" do
@@ -43,7 +43,7 @@ describe SampleReceiptStoresController do
           assigns(:sample_receipt_store).should be_a(SampleReceiptStore)
           assigns(:sample_receipt_store).should be_persisted
         end
-        
+
         describe "with json request" do
           describe "with not repeating sample_receipt_store" do
             it "creates a new SampleReceiptStore" do
@@ -53,8 +53,8 @@ describe SampleReceiptStoresController do
             end
           end
         end
-        
-        describe "forms json" do 
+
+        describe "forms json" do
           it "with newly created @sample_receipt_store id" do
             post :create, :sample_receipt_store => valid_attributes, :format => 'json'
             sample_receipt_store = SampleReceiptStore.last
@@ -78,9 +78,9 @@ describe SampleReceiptStoresController do
           json = { "placed_in_storage_datetime"=>["can't be blank"], "receipt_datetime"=>["can't be blank"]}
           ActiveSupport::JSON.decode(response.body).should eq json
         end
-      end      
+      end
     end
-    
+
     describe "PUT update" do
       describe "with json request and date change" do
         it "forms json with updated @sample_receipt_store storage_container_id" do
@@ -88,15 +88,15 @@ describe SampleReceiptStoresController do
           sample_receipt_store = SampleReceiptStore.last
           response.body.should eq sample_receipt_store.to_json
         end
-      end  
-      
+      end
+
       describe "with json request" do
         it "forms json with updated @sample_receipt_store" do
           put :update, :id => @sample_receipt_store.id, :sample_receipt_store => {:receipt_datetime => nil}, :format => 'json'
           json = { "receipt_datetime"=>["can't be blank"]}
           ActiveSupport::JSON.decode(response.body).should eq json
         end
-      end      
+      end
     end
   end
 end
