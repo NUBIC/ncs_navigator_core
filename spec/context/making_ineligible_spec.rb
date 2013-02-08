7cb1ef2c58fc41ab3f78aec8e985e14eaffd93eb
Steven Gallagher <s.thomas.gallagher@gmail.com>
2013-02-08 10:37:51 -0600

    updated testing to reflect changes in implementation: events are not deleted, participant record is deleted, Change #2828

--- spec/context/making_ineligible_spec.rb
+++ spec/context/making_ineligible_spec.rb
@@ -1,31 +1,39 @@
 # -*- coding: utf-8 -*-
 
-
 require 'spec_helper'
 
 describe MakingIneligible do
   before do
-    person = Factory(:person)
+    person =      Factory(:person)
+    father =      Factory(:person)
+    grandparent = Factory(:person)
     participant = Factory(:participant)
     provider = Factory(:provider)
     Factory(:person_provider_link, :person_id => person.id, :provider_id => provider.id)
+    Factory(:participant_person_link, :person_id => father.id, :participant_id => participant.id, :relationship_code => 4)
+    Factory(:participant_person_link, :person_id => grandparent.id,:participant_id => participant.id, :relationship_code => 10)
     participant.person= person
     participant.save!
     @response_set = Factory(:response_set, :user_id => person.id, :participant_id => participant.id)
-    @response_set.participant.person
     @event1 = Factory(:event, :participant_id => @response_set.participant.id)
     @event2 = Factory(:event, :participant_id => @response_set.participant.id)
   end
 
-  it "deletes all participant_person_links for a particular participant person combination" do
+  it "deletes all participant_person_links for a participant" do
     MakingIneligible.make_ineligible(@response_set)
-    @response_set.person.participant.should be_nil
+    ParticipantPersonLink.where(:participant_id => @response_set.participant).all.size.should be 0
   end
 
   it "sets the participant_id to nil for all events associated with the person" do
     participant_id =  @response_set.participant.id
     MakingIneligible.make_ineligible(@response_set)
-    Event.where(:participant_id => participant_id).count.should be(0)
+    Event.where(:participant_id => participant_id).count.should be 0
+  end
+
+  it "does not delete the event records" do
+    participant_id =  @response_set.participant.id
+    MakingIneligible.make_ineligible(@response_set)
+    @event1.should_not be_nil
   end
 
   it "sets the participant_id to nil for the response set" do
@@ -34,13 +42,19 @@ describe MakingIneligible do
   end
 
   it "calls #create_from_particpant! from the SampledPersonsIneligibility class" do
-    SampledPersonsIneligibility.should_receive(:create_from_participant!).with(@response_set.participant).and_return(an_instance_of(SampledPersonsIneligibility))
+    SampledPersonsIneligibility.should_receive(:create_from_person!)
+                               .with(@response_set.person)
+                               .and_return(an_instance_of(SampledPersonsIneligibility))
     MakingIneligible.make_ineligible(@response_set)
   end
 
   it "creates an ineligibility record" do
     MakingIneligible.make_ineligible(@response_set)
-    SampledPersonsIneligibility.count.should == 1
+    SampledPersonsIneligibility.count.should be 1
   end
 
+  it "deletes participant record" do
+    MakingIneligible.make_ineligible(@response_set)
+    Participant.count.should be 0
+  end
 end
