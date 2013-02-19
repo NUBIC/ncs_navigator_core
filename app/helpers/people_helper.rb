b6fc42dc5d3590bc0703dda93a0abdfd3745849c
Lockom <fln612@dhcp-165-124-223-169.nubic.northwestern.edu>
2013-02-19 15:52:20 -0600

    blanksafe on state because it is a reqired field in the create/edit #3291

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,37 +1,25 @@
 # -*- coding: utf-8 -*-
 
 module PeopleHelper
-
-  # @note This removes addresses with the same :address_one, possibly in error
-  def unique_contact_mode_entries(contact_mode_entries)
-    return [] if contact_mode_entries == nil || contact_mode_entries.first == nil
-    filter_criteria = contact_mode_entries.first.filter_criteria if !contact_mode_entries.first.nil?
-    contact_mode_entries.uniq_by(&filter_criteria)
-  end
-
-  def highest_ranking_contact_mode_entry(contact_mode_entries)
-    return [] if contact_mode_entries == nil || contact_mode_entries.first == nil
-    highest_ranking = []
-    type_code = contact_mode_entries.first.type_code if !contact_mode_entries.first.nil?
-    rank_code = contact_mode_entries.first.rank_code if !contact_mode_entries.first.nil?
-    gt = lambda { |new_rank, old_rank| ranks = [1, 2, -5, 4, -4]; ranks.index(new_rank).to_i < ranks.index(old_rank).to_i }
-    highest_ranking_hash = contact_mode_entries.each_with_object({}) { |entries, h| c = h[entries.send(type_code)]; h[entries.send(type_code)] = entries if !c || gt[entries.send(rank_code), c.send(rank_code)] }
-    highest_ranking_hash.each_value { |entry| highest_ranking << entry }
-    highest_ranking
-  end
-  
-  # ranks sorted by (1)primary, (2)secondary, (-5)other, (4)duplicate, (3)Invalid, (-4) missing in error
+  # @param contact_mode_entries array of Addresses, Phones or Emails or anyting that implements
+  #        rank_code and type_code
+  # @note ranks sorted by (1)primary, (2)secondary, (-5)other, (4)duplicate, (3)Invalid, (-4) missing in error
+  # @note might want to sort by creation time after rank
   # @return type-sorted hash of non-empty rank-sorted contact arrays.
   def sort_contact_mode_entries(contact_mode_entries)
     return {} if contact_mode_entries == nil || contact_mode_entries.first == nil
-    type_code = contact_mode_entries.first.type_code if !contact_mode_entries.first.nil?
-    rank_code = contact_mode_entries.first.rank_code if !contact_mode_entries.first.nil?
+    type_code = contact_mode_entries.first.type_code
+    rank_code = contact_mode_entries.first.rank_code 
     ranks = [1, 2, -5, 4, 3, -4]
-    out = {1=>[], 2=>[], 3=>[], 4=>[], 5=>[], -5=>[], -6=>[], -4=>[]}
+    # union of {ADDRESS,EMAIL,PHONE}_TYPE
+    out = {1=>[], 2=>[], 3=>[], 4=>[], 5=>[], -5=>[], -6=>[], -4=>[], -1=>[]}    
     
-    contact_mode_entries.each {|e| out[e.send(type_code)] << e}
-    out.delete_if {|k,v| v == []}
-    #should add secondary sort by created/updated time
+    contact_mode_entries.each do |e| 
+      out[e.send(type_code)] = [] unless out.has_key?(e.send(type_code))
+      out[e.send(type_code)] << e
+    end
+
+    out.delete_if {|k,v| v == []}    
     out.each_value do |type| 
       type.sort!{|a,b| ranks.index(a.send(rank_code))<=>ranks.index(b.send(rank_code))}
     end
