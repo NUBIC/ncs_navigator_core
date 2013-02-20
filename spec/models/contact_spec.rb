df8cdba12aad93e8a799b8f52e6d38021759dba9
Paul Friedman <p-friedman@northwestern.edu>
2013-02-20 14:12:20 -0600

    Added events association #3389
    to contact through the contact_links association

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -56,6 +56,7 @@ describe Contact do
   it { should belong_to(:who_contacted) }
 
   it { should have_many(:contact_links) }
+  it { should have_many(:events).through(:contact_links) }
   it { should have_many(:instruments).through(:contact_links) }
   it { should have_one(:non_interview_provider) }
   it { should have_many(:non_interview_reports) }
