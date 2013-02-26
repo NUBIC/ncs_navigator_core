f64149dd3f9b754b4782ba6a646940611e57d0e8
Paul Friedman <p-friedman@northwestern.edu>
2013-02-26 13:33:43 -0600

    Prevent editing event_type #3412
    This commit updates the event edit form to prevent the editing
    of the event_type attribute. Also this change includes the ability
    to update the event_start_date without any additional steps. #3411
    
    I have kept/added the ability to update the psc_ideal_date but this
    is only for system administrators. The reason behind this is to allow
    sys_admins to easily update this attribute in order to correct any
    broken PSC associations.
    
    Closes #3412

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,9 +1,25 @@
 $(document).ready(function() {
 
+  $("#enable_event_psc_ideal_date").click(function() {
+    msg = "Updating the ideal date should only be performed by System Administrators " +
+          "and under the rarest of circumstances.\nAltering the event ideal date might " +
+          "break the event association in PSC."
+    alert(msg);
+    $("#event_psc_ideal_date").removeAttr('disabled');
+    $('#disable_event_psc_ideal_date').show();
+    $('#enable_event_psc_ideal_date').hide();
+  });
+
+  $("#disable_event_psc_ideal_date").click(function() {
+    $("#event_psc_ideal_date").attr('disabled','disabled');
+    $('#enable_event_psc_ideal_date').show();
+    $('#disable_event_psc_ideal_date').hide();
+  });
+
   $("#enable_event_date_attributes").click(function() {
     $("#event_event_end_date").removeAttr('disabled');
     $("#event_event_end_time").removeAttr('disabled');
-    $('#disable_event_date_attributes').show();    
+    $('#disable_event_date_attributes').show();
     $('#enable_event_date_attributes').hide();
   });
 
@@ -11,33 +27,7 @@ $(document).ready(function() {
     $("#event_event_end_date").attr('disabled','disabled');
     $("#event_event_end_time").attr('disabled','disabled');
     $('#enable_event_date_attributes').show();
-    $('#disable_event_date_attributes').hide();    
-  });
-
-  $("#enable_event_type_code").click(function() {
-    alert("Altering the event type might break the event association in PSC.")
-    $("#event_event_type_code").removeAttr('disabled');
-    $('#disable_event_type_code').show();    
-    $('#enable_event_type_code').hide();
-  });
-
-  $("#disable_event_type_code").click(function() {
-    $("#event_event_type_code").attr('disabled','disabled');
-    $('#enable_event_type_code').show();
-    $('#disable_event_type_code').hide();    
-  });
-
-  $("#enable_event_start_date").click(function() {
-    alert("Altering the event start date might break the event association in PSC.")
-    $("#event_event_start_date").removeAttr('disabled');
-    $('#disable_event_start_date').show();
-    $('#enable_event_start_date').hide();    
-  });
-
-  $("#disable_event_start_date").click(function() {
-    $("#event_event_start_date").attr('disabled','disabled');
-    $('#enable_event_start_date').show();
-    $('#disable_event_start_date').hide();    
+    $('#disable_event_date_attributes').hide();
   });
 
 });
