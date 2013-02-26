257f9300462d29b4ad99b37be229fc4094227f97
Paul Friedman <p-friedman@northwestern.edu>
2013-02-26 14:19:15 -0600

    Removing psc_ideal_date from view #3411
    If this attribute needs to be updated we can do it on the backend.

--- app/assets/javascripts/event.js
+++ app/assets/javascripts/event.js
@@ -1,21 +1,5 @@
 $(document).ready(function() {
 
-  $("#enable_event_psc_ideal_date").click(function() {
-    msg = "Updating the ideal date should only be performed by System Administrators " +
-          "and under the rarest of circumstances.\nAltering the event ideal date might " +
-          "break the event association in PSC."
-    alert(msg);
-    $("#event_psc_ideal_date").removeAttr('disabled');
-    $('#disable_event_psc_ideal_date').show();
-    $('#enable_event_psc_ideal_date').hide();
-  });
-
-  $("#disable_event_psc_ideal_date").click(function() {
-    $("#event_psc_ideal_date").attr('disabled','disabled');
-    $('#enable_event_psc_ideal_date').show();
-    $('#disable_event_psc_ideal_date').hide();
-  });
-
   $("#enable_event_date_attributes").click(function() {
     $("#event_event_end_date").removeAttr('disabled');
     $("#event_event_end_time").removeAttr('disabled');
