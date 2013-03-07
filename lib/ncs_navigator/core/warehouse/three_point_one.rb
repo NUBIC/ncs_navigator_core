0d795fd618dae479a8f15a1645d5b64126fd06f8
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-03-07 13:46:48 -0600

    Add enumerator for MDES 3.1. #3175.

---
+++ lib/ncs_navigator/core/warehouse/three_point_one.rb
@@ -0,0 +1,7 @@
+require 'ncs_navigator/core'
+
+module NcsNavigator::Core::Warehouse
+  module ThreePointOne
+    autoload :OperationalEnumerator, 'ncs_navigator/core/warehouse/three_point_one/operational_enumerator'
+  end
+end
