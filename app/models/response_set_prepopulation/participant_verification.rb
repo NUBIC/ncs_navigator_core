8288963fee48916ff4e870d82d08298486df0d14
John Dzak <j-dzak@northwestern.edu>
2013-03-12 12:03:36 -0500

    Adapted participant verification response set prepoulator spec to new api. #2744

--- app/models/response_set_prepopulation/participant_verification.rb
+++ app/models/response_set_prepopulation/participant_verification.rb
@@ -1,5 +1,5 @@
 module ResponseSetPrepopulation
-  class ParticipantVerification
+  class ParticipantVerification < NonInterview
   	def reference_identifiers
       [
         "prepopulated_mode_of_contact",
