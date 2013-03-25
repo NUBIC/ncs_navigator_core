67bf192e57338e755ebc12587bcf275ab5b14bde
Piotr Hebal <piotr.hebal@northwestern.edu>
2013-03-25 13:54:30 -0500

    2.0 -> 2.1 migration scripts #2090

--- lib/ncs_navigator/core/mdes/version_migrations.rb
+++ lib/ncs_navigator/core/mdes/version_migrations.rb
@@ -4,5 +4,6 @@ module NcsNavigator::Core::Mdes
   module VersionMigrations
     autoload :Basic,               'ncs_navigator/core/mdes/version_migrations/basic'
     autoload :ThreeZeroToThreeTwo, 'ncs_navigator/core/mdes/version_migrations/three_zero_to_three_two'
+    autoload :TwoZeroToTwoOne,     'ncs_navigator/core/mdes/version_migrations/two_zero_to_two_one'
   end
 end
