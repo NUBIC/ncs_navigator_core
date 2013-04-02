670273a659d6c6dee3ab88cef43430f254d16783
Nataliya Shurupova <n-shurupova@northwestern.edu>
2013-04-02 16:27:38 -0500

    Audited sample/specimen models to properly declare the codes.
    #3764

--- app/models/specimen_storage.rb
+++ app/models/specimen_storage.rb
@@ -28,7 +28,6 @@ class SpecimenStorage < ActiveRecord::Base
   include NcsNavigator::Core::Mdes::MdesRecord
   acts_as_mdes_record :public_id_field => :specimen_storage_container_id 
 
-  belongs_to :psu, :conditions => "list_name = 'PSU_CL1'", :foreign_key => "psu_code", :class_name => 'NcsCode', :primary_key => :local_code
   belongs_to :specimen_processing_shipping_center
   belongs_to :specimen_equipment
   belongs_to :specimen_storage_container
@@ -36,6 +35,8 @@ class SpecimenStorage < ActiveRecord::Base
   validates_presence_of :specimen_storage_container_id
   validates_presence_of :placed_in_storage_datetime
   validates_presence_of :staff_id 
+
   ncs_coded_attribute :master_storage_unit,       'STORAGE_AREA_CL1'
+  ncs_coded_attribute :psu,                       'PSU_CL1'
 end
 
