378216075b4ba9f92dc00355b18818927be5e758
Nataliya Shurupova <n-shurupova@northwestern.edu>
2013-04-03 11:01:58 -0500

    Fixed failing specs for audited sample/specimen models. #3764.

--- spec/models/specimen_pickup_spec.rb
+++ spec/models/specimen_pickup_spec.rb
@@ -26,8 +26,7 @@ describe SpecimenPickup do
     specimen_pickup = Factory(:specimen_pickup)
     specimen_pickup.should_not be_nil
   end
-  
-  it { should belong_to(:psu) }
+
   it { should belong_to(:event) }
   it { should belong_to(:specimen_processing_shipping_center) } 
 end
--- spec/models/specimen_processing_shipping_center_spec.rb
+++ spec/models/specimen_processing_shipping_center_spec.rb
@@ -20,10 +20,9 @@ describe SpecimenProcessingShippingCenter do
     specimen_processing_shipping_center = Factory(:specimen_processing_shipping_center)
     specimen_processing_shipping_center.should_not be_nil
   end
-  
+
   it { should belong_to(:address) }
-  it { should belong_to(:psu) } 
-  
+
   context "as mdes record" do
     it "has the srsc_id" do
       spsc = Factory(:specimen_processing_shipping_center)
@@ -36,6 +35,6 @@ describe SpecimenProcessingShippingCenter do
       obj = SpecimenProcessingShippingCenter.find(spsc.id)
       obj.psu.local_code.should == 20000030
     end
-  end  
+  end
 end
 
