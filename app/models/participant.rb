6b47194ccd35b9bf6bb45261620af7e0a086f905
Paul Friedman <p-friedman@northwestern.edu>
2013-04-03 16:44:30 -0500

    Reverse commit 5e78524759e
    5e78524759e erroneously got committed into the
    master branch and tagged for deployment. This
    commit reverses that error.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -362,14 +362,7 @@ class Participant < ActiveRecord::Base
     !self.children.blank?
   end
 
-  ##
-  # Use the given event or the most recent closed event
-  # to determine the next participant state.
-  #
-  # @see Participant#update_state_to_next_event
-  # @see Event#closed
-  # @see Event#chronological
-  def advance(psc, event = events.closed.chronological.last)
+  def advance(psc, event = events.chronological.last)
     if self.pending_events.blank?
       update_state_to_next_event(event)
       Event.schedule_and_create_placeholder(psc, self)
