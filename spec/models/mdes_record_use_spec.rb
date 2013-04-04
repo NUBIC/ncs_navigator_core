3fcef52eb2dae0e4e0775342d56bb677238fd3a4
Rhett Sutphin <rhett@detailedbalance.net>
2013-04-04 00:48:50 -0500

    Add more coded attribute checks. #3764.

--- spec/models/mdes_record_use_spec.rb
+++ spec/models/mdes_record_use_spec.rb
@@ -18,5 +18,31 @@ describe 'Every MdesRecord model in Cases' do
         errors.should == []
       end
     end
+
+    it 'has a corresponding column' do
+      errors = NcsNavigator::Core::Mdes::MdesRecord.models.collect do |m|
+        code_columns = m.columns.collect(&:name).grep(/_code\z/)
+        nca_fks = m.ncs_coded_attributes.values.collect(&:foreign_key_name)
+        no_column = nca_fks - code_columns
+
+        no_column.collect { |missing_column| "#{m} has a coded attribute declaration for #{missing_column} but that column is not present in the model" }
+      end.flatten
+
+      errors.should == []
+    end
+  end
+
+  describe 'each _code column' do
+    it 'has an ncs_coded_attribute declaration' do
+      errors = NcsNavigator::Core::Mdes::MdesRecord.models.collect do |m|
+        code_columns = m.columns.collect(&:name).grep(/_code\z/)
+        nca_fks = m.ncs_coded_attributes.values.collect(&:foreign_key_name)
+        no_nca = code_columns - nca_fks
+
+        no_nca.collect { |missing_nca| "#{m} has a column #{missing_nca} but no corresponding ncs_coded_attribute declaration" }
+      end.flatten
+
+      errors.should == []
+    end
   end
 end
