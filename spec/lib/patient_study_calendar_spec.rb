7afb2854dc77334ab53429817f488b203a8dd5e8
Paul Friedman <p-friedman@northwestern.edu>
2013-04-05 14:24:03 -0500

    Fixed failing spec
    There are 19 segments!
    (said in best Jean Luc Picard voice)

--- spec/lib/patient_study_calendar_spec.rb
+++ spec/lib/patient_study_calendar_spec.rb
@@ -85,7 +85,7 @@ describe PatientStudyCalendar do
   it "gets the segments for the study" do
     use_template_snapshot_cassette do
       segments = subject.segments
-      segments.size.should == 20
+      segments.size.should == 19
       segments.first.attr('name').should == "Pregnancy Screener"
     end
   end
