34a843eac327531c1f3f2c1cd2d3008ddb21ee67
Nataliya Shurupova <n-shurupova@northwestern.edu>
2013-04-08 11:58:31 -0500

    Renamed the p_type method to is_p_type_15
    #3746

--- app/models/response_set_prepopulation/birth_cohort_prepopulator.rb
+++ app/models/response_set_prepopulation/birth_cohort_prepopulator.rb
@@ -1,7 +1,7 @@
 require 'ncs_navigator/core'
 module ResponseSetPrepopulation
   module BirthCohortPrepopulator
-    def is_participant_p_type_15?(question, participant)
+    def is_p_type_15?(question, participant)
       if ((participant.p_type.local_code == 15) || (participant.mother && participant.mother.participant.p_type.local_code == 15))
         answer_for(question, true)
       else
