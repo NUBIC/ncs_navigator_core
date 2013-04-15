d2d3ee89ffad658b38e3a0da3dac68acf814ee06
Jalpa Patel <jalpa-patel@northwestern.edu>
2013-04-15 13:20:20 -0500

    Add date to redis key prefix. Load all the participants first instead of loading one by one. #3790.

--- lib/ncs_navigator/core/psc_registration_processor.rb
+++ lib/ncs_navigator/core/psc_registration_processor.rb
@@ -1,35 +1,33 @@
 class NcsNavigator::Core::PscRegistrationProcessor
   def initialize(pid_io, psc, wh_config)
-    @pid_io = pid_io
+    @csv ||= Rails.application.csv_impl.read(pid_io, :headers => true, :header_converters => :symbol)
     @psc = psc
     @wh_config = wh_config
     @sync_loader = Psc::SyncLoader.new(keygen)
   end
 
-  def csv
-    @csv ||= Rails.application.csv_impl.read(@pid_io, :headers => true, :header_converters => :symbol)
-  end
-
   def register_to_psc
-    csv.each do |row|
-      next if row.header_row?
-      process_participant(row[:p_id])
-    end
-
     @wh_config.shell.say_line("Preparing records for PSC sync...")
+    p_ids = []
+    @csv.each do |row|
+      next if row.header_row?
+      p_ids << row[:p_id]
+    end
+    process_participants(p_ids)
     NcsNavigator::Core::Warehouse::OperationalImporterPscSync.new(@psc, @wh_config, keygen).import('psc_registration')
   end
 
   def keygen
     @keygen ||= lambda do |*c|
-      ['psc_registration', c].flatten.join(':')
+      ["psc_registration:#{Date.today}", c].flatten.join(":")
     end
   end
 
-  def process_participant(p_id)
-    if participant = find_participant(p_id)
-      @sync_loader.cache_participant(participant)
-      participant.events.each do |e|
+  def process_participants(p_ids)
+    participants = Participant.where(:p_id => p_ids)
+    participants.each do |p|
+      @sync_loader.cache_participant(p)
+      p.events.each do |e|
         @sync_loader.cache_event(e, e.participant)
         e.contact_links.each do |cl|
           @sync_loader.cache_contact_link(cl, cl.contact, cl.event, cl.participant)
@@ -37,14 +35,5 @@ class NcsNavigator::Core::PscRegistrationProcessor
       end
     end
   end
-  private :process_participant
-
-  def find_participant(p_id)
-    participant = Participant.where(:p_id => p_id).first
-    unless participant
-      return nil
-    end
-    participant
-  end
-  private :find_participant
+  private :process_participants
 end
