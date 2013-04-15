6531112ad25c8a67eafc010db937576e5d6b06c4
Piotr Hebal <piotr.hebal@northwestern.edu>
2013-04-15 08:59:06 -0500

    2.1 to 2.2 MDES migration for cases #3627

--- lib/ncs_navigator/core/mdes/version_migrations.rb
+++ lib/ncs_navigator/core/mdes/version_migrations.rb
@@ -4,6 +4,7 @@ module NcsNavigator::Core::Mdes
   module VersionMigrations
     autoload :Basic,               'ncs_navigator/core/mdes/version_migrations/basic'
     autoload :ThreeZeroToThreeTwo, 'ncs_navigator/core/mdes/version_migrations/three_zero_to_three_two'
+    autoload :TwoOneToTwoTwo,      'ncs_navigator/core/mdes/version_migrations/two_one_to_two_two.rb'
     autoload :TwoZeroToTwoOne,     'ncs_navigator/core/mdes/version_migrations/two_zero_to_two_one'
   end
 end
