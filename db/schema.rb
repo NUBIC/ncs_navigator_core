1b0175c8bc6288cf4fe1057ff4e118ba67f4cc62
Piotr Hebal <piotr.hebal@northwestern.edu>
2013-04-15 09:13:58 -0500

    Ineligible women batch entry feature #3211 Two tables now display in /providers/#: the top one shows regular people associated with the provider. Bottom one shows people created through the batch-entry form. Form for entry of ineligible women is accessible via /providers/#/ineligibility_batches/new uri. The IneligibilityBatch model keeps track of the batched entries. Those entries are then transformed to the appropriate warehouse models for all MDES versions by IneligibilityBatchEnumerator.

--- db/schema.rb
+++ db/schema.rb
@@ -331,6 +331,26 @@ ActiveRecord::Schema.define(:version => 20130415192041) do
     t.boolean  "being_processed",                            :default => false
   end
 
+  create_table "ineligible_batches", :force => true do |t|
+    t.integer  "age_eligible_code"
+    t.string   "batch_id",                     :limit => 36, :null => false
+    t.integer  "county_of_residence_code"
+    t.date     "created_at"
+    t.string   "date_first_visit",                           :null => false
+    t.date     "date_first_visit_date",                      :null => false
+    t.integer  "first_prenatal_visit_code"
+    t.integer  "ineligible_by_code"
+    t.integer  "people_count",                               :null => false
+    t.integer  "pre_screening_status_code",                  :null => false
+    t.integer  "pregnancy_eligible_code"
+    t.integer  "provider_id",                                :null => false
+    t.integer  "provider_intro_outcome_code",                :null => false
+    t.string   "provider_intro_outcome_other"
+    t.integer  "psu_code",                                   :null => false
+    t.integer  "sampled_person_code",                        :null => false
+    t.datetime "updated_at"
+  end
+
   create_table "institution_person_links", :force => true do |t|
     t.integer  "psu_code",                               :null => false
     t.string   "person_institute_id",      :limit => 36, :null => false
