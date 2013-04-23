NUBIC/ncs_navigator_core

commit 21ba2f9d01de7738e1c5c85618a13185be5f5a30

Do not declare #value as an attribute method.
Some revision between ActiveRecord 3.1 and 3.2 introduced a requirement
that attribute methods in ActiveRecord::Base descendants be backed by
columns.

I'm not sure what revision that is because I'm not interested enough to
bisect Rails for this, but I do know that you get a stack trace like
this without this commit:

    activerecord-3.2.13/lib/active_record/attribute_methods/time_zone_conversion.rb:60:in `create_time_zone_conversion_attribute?':
        undefined method `type' for nil:NilClass (NoMethodError)
    from activerecord-3.2.13/lib/active_record/attribute_methods/time_zone_conversion.rb:24:in `attribute_cast_code'
    from activerecord-3.2.13/lib/active_record/attribute_methods/serialization.rb:80:in `attribute_cast_code'
    from activerecord-3.2.13/lib/active_record/attribute_methods/read.rb:72:in `define_method_attribute'
    from activerecord-3.2.13/lib/active_record/attribute_methods/primary_key.rb:29:in `define_method_attribute'
    from activemodel-3.2.13/lib/active_model/attribute_methods.rb:267:in `block in define_attribute_method'
    from activemodel-3.2.13/lib/active_model/attribute_methods.rb:260:in `each'
    from activemodel-3.2.13/lib/active_model/attribute_methods.rb:260:in `define_attribute_method'
    from /Users/davidyip/Projects/ncs/core/lib/ncs_navigator/core/surveyor/response_value.rb:28:in `included'

The frame at the ActiveModel -> ActiveRecord transition point (i.e.
@ primary_key.rb:29) is executing code that was introduced in Rails @
e9fb6d04bdfbe3dfc4ba1a3cf442ec5d25764300.  Again, that's not
establishing causation, which means that this whole paragraph is really
worthless, but just in Case You Might Want To Know, etc.

More motivated detectives are welcome.

---

You might be wondering why @value_set is unconditionally set in #value=.
The reason is that calling #value= does indeed always set @value, even
if @value just happens to equal #value_from_response.

lib/ncs_navigator/core/surveyor/response_value.rb
@@ -27,7 +27,6 @@ module NcsNavigator::Core::Surveyor
 
     def self.included(model)
       model.before_save :write_value_to_record
-      model.define_attribute_method :value
     end
 
     ##
@@ -79,9 +78,9 @@ module NcsNavigator::Core::Surveyor
     # from malicious intent, and the error is entirely recoverable (i.e. by not
     # doing anything).
     def value=(v)
-      value_will_change! unless @value == v
-
       @value = v
+      @value_set = true
+      @value
     end
 
     ##
@@ -97,11 +96,17 @@ module NcsNavigator::Core::Surveyor
     # 3. If the response's value is not dirty, then this returns the persisted
     #    value of the response's corresponding value column.
     def value
-      if value_changed?
+      if @value_set
         @value
       else
         value_from_response
       end
+    end
+
+    def reload(*)
+      remove_instance_variable(:@value)
+      remove_instance_variable(:@value_set)
+      super
     end
 
     private
@@ -113,7 +118,7 @@ module NcsNavigator::Core::Surveyor
     end
 
     def write_value_to_record
-      return unless value_changed?
+      return unless @value_set
 
       # before_save callbacks run after before_validation callbacks, and
       # Surveyor installs a presence check on answer_id, so we know that we

spec/lib/ncs_navigator/core/surveyor/response_value_spec.rb
@@ -134,6 +134,22 @@ module NcsNavigator::Core::Surveyor
       end
     end
 
+    describe '#value=' do
+      it 'sets non-nil response values to nil' do
+        a.update_attribute(:response_class, 'integer')
+
+        r.value = 42
+        r.save!
+        r.reload
+
+        r.value = nil
+        r.save!
+        r.reload
+
+        r.value.should be_nil
+      end
+    end
+
     describe 'with response class answer' do
       before do
         a.update_attribute(:response_class, 'answer')

commit cc61e1bb2b22962fbc9972280f2ec86c2554ef50

Don't fail when value is set to nil.
The changes made in 21ba2f9d0 broke SurveyTaker, which expects to be
able to set the value to nil when the response_class is answer.

Also includes a change to the error message to make it more useful
in tests or anywhere the response is not persisted yet.

lib/ncs_navigator/core/surveyor/response_value.rb
@@ -69,9 +69,10 @@ module NcsNavigator::Core::Surveyor
     #
     # Some response classes don't have values.  (Answer, for example.)
     #
-    # In non-production environments, attempting to set a value for such a
-    # response will cause a ResponseValue::CannotSetValue exception to be
-    # raised.
+    # In non-production environments, attempting to set a concrete value for
+    # such a response will cause a ResponseValue::CannotSetValue exception to be
+    # raised. (Setting to nil does nothing as that is conceptually the value for
+    # a non-value response class.)
     #
     # In production, however, no exception is raised and the attempted setting
     # is logged at WARN level.  Rationale: it's possible that this may result
@@ -130,8 +131,9 @@ module NcsNavigator::Core::Surveyor
       if field
         reset_values
         send("#{field}=", value)
-      else
-        msg = "A value was set for response #{id}, but the response has non-value response class #{k.inspect}.  The value will not be set."
+      elsif !value.nil?
+        response_name = id ? "response #{id}" : "a response to question #{question.try(:reference_identifier) || question_id.inspect}"
+        msg = "A value was set for #{response_name}, but the response has non-value response class #{k.inspect}.  The value will not be set."
 
         if Rails.env.production?
           Rails.logger.warn msg

spec/lib/ncs_navigator/core/surveyor/response_value_spec.rb
@@ -179,6 +179,12 @@ module NcsNavigator::Core::Surveyor
 
             expect { r.save }.to raise_error(ResponseValue::CannotSetValue)
           end
+
+          it 'does not raise when set to nil' do
+            r.value = nil
+
+            expect { r.save }.to_not raise_error
+          end
         end
       end
     end