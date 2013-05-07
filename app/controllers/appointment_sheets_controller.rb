c374592bfb4b175be6fd9bcc830d354d6ca821a3
Steven Gallagher <s.thomas.gallagher@gmail.com>
2013-05-07 12:33:47 -0500

    added date parameter to appointment sheet #3785

--- app/controllers/appointment_sheets_controller.rb
+++ app/controllers/appointment_sheets_controller.rb
@@ -1,7 +1,7 @@
 class AppointmentSheetsController < ApplicationController
 
   def show
-    @sheet = AppointmentSheet.new(params[:person])
+    @sheet = AppointmentSheet.new(params[:person], params[:date])
     @person = @sheet.person
     @participant = @person.participant
     @participant_activity_plan = psc.build_activity_plan(@participant)
