15256b7cd606073fe84df768c351d7ad343a5703
Paul Friedman <p-friedman@northwestern.edu>
2013-06-03 16:24:49 -0500

    Check for nil #4168
    If there is no response for CHILD_DOB
    do not try to set that value
    
    Fixes #4168

--- app/models/operational_data_extractor/pbs_participant_verification.rb
+++ app/models/operational_data_extractor/pbs_participant_verification.rb
@@ -3,13 +3,21 @@
 module OperationalDataExtractor
   class PbsParticipantVerification < Base
 
+    ##
+    # If there is an associated child record and
+    # a response with the reference identifier 'CHILD_DOB'
+    # and that response has a value, update the child record
+    # to set the person_dob field to that value.
+    #
+    # @see OperationalDataExtractor::Base#child
     def extract_data
-      child_dob_reference_identifier = 'CHILD_DOB'
       if child
-        r = response_set.responses.detect{|r| r.question.reference_identifier == child_dob_reference_identifier}
-        value = response_value(r)
-        child.person_dob = value
-        child.save!
+        if r = response_set.responses.detect{|r| r.question.reference_identifier == 'CHILD_DOB'}
+          if value = response_value(r)
+            child.person_dob = value
+            child.save!
+          end
+        end
       end
     end
   end
