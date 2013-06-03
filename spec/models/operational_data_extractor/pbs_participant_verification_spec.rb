22f6e7161d891a7176cc74748a4bf554c632497e
Paul Friedman <p-friedman@northwestern.edu>
2013-06-03 16:24:49 -0500

    Check for nil #4168
    If there is no response for CHILD_DOB
    do not try to set that value
    
    Fixes #4168

--- spec/models/operational_data_extractor/pbs_participant_verification_spec.rb
+++ spec/models/operational_data_extractor/pbs_participant_verification_spec.rb
@@ -15,7 +15,7 @@ describe OperationalDataExtractor::PbsParticipantVerification do
       @participant.person = @person
 
       @child_participant = @participant.create_child_person_and_participant!(
-        {:first_name => "child_fname", :last_name => "child_lname"})
+        {:first_name => "child_fname", :last_name => "child_lname", :person_dob => nil})
 
       @participant.participant_person_links.size.should == 1
       @participant.save!
@@ -42,5 +42,24 @@ describe OperationalDataExtractor::PbsParticipantVerification do
       child.person_dob.should == '2013-01-01'
 
     end
+
+    it "does not throw an error if there is no answer to 'CHILD_DOB'" do
+      response_set, instrument = prepare_instrument(@person, @child_participant, survey)
+
+      response_set.complete!
+      response_set.save!
+      response_set.responses.should be_blank
+
+      mother = Person.find(@person.id)
+      participant = mother.participant
+      participant.participant_person_links.size.should == 2
+      participant.children.should_not be_nil
+
+      child = participant.children.first
+      child.should == @child_participant.person
+      child.person_dob.should be_blank
+    end
+
+
   end
 end
