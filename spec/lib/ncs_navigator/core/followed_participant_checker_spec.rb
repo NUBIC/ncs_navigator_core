3f389af49222507549aa43ccc23b52c6b7c1a122
Rhett Sutphin <rhett@detailedbalance.net>
2013-06-03 14:02:44 -0500

    Reject CSV without p_id. #4169.
    It does not make sense to use this with a CSV that does not include
    a p_id column. Fail fast in case someone makes that mistake.

--- spec/lib/ncs_navigator/core/followed_participant_checker_spec.rb
+++ spec/lib/ncs_navigator/core/followed_participant_checker_spec.rb
@@ -50,6 +50,20 @@ module NcsNavigator::Core
             ['Participant B567 does not have an intensity value']
         end
       end
+
+      describe 'without a p_id column' do
+        before do
+          create_csv(
+            %w(participants),
+            %w(A400),
+            %w(B747)
+          )
+        end
+
+        it 'throws an exception on CSV read' do
+          expect { checker.expected_participants }.to raise_error(/has no p_id column/)
+        end
+      end
     end
 
     describe '#expected_p_ids' do
