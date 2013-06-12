29426e66f5aa89fad2c7b844134078a0738cbe72
David Yip <yipdw@northwestern.edu>
2013-06-12 14:55:05 -0500

    Address proper event; restore dependent PSC fixture.
    99d2028c77c646e5be7334cb082fec54e745c0f7 erroneously removed a test
    fixture used by the spec affected in this commit.   This commit restores
    that fixture and updates said spec for other changes in the event-PSC
    linkage context.

--- spec/integration/models/event_spec.rb
+++ spec/integration/models/event_spec.rb
@@ -9,7 +9,7 @@ describe Event do
     include_context 'event-PSC linkage'
 
     it 'loads scheduled activities for each event in a relation' do
-      es = Event.where(:id => [screener, pv1].map(&:id)).with_psc_data(psc)
+      es = Event.where(:id => [screener, pv1_1].map(&:id)).with_psc_data(psc)
 
       es.length.should == 2
       es.all? { |e| e.scheduled_activities.length == 1 }.should be_true
