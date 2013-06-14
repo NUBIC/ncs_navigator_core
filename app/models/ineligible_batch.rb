d3a5de39eae2b05fdd684a38442d6823fb6932dc
Piotr Hebal <piotr.hebal@northwestern.edu>
2013-06-14 09:47:44 -0500

    Provider_id column should be of type string #9465

--- app/models/ineligible_batch.rb
+++ app/models/ineligible_batch.rb
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # == Schema Information
-# Schema version: 20130405160400
+# Schema version: 20130614142954
 #
 # Table name: ineligible_batches
 #
 #  age_eligible_code            :integer
 #  batch_id                     :string(36)       not null
 #  county_of_residence_code     :integer
-#  created_at                   :date
+#  created_at                   :date             not null
 #  date_first_visit             :string(255)      not null
 #  date_first_visit_date        :date             not null
 #  first_prenatal_visit_code    :integer
@@ -16,12 +16,12 @@
 #  people_count                 :integer          not null
 #  pre_screening_status_code    :integer          not null
 #  pregnancy_eligible_code      :integer
-#  provider_id                  :integer          not null
+#  provider_id                  :string(36)       not null
 #  provider_intro_outcome_code  :integer          not null
 #  provider_intro_outcome_other :string(255)
 #  psu_code                     :integer          not null
 #  sampled_person_code          :integer          not null
-#  updated_at                   :datetime
+#  updated_at                   :datetime         not null
 #
 
 class IneligibleBatch < ActiveRecord::Base
