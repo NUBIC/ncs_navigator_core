e4fb7364a77f063f1654e46ef8eced834b25326f
Paul Friedman <p-friedman@northwestern.edu>
2013-07-05 13:27:09 -0500

    add non conflict for mdes_date field #3905

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -75,6 +75,13 @@ $(document).ready(function() {
     changeYear: true,
     yearRange: '1920:2020'
   });
+  $("input[type='text'].mdes_date").datepicker({
+    dateFormat: 'yy-mm-dd',
+    changeMonth: true,
+    changeYear: true,
+    yearRange: '1920:2020',
+    constrainInput: false
+  });
   $("input[type='text'].datepicker").datepicker({
     dateFormat: 'yy-mm-dd',
     changeMonth: true,
